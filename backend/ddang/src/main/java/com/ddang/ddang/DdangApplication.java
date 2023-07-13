00131337fe68344239081e7463db4fcfeef28622
제이미 <63184334+JJ503@users.noreply.github.com>
2023-07-13 17:06:36 +0900

    chore: #7 프로젝트 세팅 (#8)

---
+++ backend/ddang/src/main/java/com/ddang/ddang/DdangApplication.java
@@ -0,0 +1,13 @@
+package com.ddang.ddang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdangApplication.class, args);
+	}
+
+}
