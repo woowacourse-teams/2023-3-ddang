5ccb3f9821a972363152ed49b7a7acb63e789c20
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-07-13 17:50:58 +0900

    feat: 전역 예외 처리 핸들러 추가
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>

---
+++ backend/ddang/src/main/java/com/ddang/ddang/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.ddang.ddang.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+        log.error("exception : {}, {}", ex.getClass().getSimpleName(), ex);
+
+        return super.handleExceptionInternal(ex, body, headers, statusCode, request);
+    }
+}
