90665cc101025ac2411e6e8de1c78857205034c9
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-07-14 11:54:44 +0900

    refactor: 불필요한 어노테이션 제거
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>

--- backend/ddang/src/main/java/com/ddang/ddang/exception/GlobalExceptionHandler.java
+++ backend/ddang/src/main/java/com/ddang/ddang/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.ddang.ddang.exception;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -8,13 +7,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    private static final String EXCEPTION_FORMAT = "%s : ";
+
     @Override
-    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
-        log.error("exception : {}, {}", ex.getClass().getSimpleName(), ex);
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex,
+            Object body,
+            HttpHeaders headers,
+            HttpStatusCode statusCode,
+            WebRequest request
+    ) {
+        logger.error(String.format(EXCEPTION_FORMAT, ex.getClass()
+                                                       .getSimpleName()), ex);
 
         return super.handleExceptionInternal(ex, body, headers, statusCode, request);
     }
