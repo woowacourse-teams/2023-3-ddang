fe73d9e219ed64f49ea5914d537c286ceadff64b
제이미 <63184334+JJ503@users.noreply.github.com>
2023-07-14 16:37:14 +0900

    chore: #7 프로젝트 기초 세팅 추가 (#10)
    * feat: 전역 예외 처리 핸들러 추가
    
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>
    
    * refactor: 불필요한 어노테이션 제거
    
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>
    
    * chore: jacoco 기초 설정 추가
    
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>
    
    * chore: rest docs 기초 설정 추가
    
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>
    
    * chore: log 기초 설정 추가
    
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>
    
    * feat: 에러로그 발생 시 슬랙에 메세지를 전송하는 기능 추가
    
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>
    
    ---------
    
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>

---
+++ backend/ddang/src/main/java/com/ddang/ddang/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.ddang.ddang.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final String EXCEPTION_FORMAT = "%s : ";
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex,
+            Object body,
+            HttpHeaders headers,
+            HttpStatusCode statusCode,
+            WebRequest request
+    ) {
+        logger.error(String.format(EXCEPTION_FORMAT, ex.getClass()
+                                                       .getSimpleName()), ex);
+
+        return super.handleExceptionInternal(ex, body, headers, statusCode, request);
+    }
+}
