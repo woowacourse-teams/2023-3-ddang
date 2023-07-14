6b8eaa666cdff04bb6f127ef7bb700795903ca68
mendel <rhthrhrl0@naver.com>
2023-07-14 17:32:50 +0900

    feat: 패키지 구조 설계

--- android/app/src/main/java/com/ddangddangddang/android/feature/main/MainActivity.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ddangddangddang.android
+package com.ddangddangddang.android.feature.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ddangddangddang.android.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
