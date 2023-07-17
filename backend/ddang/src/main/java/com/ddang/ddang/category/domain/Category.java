59860ef6bf7d9a91af5c37d239f1ff2649a34db0
제이미 <63184334+JJ503@users.noreply.github.com>
2023-07-17 15:40:04 +0900

    feat: #11 카테고리 엔티티 및 레포지토리 추가 (#19)
    * feat: 카테고리 엔티티 추가
    
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>
    
    * fix: DB 설정 문제 해결
    
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>
    
    * refactor: 필요 없는 클래스 제거
    
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>
    
    * feat: Equals&HashCode 재정의 추가
    
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>
    
    * feat: 카테고리 레포지토리 추가
    
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>
    
    * refactor: 카테고리 연관관계 메서드 이름 변경
    
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>
    
    * style: 컨벤션 적용
    
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>
    
    * test: 유틸리티 어노테이션 추가
    
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>
    
    ---------
    
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>

---
+++ backend/ddang/src/main/java/com/ddang/ddang/category/domain/Category.java
@@ -0,0 +1,54 @@
+package com.ddang.ddang.category.domain;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EqualsAndHashCode(of = "id")
+@ToString(exclude = {"subCategories", "mainCategory"})
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30)
+    private String name;
+
+    @OneToMany(mappedBy = "mainCategory", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<Category> subCategories = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "main_category_id", foreignKey = @ForeignKey(name = "fk_main_sub"))
+    private Category mainCategory;
+
+    public Category(final String name) {
+        this.name = name;
+    }
+
+    public void addSubCategory(final Category subCategory) {
+        subCategories.add(subCategory);
+        subCategory.mainCategory = this;
+    }
+}
