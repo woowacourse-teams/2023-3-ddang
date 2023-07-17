c1f34d71f8aa6a321712ce09a0f76548dfaa6a2a
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-07-17 19:56:36 +0900

    feat: 지역 엔티티 추가
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>

---
+++ backend/ddang/src/test/java/com/ddang/ddang/region/domain/RegionTest.java
@@ -0,0 +1,52 @@
+package com.ddang.ddang.region.domain;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class RegionTest {
+
+    @Test
+    void 첫번째_지역과_두번째_지역의_연관관계를_세팅한다() {
+        // given
+        final Region first = new Region("서울특별시");
+        final Region second = new Region("강남구");
+
+        // when
+        first.addSecondRegion(second);
+
+        // then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(first.getSecondRegions())
+                          .hasSize(1);
+            softAssertions.assertThat(second.getFirstRegion())
+                          .isNotNull();
+        });
+    }
+
+    @Test
+    void 두번째_지역과_세번째_지역의_연관관계를_세팅한다() {
+        // given
+        final Region first = new Region("서울특별시");
+        final Region second = new Region("강남구");
+        final Region third = new Region("역삼동");
+
+        first.addSecondRegion(second);
+
+        // when
+        second.addThirdRegion(third);
+
+        // then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(second.getThirdRegions())
+                          .hasSize(1);
+            softAssertions.assertThat(third.getSecondRegion())
+                          .isNotNull();
+            softAssertions.assertThat(third.getFirstRegion())
+                          .isNotNull();
+        });
+    }
+}
