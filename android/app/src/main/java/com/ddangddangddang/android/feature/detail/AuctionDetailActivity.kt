de4a96b73537243e08ded212dabde01f84754193
mendel <rhthrhrl0@naver.com>
2023-07-18 07:39:17 +0900

    feat: 상품 이미지 뷰페이저 추가

--- android/app/src/main/java/com/ddangddangddang/android/feature/detail/AuctionDetailActivity.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/detail/AuctionDetailActivity.kt
@@ -4,19 +4,42 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.viewpager2.widget.MarginPageTransformer
 import com.ddangddangddang.android.R
 import com.ddangddangddang.android.databinding.ActivityAuctionDetailBinding
 import com.ddangddangddang.android.feature.common.viewModelFactory
 import com.ddangddangddang.android.util.binding.BindingActivity
+import com.google.android.material.tabs.TabLayoutMediator
 
 class AuctionDetailActivity :
     BindingActivity<ActivityAuctionDetailBinding>(R.layout.activity_auction_detail) {
     private val viewModel: AuctionDetailViewModel by viewModels { viewModelFactory }
+
+    private val images = listOf(
+        "https://cdn.pixabay.com/photo/2019/12/26/10/44/horse-4720178_1280.jpg",
+        "https://cdn.pixabay.com/photo/2020/11/04/15/29/coffee-beans-5712780_1280.jpg",
+        "https://cdn.pixabay.com/photo/2020/03/08/21/41/landscape-4913841_1280.jpg",
+        "https://cdn.pixabay.com/photo/2020/09/02/18/03/girl-5539094_1280.jpg",
+        "https://cdn.pixabay.com/photo/2014/03/03/16/15/mosque-279015_1280.jpg",
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.viewModel = viewModel
         val auctionId = intent.getLongExtra(AUCTION_ID_KEY, -1L)
         setupViewModel(auctionId)
+
+        binding.vpImageList.apply {
+            clipToPadding = false
+            clipChildren = false
+            offscreenPageLimit = 1
+            adapter = AuctionImageAdapter(images)
+            setPageTransformer(MarginPageTransformer(100))
+            setPadding(200, 0, 200, 0)
+        }
+
+        TabLayoutMediator(binding.tlIndicator, binding.vpImageList) { tab, position ->
+        }.attach()
     }
 
     private fun setupViewModel(auctionId: Long) {
