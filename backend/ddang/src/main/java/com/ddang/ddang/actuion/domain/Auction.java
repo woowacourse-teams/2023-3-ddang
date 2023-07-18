f31f0d67d2cf4c9a01ef730a940be8de056eec02
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-07-18 13:47:54 +0900

    feat: 경매 엔티티 추가
    Co-authored-by: swonny <wonny921@gmail.com>
    Co-authored-by: apptie <appti@naver.com>
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>

---
+++ backend/ddang/src/main/java/com/ddang/ddang/actuion/domain/Auction.java
@@ -0,0 +1,53 @@
+package com.ddang.ddang.actuion.domain;
+
+import com.ddang.ddang.common.entity.BaseTimeEntity;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EqualsAndHashCode(callSuper = false, of = {"id"})
+@ToString
+public class Auction extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30)
+    private String title;
+
+    @Lob
+    private String description;
+
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "start_bid_price"))
+    private Price startBidPrice;
+
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "last_bid_price"))
+    private Price lastBidPrice;
+
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "winning_bid_price"))
+    private Price winningBidPrice;
+
+    @Column(name = "is_deleted")
+    private boolean deleted;
+
+    private LocalDateTime closingTime;
+}
