498791576a232a4b0a5408232b5c519e44b6110d
hyemdooly <hyemdooly@gmail.com>
2023-07-18 14:47:12 +0900

    feat: 클릭 시 날짜, 시간 선택창 띄우기 구현

--- android/app/src/main/java/com/ddangddangddang/android/feature/register/RegisterAuctionActivity.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/register/RegisterAuctionActivity.kt
@@ -1,17 +1,59 @@
 package com.ddangddangddang.android.feature.register
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import com.ddangddangddang.android.R
 import com.ddangddangddang.android.databinding.ActivityRegisterAuctionBinding
 import com.ddangddangddang.android.feature.common.viewModelFactory
 import com.ddangddangddang.android.util.binding.BindingActivity
 
-class RegisterAuctionActivity : BindingActivity<ActivityRegisterAuctionBinding>(R.layout.activity_register_auction) {
+class RegisterAuctionActivity :
+    BindingActivity<ActivityRegisterAuctionBinding>(R.layout.activity_register_auction) {
     private val viewModel by viewModels<RegisterAuctionViewModel> { viewModelFactory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.viewModel = viewModel
+
+        viewModel.event.observe(
+            this,
+            Observer {
+                showDateTimePicker(it.year, it.month, it.dayOfMonth, it.hourOfDay, it.minute)
+            },
+        )
+    }
+
+    private fun showDateTimePicker(
+        selectedYear: Int,
+        selectedMonth: Int,
+        selectedDay: Int,
+        selectedHour: Int,
+        selectedMinute: Int,
+    ) {
+        DatePickerDialog(
+            this,
+            { _, year, month, dayOfMonth ->
+                viewModel.setClosingDate(year, month, dayOfMonth)
+                showTimePicker(selectedHour, selectedMinute)
+            },
+            selectedYear,
+            selectedMonth,
+            selectedDay,
+        ).show()
+    }
+
+    private fun showTimePicker(selectedHour: Int, selectedMinute: Int) {
+        TimePickerDialog(
+            this,
+            { _, hourOfDay, minute ->
+                viewModel.setClosingTime(hourOfDay, minute)
+            },
+            selectedHour,
+            selectedMinute,
+            false,
+        ).show()
     }
 }
--- android/app/src/main/java/com/ddangddangddang/android/feature/register/RegisterAuctionViewModel.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/register/RegisterAuctionViewModel.kt
@@ -1,5 +1,40 @@
 package com.ddangddangddang.android.feature.register
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.ddangddangddang.android.util.livedata.SingleLiveEvent
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
 
-class RegisterAuctionViewModel : ViewModel()
+class RegisterAuctionViewModel : ViewModel() {
+    private var closingTime: LocalDateTime = LocalDateTime.now()
+
+    private val _event: SingleLiveEvent<Event.ShowDatePicker> = SingleLiveEvent()
+    val event: LiveData<Event.ShowDatePicker>
+        get() = _event
+
+    fun onClickDatePicker() {
+        _event.value = Event.ShowDatePicker(
+            closingTime.year,
+            closingTime.month.value,
+            closingTime.dayOfMonth,
+            closingTime.hour,
+            closingTime.minute,
+        )
+    }
+
+    fun setClosingDate(year: Int, month: Int, dayOfMonth: Int) {
+        closingTime =
+            LocalDateTime.of(LocalDate.of(year, month, dayOfMonth), closingTime.toLocalTime())
+    }
+
+    fun setClosingTime(hourOfDay: Int, minute: Int) {
+        closingTime = LocalDateTime.of(closingTime.toLocalDate(), LocalTime.of(hourOfDay, minute))
+    }
+
+    sealed class Event {
+        class ShowDatePicker(val year: Int, val month: Int, val dayOfMonth: Int, val hourOfDay: Int, val minute: Int) :
+            Event()
+    }
+}
