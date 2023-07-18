854ef582b59dbc48381f6174b00b701a6b6eec1d
mendel <rhthrhrl0@naver.com>
2023-07-18 17:26:28 +0900

    refactor: 등록된 상품의 임시 이미지 url 데이터의 위치를 뷰모델로 수정

--- android/app/src/main/java/com/ddangddangddang/android/feature/detail/AuctionDetailActivity.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/detail/AuctionDetailActivity.kt
@@ -15,14 +15,6 @@ class AuctionDetailActivity :
     BindingActivity<ActivityAuctionDetailBinding>(R.layout.activity_auction_detail) {
     private val viewModel: AuctionDetailViewModel by viewModels { viewModelFactory }
 
-    private val images = listOf(
-        "https://cdn.pixabay.com/photo/2019/12/26/10/44/horse-4720178_1280.jpg",
-        "https://cdn.pixabay.com/photo/2020/11/04/15/29/coffee-beans-5712780_1280.jpg",
-        "https://cdn.pixabay.com/photo/2020/03/08/21/41/landscape-4913841_1280.jpg",
-        "https://cdn.pixabay.com/photo/2020/09/02/18/03/girl-5539094_1280.jpg",
-        "https://cdn.pixabay.com/photo/2014/03/03/16/15/mosque-279015_1280.jpg",
-    )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.viewModel = viewModel
@@ -33,7 +25,7 @@ class AuctionDetailActivity :
             clipToPadding = false
             clipChildren = false
             offscreenPageLimit = 1
-            adapter = AuctionImageAdapter(images)
+            adapter = AuctionImageAdapter(viewModel.images)
             setPageTransformer(MarginPageTransformer(100))
             setPadding(200, 0, 200, 0)
         }
--- android/app/src/main/java/com/ddangddangddang/android/feature/detail/AuctionDetailViewModel.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/detail/AuctionDetailViewModel.kt
@@ -9,6 +9,14 @@ class AuctionDetailViewModel : ViewModel() {
     val event: LiveData<MainScreenEvent>
         get() = _event
 
+    val images = listOf(
+        "https://cdn.pixabay.com/photo/2019/12/26/10/44/horse-4720178_1280.jpg",
+        "https://cdn.pixabay.com/photo/2020/11/04/15/29/coffee-beans-5712780_1280.jpg",
+        "https://cdn.pixabay.com/photo/2020/03/08/21/41/landscape-4913841_1280.jpg",
+        "https://cdn.pixabay.com/photo/2020/09/02/18/03/girl-5539094_1280.jpg",
+        "https://cdn.pixabay.com/photo/2014/03/03/16/15/mosque-279015_1280.jpg",
+    )
+
     fun exit() {
         _event.value = MainScreenEvent.Exit
     }
