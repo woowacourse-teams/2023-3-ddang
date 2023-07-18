6602d3c1ab01107a045ef0e2286b97a1e5fe5532
Mendel <67176829+rhthrhrl0@users.noreply.github.com>
2023-07-18 21:11:51 +0900

    경매 상품 상세 페이지 UI 추가 (#39)
    * feat: 상단 앱 바 UI 추가
    
    * feat: 경매 입찰 버튼 UI 추가
    
    * feat: 상품 이미지 뷰페이저 추가
    
    * feat: 상품 정보 UI 추가
    
    * feat: 판매자 정보 UI 추가
    
    * refactor: 등록된 상품의 임시 이미지 url 데이터의 위치를 뷰모델로 수정
    
    * refactor: 함수 분리
    
    * rename: Event를 AuctionDetailEvent로 이름 변경
    
    * refactor: root element를 shape로 수정
    
    * refactor: 상세 화면에서만 사용하는 색상을 colors.xml에서 제거
    
    * refactor: 스트링 리소스 정리

--- android/app/src/main/java/com/ddangddangddang/android/feature/common/ViewModelFactory.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/common/ViewModelFactory.kt
@@ -3,6 +3,7 @@ package com.ddangddangddang.android.feature.common
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import com.ddangddangddang.android.feature.detail.AuctionDetailViewModel
 import com.ddangddangddang.android.feature.main.MainViewModel
 import com.ddangddangddang.android.feature.register.RegisterAuctionViewModel
 
@@ -13,8 +14,8 @@ val viewModelFactory = object : ViewModelProvider.Factory {
             // 레포지토리 싱글톤 객체 얻어옴
             when {
                 isAssignableFrom(MainViewModel::class.java) -> MainViewModel()
+                isAssignableFrom(AuctionDetailViewModel::class.java) -> AuctionDetailViewModel()
                 isAssignableFrom(RegisterAuctionViewModel::class.java) -> RegisterAuctionViewModel()
-                // 여기에 뷰모델 추가 로직 작성하면 됨
                 else ->
                     throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
             }
---
+++ android/app/src/main/java/com/ddangddangddang/android/feature/detail/AuctionDetailViewModel.kt
@@ -0,0 +1,27 @@
+package com.ddangddangddang.android.feature.detail
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.ddangddangddang.android.util.livedata.SingleLiveEvent
+
+class AuctionDetailViewModel : ViewModel() {
+    private val _event: SingleLiveEvent<AuctionDetailEvent> = SingleLiveEvent()
+    val event: LiveData<AuctionDetailEvent>
+        get() = _event
+
+    val images = listOf(
+        "https://cdn.pixabay.com/photo/2019/12/26/10/44/horse-4720178_1280.jpg",
+        "https://cdn.pixabay.com/photo/2020/11/04/15/29/coffee-beans-5712780_1280.jpg",
+        "https://cdn.pixabay.com/photo/2020/03/08/21/41/landscape-4913841_1280.jpg",
+        "https://cdn.pixabay.com/photo/2020/09/02/18/03/girl-5539094_1280.jpg",
+        "https://cdn.pixabay.com/photo/2014/03/03/16/15/mosque-279015_1280.jpg",
+    )
+
+    fun exit() {
+        _event.value = AuctionDetailEvent.Exit
+    }
+
+    sealed class AuctionDetailEvent {
+        object Exit : AuctionDetailEvent()
+    }
+}
