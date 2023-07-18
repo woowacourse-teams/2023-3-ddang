e179faf68eba0b157249d83166ce553f609d2def
Song Hyemin <hyemdooly@gmail.com>
2023-07-18 16:45:00 +0900

    feat: #20 경매 상품 등록 페이지 날짜, 시간 Picker 추가 (#38)
    * feat: 클릭 시 날짜, 시간 선택창 띄우기 구현
    
    * feat: 함수 분리 및 정리, 뷰가 선택한 시간을 반영하도록 수정
    
    * refactor: 함수명 수정

--- android/app/src/main/java/com/ddangddangddang/android/feature/register/RegisterAuctionActivity.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/register/RegisterAuctionActivity.kt
@@ -1,17 +1,64 @@
 package com.ddangddangddang.android.feature.register
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.ddangddangddang.android.R
 import com.ddangddangddang.android.databinding.ActivityRegisterAuctionBinding
 import com.ddangddangddang.android.feature.common.viewModelFactory
 import com.ddangddangddang.android.util.binding.BindingActivity
+import java.time.LocalDateTime
+import java.time.LocalTime
 
-class RegisterAuctionActivity : BindingActivity<ActivityRegisterAuctionBinding>(R.layout.activity_register_auction) {
+class RegisterAuctionActivity :
+    BindingActivity<ActivityRegisterAuctionBinding>(R.layout.activity_register_auction) {
     private val viewModel by viewModels<RegisterAuctionViewModel> { viewModelFactory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.viewModel = viewModel
+        setupObserveEvent()
+    }
+
+    private fun setupObserveEvent() {
+        viewModel.event.observe(
+            this,
+        ) {
+            handleEvent(it)
+        }
+    }
+
+    private fun handleEvent(event: RegisterAuctionViewModel.Event) {
+        when (event) {
+            is RegisterAuctionViewModel.Event.ClosingTimePicker -> {
+                showDateTimePicker(event.dateTime)
+            }
+        }
+    }
+
+    private fun showDateTimePicker(selectedDateTime: LocalDateTime) {
+        DatePickerDialog(
+            this,
+            { _, year, month, dayOfMonth ->
+                viewModel.setClosingDate(year, month, dayOfMonth)
+                showTimePicker(selectedDateTime.toLocalTime())
+            },
+            selectedDateTime.year,
+            selectedDateTime.monthValue,
+            selectedDateTime.dayOfMonth,
+        ).show()
+    }
+
+    private fun showTimePicker(selectedTime: LocalTime) {
+        TimePickerDialog(
+            this,
+            { _, hourOfDay, minute ->
+                viewModel.setClosingTime(hourOfDay, minute)
+            },
+            selectedTime.hour,
+            selectedTime.minute,
+            false,
+        ).show()
     }
 }
