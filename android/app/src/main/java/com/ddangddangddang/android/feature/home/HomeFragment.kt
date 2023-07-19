08f6df2b3c635e9a1545b914b616e9bca364e072
ippnsj <ippnsj@gmail.com>
2023-07-19 16:12:13 +0900

    feat: 경매 상품 선택 시 경매 상세를 보여주는 기능 구현

--- android/app/src/main/java/com/ddangddangddang/android/feature/home/HomeFragment.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.viewModels
 import com.ddangddangddang.android.R
 import com.ddangddangddang.android.databinding.FragmentHomeBinding
 import com.ddangddangddang.android.feature.common.viewModelFactory
+import com.ddangddangddang.android.feature.detail.AuctionDetailActivity
 import com.ddangddangddang.android.util.binding.BindingFragment
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
@@ -13,13 +14,33 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupViewModel()
         setupAuctionRecyclerView()
+    }
+
+    private fun setupViewModel() {
+        viewModel.event.observe(viewLifecycleOwner) { handleEvent(it) }
+    }
+
+    private fun handleEvent(event: HomeViewModel.HomeEvent) {
+        when (event) {
+            is HomeViewModel.HomeEvent.NavigateToAuctionDetail -> {
+                navigateToAuctionDetail(event.auctionId)
+            }
+        }
     }
 
     private fun setupAuctionRecyclerView() {
         with(binding.rvAuction) {
-            adapter = AuctionAdapter().apply { setAuctions(viewModel.auctions) }
+            adapter = AuctionAdapter { auctionId ->
+                viewModel.navigateToAuctionDetail(auctionId)
+            }.apply { setAuctions(viewModel.auctions) }
             addItemDecoration(AuctionSpaceItemDecoration(spanCount = 2, space = 20))
         }
+    }
+
+    private fun navigateToAuctionDetail(auctionId: Long) {
+        val intent = AuctionDetailActivity.getIntent(requireContext(), auctionId)
+        startActivity(intent)
     }
 }
