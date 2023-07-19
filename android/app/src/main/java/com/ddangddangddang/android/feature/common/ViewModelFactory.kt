feb0ebab7489f12d246bebdc77d9fdbecafd757c
ippnsj <49394114+ippnsj@users.noreply.github.com>
2023-07-19 14:40:05 +0900

    feat: #22 경매 상품 목록 레이아웃 작성 (#54)
    * feat: 경매 상품 목록 페이지 레이아웃 작성
    
    * feat: 경매 상품 목록 필터 아이템 레이아웃 작성
    
    * rename: 파일명 및 속성명 변경
    
    * feat: 경매 상품 목록 경매 아이템 레이아웃 작성
    
    * refactor: selector 에서 true 조건 먼저 확인하도록 수정 및 디폴트 조건 제거
    
    * refactor: 조건문 코드 한 줄로 수정
    
    * refactor: 상태값을 string resource로 분리
    
    * refactor: 불필요한 로그 삭제

--- android/app/src/main/java/com/ddangddangddang/android/feature/common/ViewModelFactory.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/common/ViewModelFactory.kt
@@ -3,6 +3,7 @@ package com.ddangddangddang.android.feature.common
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import com.ddangddangddang.android.feature.home.HomeViewModel
 import com.ddangddangddang.android.feature.detail.AuctionDetailViewModel
 import com.ddangddangddang.android.feature.main.MainViewModel
 import com.ddangddangddang.android.feature.register.RegisterAuctionViewModel
@@ -14,6 +15,7 @@ val viewModelFactory = object : ViewModelProvider.Factory {
             // 레포지토리 싱글톤 객체 얻어옴
             when {
                 isAssignableFrom(MainViewModel::class.java) -> MainViewModel()
+                isAssignableFrom(HomeViewModel::class.java) -> HomeViewModel()
                 isAssignableFrom(AuctionDetailViewModel::class.java) -> AuctionDetailViewModel()
                 isAssignableFrom(RegisterAuctionViewModel::class.java) -> RegisterAuctionViewModel()
                 else ->
