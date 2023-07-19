1d249ef676998e165c77368c600e4166e6b05442
ippnsj <ippnsj@gmail.com>
2023-07-19 14:27:12 +0900

    refactor: 상태값을 string resource로 분리

--- android/app/src/main/java/com/ddangddangddang/android/model/AuctionHomeStatusModel.kt
+++ android/app/src/main/java/com/ddangddangddang/android/model/AuctionHomeStatusModel.kt
@@ -1,15 +1,16 @@
 package com.ddangddangddang.android.model
 
 import androidx.annotation.ColorRes
+import androidx.annotation.StringRes
 import com.ddangddangddang.android.R
 
 enum class AuctionHomeStatusModel(
-    val priceStatus: String,
-    val progressStatus: String,
+    @StringRes val priceStatus: Int,
+    @StringRes val progressStatus: Int,
     @ColorRes val colorId: Int,
 ) {
-    ONGOING("현재가", "경매중", R.color.red_100),
-    UNBIDDEN("시작가", "경매중", R.color.green),
-    SUCCESS("낙찰가", "낙찰 완료", R.color.black_600),
-    FAILURE("시작가", "경매 유찰", R.color.black_600),
+    ONGOING(R.string.all_current_price, R.string.all_auction_ongoing, R.color.red_100),
+    UNBIDDEN(R.string.all_start_price, R.string.all_auction_ongoing, R.color.green),
+    SUCCESS(R.string.all_winning_bid_price, R.string.all_auction_success, R.color.black_600),
+    FAILURE(R.string.all_start_price, R.string.all_auction_failure, R.color.black_600),
 }
