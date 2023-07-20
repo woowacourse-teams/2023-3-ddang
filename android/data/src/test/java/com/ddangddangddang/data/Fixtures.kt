8f85bff2c5fc71df65491f8954004e925267185f
Song Hyemin <hyemdooly@gmail.com>
2023-07-20 17:52:55 +0900

    test: #91 Repository 테스트 작성 - 경매 상품 등록, 상세 정보 (#93)
    * test: 경매 등록 테스트 코드 작성
    
    * test: 경매 상품 상세 정보 테스트 코드 작성
    
    * refactor: 상수 수정

--- android/data/src/test/java/com/ddangddangddang/data/Fixtures.kt
+++ android/data/src/test/java/com/ddangddangddang/data/Fixtures.kt
@@ -1,7 +1,16 @@
 package com.ddangddangddang.data
 
+import com.ddangddangddang.data.model.request.CategoryRequest
+import com.ddangddangddang.data.model.request.DirectRegionRequest
+import com.ddangddangddang.data.model.request.RegisterAuctionRequest
+import com.ddangddangddang.data.model.response.AuctionDetailResponse
 import com.ddangddangddang.data.model.response.AuctionPreviewResponse
 import com.ddangddangddang.data.model.response.AuctionPreviewsResponse
+import com.ddangddangddang.data.model.response.AuctionResponse
+import com.ddangddangddang.data.model.response.CategoryResponse
+import com.ddangddangddang.data.model.response.DirectRegionResponse
+import com.ddangddangddang.data.model.response.RegisterAuctionResponse
+import com.ddangddangddang.data.model.response.SellerResponse
 
 private fun createAuctionPreviewResponse(
     id: Long = 1,
@@ -16,3 +25,66 @@ fun createAuctionPreviewsResponse(
     auctions: List<AuctionPreviewResponse> = listOf(createAuctionPreviewResponse()),
     lastAuctionId: Long = 1,
 ) = AuctionPreviewsResponse(auctions, lastAuctionId)
+
+fun createRegisterAuctionRequest(
+    images: List<String> = listOf("https://i0.wp.com/opensea.kr/wp-content/uploads/2021/01/2169FF1C-8A4A-46CE-A55A-7ED291EEA033_1_105_c-1.jpeg?w=1051&ssl=1"),
+    title: String = "맥북 에어 13인치",
+    category: CategoryRequest = CategoryRequest("전자기기", "노트북"),
+    description: String = "얼마 안쓴 맥북 팝니다",
+    startPrice: Int = 100000,
+    bidUnit: Int = 1000,
+    closingTime: String = "2100-07-31T12:00:00",
+    directRegions: List<DirectRegionRequest> = listOf(DirectRegionRequest("경기도", "부천시", "원미구")),
+) = RegisterAuctionRequest(
+    images,
+    title,
+    category,
+    description,
+    startPrice,
+    bidUnit,
+    closingTime,
+    directRegions,
+)
+
+fun createRegisterAuctionResponse(id: Long = 2) = RegisterAuctionResponse(id)
+fun createAuctionDetailResponse(
+    auction: AuctionResponse = createAuctionResponse(),
+    seller: SellerResponse = createSellerResponse(),
+) = AuctionDetailResponse(auction, seller)
+
+private fun createAuctionResponse(
+    id: Long = 2,
+    images: List<String> = listOf("https://www.apple.com/newsroom/images/product/mac/standard/Apple_MacBook-Pro_14-16-inch_10182021_big.jpg.large.jpg"),
+    title: String = "맥북 16인치",
+    categoryResponse: CategoryResponse = CategoryResponse("전자기기", "노트북"),
+    description: String = "맥북 2019 16인치 팝니다. 급전이 필요해요...",
+    startPrice: Int = 900000,
+    lastBidPrice: Int = 100000,
+    status: String = "UNBIDDEN",
+    bidUnit: Int = 3000,
+    registerTime: String = "2023-07-20T16:38:28",
+    closingTime: String = "2023-07-25T16:38:28",
+    directRegions: List<DirectRegionResponse> = listOf(DirectRegionResponse("서울특별시", "관악구", "성현동")),
+    auctioneerCount: Int = 0,
+) = AuctionResponse(
+    id,
+    images,
+    title,
+    categoryResponse,
+    description,
+    startPrice,
+    lastBidPrice,
+    status,
+    bidUnit,
+    registerTime,
+    closingTime,
+    directRegions,
+    auctioneerCount,
+)
+
+private fun createSellerResponse(
+    id: Long = 1,
+    image: String = "https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/7r5X/image/9djEiPBPMLu_IvCYyvRPwmZkM1g.jpg",
+    nickname: String = "닉네임",
+    reliability: Double = 5.0,
+) = SellerResponse(id, image, nickname, reliability)
--- android/data/src/test/java/com/ddangddangddang/data/MockServer.kt
+++ android/data/src/test/java/com/ddangddangddang/data/MockServer.kt
@@ -17,6 +17,21 @@ object MockServer {
                         .setResponseCode(200)
                         .setBody(auctionPreviewsResponse)
                 }
+
+                path == "/auctions" && request.method == "POST" -> {
+                    MockResponse()
+                        .setHeader("Content-Type", "application/json")
+                        .setResponseCode(200)
+                        .setBody(registerAuctionResponse)
+                }
+
+                path.contains("/auctions") && path.split("/").size > 1 && request.method == "GET" -> {
+                    MockResponse()
+                        .setHeader("Content-Type", "application/json")
+                        .setResponseCode(200)
+                        .setBody(auctionDetailResponse)
+                }
+
                 else -> {
                     MockResponse().setResponseCode(404)
                 }
@@ -38,6 +53,52 @@ object MockServer {
                     }
                 ],
                 "lastAuctionId" : 1
+            }
+        """.trimIndent()
+
+    private val registerAuctionResponse: String =
+        """
+            {
+                "id": 2
+            }
+        """.trimIndent()
+
+    private val auctionDetailResponse: String =
+        """
+            {
+                "auction":
+                    {
+                        "id": 2,
+                        "images": [ "https://www.apple.com/newsroom/images/product/mac/standard/Apple_MacBook-Pro_14-16-inch_10182021_big.jpg.large.jpg" ],
+                        "title": "맥북 16인치",
+                        "category":
+                            {
+                                "main": "전자기기",
+                                "sub": "노트북"
+                            },
+                        "description": "맥북 2019 16인치 팝니다. 급전이 필요해요...",
+                        "startPrice": 900000,
+                        "lastBidPrice": 100000,
+                        "status": "UNBIDDEN",
+                        "bidUnit": 3000,
+                        "registerTime": "2023-07-20T16:38:28",
+                        "closingTime": "2023-07-25T16:38:28",
+                        "directRegions": [
+                            {
+                                "first": "서울특별시",
+                                "second": "관악구",
+                                "third": "성현동"
+                            }
+                        ],
+                        "auctioneerCount": 0
+                    },
+                "seller":
+                    {
+                        "id": 1,
+                        "image": "https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/7r5X/image/9djEiPBPMLu_IvCYyvRPwmZkM1g.jpg",
+                        "nickname": "닉네임",
+                        "reliability": 5.0
+                    }
             }
         """.trimIndent()
 
