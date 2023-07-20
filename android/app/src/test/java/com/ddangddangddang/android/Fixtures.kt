77821ed660cdc5e3f62ac95d54958722ea23f711
ippnsj <49394114+ippnsj@users.noreply.github.com>
2023-07-20 19:34:45 +0900

    test: #92 경매 상품 목록 페이지 ViewModel 테스트 추가 (#95)

---
+++ android/app/src/test/java/com/ddangddangddang/android/Fixtures.kt
@@ -0,0 +1,29 @@
+package com.ddangddangddang.android
+
+import com.ddangddangddang.android.model.AuctionHomeModel
+import com.ddangddangddang.android.model.AuctionHomeStatusModel
+import com.ddangddangddang.data.model.response.AuctionPreviewResponse
+import com.ddangddangddang.data.model.response.AuctionPreviewsResponse
+
+private fun createAuctionPreviewResponse(
+    id: Long = 1,
+    title: String = "맥북 2021 13인치",
+    image: String = "https://www.apple.com/newsroom/images/product/mac/standard/Apple_MacBook-Pro_14-16-inch_10182021_big.jpg.large.jpg",
+    auctionPrice: Int = 1000000,
+    status: String = "FAILURE",
+    auctioneerCount: Int = 0,
+) = AuctionPreviewResponse(id, title, image, auctionPrice, status, auctioneerCount)
+
+fun createAuctionPreviewsResponse(
+    auctions: List<AuctionPreviewResponse> = listOf(createAuctionPreviewResponse()),
+    lastAuctionId: Long = 1,
+) = AuctionPreviewsResponse(auctions, lastAuctionId)
+
+fun createAuctionHomeModel(
+    id: Long = 1,
+    title: String = "맥북 2021 13인치",
+    image: String = "https://www.apple.com/newsroom/images/product/mac/standard/Apple_MacBook-Pro_14-16-inch_10182021_big.jpg.large.jpg",
+    auctionPrice: Int = 1000000,
+    status: AuctionHomeStatusModel = AuctionHomeStatusModel.FAILURE,
+    auctioneerCount: Int = 0,
+) = AuctionHomeModel(id, title, image, auctionPrice, status, auctioneerCount)
