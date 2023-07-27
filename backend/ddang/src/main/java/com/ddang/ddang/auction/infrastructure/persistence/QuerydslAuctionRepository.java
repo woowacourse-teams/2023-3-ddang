4cd6c3dbb12d8eae8b471cf33eef26135d6650c8
apptie <appti@naver.com>
2023-07-27 16:13:55 +0900

    feat: Auction과 AuctionRegion을 fetch join하는 기능 추가

--- backend/ddang/src/main/java/com/ddang/ddang/auction/infrastructure/persistence/QuerydslAuctionRepository.java
+++ backend/ddang/src/main/java/com/ddang/ddang/auction/infrastructure/persistence/QuerydslAuctionRepository.java
@@ -3,8 +3,11 @@ package com.ddang.ddang.auction.infrastructure.persistence;
 import com.ddang.ddang.auction.domain.Auction;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface QuerydslAuctionRepository {
 
     List<Auction> findAuctionsAllByLastAuctionId(final Long lastAuctionId, final int size);
+
+    Optional<Auction> findAuctionWithRegionsById(final Long auctionId);
 }
--- backend/ddang/src/main/java/com/ddang/ddang/auction/infrastructure/persistence/QuerydslAuctionRepositoryImpl.java
+++ backend/ddang/src/main/java/com/ddang/ddang/auction/infrastructure/persistence/QuerydslAuctionRepositoryImpl.java
@@ -1,14 +1,16 @@
 package com.ddang.ddang.auction.infrastructure.persistence;
 
+import static com.ddang.ddang.auction.domain.QAuction.auction;
+import static com.ddang.ddang.region.domain.QAuctionRegion.auctionRegion;
+import static com.ddang.ddang.region.domain.QRegion.region;
+
 import com.ddang.ddang.auction.domain.Auction;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-import static com.ddang.ddang.auction.domain.QAuction.auction;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,7 +22,7 @@ public class QuerydslAuctionRepositoryImpl implements QuerydslAuctionRepository 
     public List<Auction> findAuctionsAllByLastAuctionId(final Long lastAuctionId, final int size) {
         return queryFactory
                 .selectFrom(auction)
-                .where(auction.deleted.isFalse().and(lessThanLastAuctionId(lastAuctionId)))
+                .where(auction.deleted.isFalse(), lessThanLastAuctionId(lastAuctionId))
                 .orderBy(auction.id.desc())
                 .limit(size)
                 .fetch();
@@ -32,5 +34,19 @@ public class QuerydslAuctionRepositoryImpl implements QuerydslAuctionRepository 
         }
 
         return auction.id.lt(lastAuctionId);
+    }
+
+    @Override
+    public Optional<Auction> findAuctionWithRegionsById(final Long auctionId) {
+        final Auction findAuction = queryFactory
+                .selectFrom(auction)
+                .leftJoin(auction.auctionRegions, auctionRegion).fetchJoin()
+                .leftJoin(auctionRegion.thirdRegion, region).fetchJoin()
+                .leftJoin(region.firstRegion).fetchJoin()
+                .leftJoin(region.secondRegion).fetchJoin()
+                .where(auction.deleted.isFalse(), auction.id.eq(auctionId))
+                .fetchOne();
+
+        return Optional.ofNullable(findAuction);
     }
 }
