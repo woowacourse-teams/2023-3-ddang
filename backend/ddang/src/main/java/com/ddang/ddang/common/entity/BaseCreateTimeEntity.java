a206b8ca5bb69b7f4a4636864c96e7d1e256fea6
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-07-27 16:23:34 +0900

    refactor: 생성 시간과 수정 시간에 대한 엔티티 분리

--- backend/ddang/src/main/java/com/ddang/ddang/common/entity/BaseCreateTimeEntity.java
+++ backend/ddang/src/main/java/com/ddang/ddang/common/entity/BaseCreateTimeEntity.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -12,9 +12,9 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
-public class BaseTimeEntity extends BaseCreateTimeEntity {
+public class BaseCreateTimeEntity {
 
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime lastModifiedTime;
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdTime;
 }
