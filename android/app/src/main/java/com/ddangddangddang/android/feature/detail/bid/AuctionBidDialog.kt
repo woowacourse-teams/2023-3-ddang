635d494ac1cd683572a8e2b7e61f8fb54ce2dcf5
mendel <rhthrhrl0@naver.com>
2023-07-28 07:11:03 +0900

    refactor: 경매 입찰 팝업창 가로 너비 동적 계산 로직 제거

--- android/app/src/main/java/com/ddangddangddang/android/feature/detail/bid/AuctionBidDialog.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/detail/bid/AuctionBidDialog.kt
@@ -1,16 +1,11 @@
 package com.ddangddangddang.android.feature.detail.bid
 
-import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import android.os.Build
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowInsets
-import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import com.ddangddangddang.android.databinding.FragmentAuctionBidDialogBinding
@@ -38,30 +33,6 @@ class AuctionBidDialog : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-    }
-
-    override fun onResume() {
-        super.onResume()
-        val deviceWidth = getScreenWidth()
-
-        val params: ViewGroup.LayoutParams? = dialog?.window?.attributes
-        params?.width = (deviceWidth * 0.9).toInt()
-        dialog?.window?.attributes = params as WindowManager.LayoutParams
-    }
-
-    private fun getScreenWidth(): Int {
-        val wm = requireContext().getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            val windowMetrics = wm.currentWindowMetrics
-            val insets = windowMetrics.windowInsets
-                .getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
-            windowMetrics.bounds.width() - insets.left - insets.right
-        } else {
-            val displayMetrics = DisplayMetrics()
-            @Suppress("DEPRECATION")
-            wm.defaultDisplay.getMetrics(displayMetrics)
-            displayMetrics.widthPixels
-        }
     }
 
     override fun onDestroyView() {
