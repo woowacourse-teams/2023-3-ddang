908b0bda880891bb208e1254f462462020c1bab3
apptie <appti@naver.com>
2023-07-27 18:13:20 +0900

    feat: AuctionRegion의 테이블 명을 명시적으로 지정

--- backend/ddang/src/main/java/com/ddang/ddang/region/domain/AuctionRegion.java
+++ backend/ddang/src/main/java/com/ddang/ddang/region/domain/AuctionRegion.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -16,6 +17,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
+@Table(name = "auction_region")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @EqualsAndHashCode(of = "id")
