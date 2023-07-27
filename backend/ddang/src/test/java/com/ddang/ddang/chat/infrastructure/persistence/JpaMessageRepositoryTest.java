685d7ad7da19f9d52deea82af51fa24a02cc5912
kwonyj1022 <kwonyj1022@naver.com>
2023-07-27 16:40:06 +0900

    test: 불필요한 테스트 삭제
    Co-authored-by: swonny <wonny921@gmail.com>

--- backend/ddang/src/test/java/com/ddang/ddang/chat/infrastructure/persistence/JpaMessageRepositoryTest.java
+++ backend/ddang/src/test/java/com/ddang/ddang/chat/infrastructure/persistence/JpaMessageRepositoryTest.java
@@ -10,15 +10,12 @@ import com.ddang.ddang.user.domain.User;
 import com.ddang.ddang.user.infrastructure.persistence.JpaUserRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -69,43 +66,5 @@ class JpaMessageRepositoryTest {
         em.clear();
 
         assertThat(message.getId()).isPositive();
-    }
-
-    @Test
-    void 지정한_아이디에_대한_메시지를_조회한다() {
-        // given
-        final User seller = new User("판매자", "이미지", 5.0);
-        final User buyer = new User("구매자", "이미지", 5.0);
-        final Auction auction = Auction.builder()
-                                       .title("title")
-                                       .build();
-
-        userRepository.save(seller);
-        userRepository.save(buyer);
-        auctionRepository.save(auction);
-
-        final ChatRoom chatRoom = new ChatRoom(auction, seller, buyer);
-
-        chatRoomRepository.save(chatRoom);
-
-        final Message expected = new Message(chatRoom, seller, buyer, "안녕하세요.");
-
-        messageRepository.save(expected);
-
-        em.flush();
-        em.clear();
-
-        // when
-        final Optional<Message> actual = messageRepository.findById(expected.getId());
-
-        // then
-        SoftAssertions.assertSoftly(softAssertions -> {
-            softAssertions.assertThat(actual).isPresent();
-            softAssertions.assertThat(actual.get().getId()).isEqualTo(expected.getId());
-            softAssertions.assertThat(actual.get().getChatRoom()).isEqualTo(expected.getChatRoom());
-            softAssertions.assertThat(actual.get().getWriter()).isEqualTo(expected.getWriter());
-            softAssertions.assertThat(actual.get().getReceiver()).isEqualTo(expected.getReceiver());
-            softAssertions.assertThat(actual.get().getContents()).isEqualTo(expected.getContents());
-        });
     }
 }
