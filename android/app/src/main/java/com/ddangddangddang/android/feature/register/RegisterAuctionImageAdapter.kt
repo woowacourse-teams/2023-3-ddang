52e51d0cd513b6c7304849ca7d01091c3c581c18
ippnsj <ippnsj@gmail.com>
2023-07-28 17:43:16 +0900

    feat: 경매 등록 시 갤러리에서 사진 선택하는 기능 구현

--- android/app/src/main/java/com/ddangddangddang/android/feature/register/RegisterAuctionImageAdapter.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/register/RegisterAuctionImageAdapter.kt
@@ -3,9 +3,10 @@ package com.ddangddangddang.android.feature.register
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import com.ddangddangddang.android.model.RegisterImageModel
 
-class RegisterAuctionImageAdapter(private val onDeleteImageListener: (String) -> Unit) :
-    ListAdapter<String, RegisterAuctionImageViewHolder>(RegisterAuctionImageDiffUtil) {
+class RegisterAuctionImageAdapter(private val onDeleteImageListener: (RegisterImageModel) -> Unit) :
+    ListAdapter<RegisterImageModel, RegisterAuctionImageViewHolder>(RegisterAuctionImageDiffUtil) {
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -18,25 +19,26 @@ class RegisterAuctionImageAdapter(private val onDeleteImageListener: (String) ->
         holder.bind(currentList[position])
     }
 
-    fun setImages(images: List<String>) {
+    fun setImages(images: List<RegisterImageModel>) {
         submitList(images)
     }
 
     companion object {
-        private val RegisterAuctionImageDiffUtil = object : DiffUtil.ItemCallback<String>() {
-            override fun areItemsTheSame(
-                oldItem: String,
-                newItem: String,
-            ): Boolean {
-                return oldItem == newItem
-            }
+        private val RegisterAuctionImageDiffUtil =
+            object : DiffUtil.ItemCallback<RegisterImageModel>() {
+                override fun areItemsTheSame(
+                    oldItem: RegisterImageModel,
+                    newItem: RegisterImageModel,
+                ): Boolean {
+                    return oldItem.id == newItem.id
+                }
 
-            override fun areContentsTheSame(
-                oldItem: String,
-                newItem: String,
-            ): Boolean {
-                return oldItem == newItem
+                override fun areContentsTheSame(
+                    oldItem: RegisterImageModel,
+                    newItem: RegisterImageModel,
+                ): Boolean {
+                    return oldItem == newItem
+                }
             }
-        }
     }
 }
