8df177669dd340c1126ebe410b96f0fefbdd846c
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-07-28 12:58:34 +0900

    feat: 입찰 등록 서비스 추가

--- backend/ddang/src/main/java/com/ddang/ddang/bid/infrastructure/persistence/QuerydslBidRepositoryImpl.java
+++ backend/ddang/src/main/java/com/ddang/ddang/bid/infrastructure/persistence/QuerydslBidRepositoryImpl.java
@@ -13,7 +13,7 @@ public class QuerydslBidRepositoryImpl implements QuerydslBidRepository {
 
     private final JPAQueryFactory queryFactory;
 
-    // TODO: 2023/07/28 orderBy를 id와 createdTime 중 무엇으로 하는 것이 좋은가? (acution은 id임)
+    // TODO: 2023/07/28 (고민) orderBy를 id와 createdTime 중 무엇으로 하는 것이 좋은가? (acution은 id임)
     @Override
     public Bid findLastBidByAuctionId(final Long id) {
         return queryFactory
