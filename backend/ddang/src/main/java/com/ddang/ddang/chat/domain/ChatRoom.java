bcb8bf95b9e4bc67746f3bd7595c93cc67198460
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2023-07-28 13:59:57 +0900

    feat: #118 채팅 기능 구현에 필요한 엔티티 생성 (#122)
    * feat: 채팅 기능 구현에 필요한 엔티티 생성
    
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * refactor: 채팅 가능 여부 체크 메서드 파라미터 추가
    
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * feat: 채팅방 레포지토리 추가
    
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * feat: 메시지 레포지토리 추가
    
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * refactor: `@ToString` 메서드 옵션 추가
    
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * style: 마지막 줄 개행 추가
    
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * test: 불필요한 테스트 삭제
    
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * refactor: column 옵션 추가
    
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * refactor: Builder 추가
    
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * test: 테스트 어노테이션 변경
    
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * feat: 메시지 읽음 필드 추가
    
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * refactor: 메시지 읽음 필드 삭제
    
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * refactor: 수정 시간 미포함된 엔티티 상속으로 변경
    
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * refactor: 중복 필드 삭제
    
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * refactor: 불필요한 어노테이션 옵션 삭제
    
    Co-authored-by: swonny <wonny921@gmail.com>
    
    ---------
    
    Co-authored-by: swonny <wonny921@gmail.com>

---
+++ backend/ddang/src/main/java/com/ddang/ddang/chat/domain/ChatRoom.java
@@ -0,0 +1,55 @@
+package com.ddang.ddang.chat.domain;
+
+import com.ddang.ddang.auction.domain.Auction;
+import com.ddang.ddang.common.entity.BaseCreateTimeEntity;
+import com.ddang.ddang.user.domain.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EqualsAndHashCode(of = "id")
+@ToString(of = "id")
+public class ChatRoom extends BaseCreateTimeEntity {
+
+    public static final long CHAT_EXPIRATION_DAY = 10L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "auction_id", nullable = false)
+    private Auction auction;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "buyer_id", nullable = false, foreignKey = @ForeignKey(name = "fk_chat_room_buyer"))
+    private User buyer;
+
+    public ChatRoom(final Auction auction, final User buyer) {
+        this.auction = auction;
+        this.buyer = buyer;
+    }
+
+    // TODO : 쪽지 비활성화 일수 리팩토링시 수정
+    public boolean isChatAvailable(final LocalDateTime targetTime) {
+        final LocalDateTime maxChatTime = getCreatedTime().plusDays(CHAT_EXPIRATION_DAY);
+
+        return targetTime.isBefore(maxChatTime);
+    }
+}
---
+++ backend/ddang/src/test/java/com/ddang/ddang/chat/infrastructure/persistence/JpaMessageRepositoryTest.java
@@ -0,0 +1,75 @@
+package com.ddang.ddang.chat.infrastructure.persistence;
+
+import com.ddang.ddang.auction.domain.Auction;
+import com.ddang.ddang.auction.infrastructure.persistence.JpaAuctionRepository;
+import com.ddang.ddang.chat.domain.ChatRoom;
+import com.ddang.ddang.chat.domain.Message;
+import com.ddang.ddang.configuration.JpaConfiguration;
+import com.ddang.ddang.configuration.QuerydslConfiguration;
+import com.ddang.ddang.user.domain.User;
+import com.ddang.ddang.user.infrastructure.persistence.JpaUserRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+@Import({JpaConfiguration.class, QuerydslConfiguration.class})
+class JpaMessageRepositoryTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired
+    JpaAuctionRepository auctionRepository;
+
+    @Autowired
+    JpaUserRepository userRepository;
+
+    @Autowired
+    JpaChatRoomRepository chatRoomRepository;
+
+    @Autowired
+    JpaMessageRepository messageRepository;
+
+    @Test
+    void 메시지를_저장한다() {
+        // given
+        final User participant1 = new User("판매자", "이미지", 5.0);
+        final User participant2 = new User("구매자", "이미지", 5.0);
+        final Auction auction = Auction.builder()
+                                       .title("title")
+                                       .build();
+
+        userRepository.save(participant1);
+        userRepository.save(participant2);
+        auctionRepository.save(auction);
+
+        final ChatRoom chatRoom = new ChatRoom(auction, participant2);
+        chatRoomRepository.save(chatRoom);
+
+        final Message message = Message.builder()
+                                       .chatRoom(chatRoom)
+                                       .writer(participant1)
+                                       .receiver(participant2)
+                                       .contents("안녕하세요")
+                                       .build();
+
+        // when
+        messageRepository.save(message);
+
+        // then
+        em.flush();
+        em.clear();
+
+        assertThat(message.getId()).isPositive();
+    }
+}
