df9b47a2de1ed9e080a834e1e911236fe6985a35
mendel <rhthrhrl0@naver.com>
2023-07-28 12:43:11 +0900

    refactor: binding 객체 접근 제어자 수정

--- android/app/src/main/java/com/ddangddangddang/android/feature/detail/bid/AuctionBidDialog.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/detail/bid/AuctionBidDialog.kt
@@ -14,7 +14,7 @@ import com.ddangddangddang.android.feature.detail.AuctionDetailViewModel
 
 class AuctionBidDialog : DialogFragment() {
     private var _binding: FragmentAuctionBidDialogBinding? = null
-    val binding: FragmentAuctionBidDialogBinding
+    private val binding: FragmentAuctionBidDialogBinding
         get() = _binding!!
 
     private val activityViewModel: AuctionDetailViewModel by activityViewModels { viewModelFactory }
