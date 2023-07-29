3171363301627b879674b07f8e80ae61c8aa2f94
hyemdooly <hyemdooly@gmail.com>
2023-07-29 19:27:03 +0900

    feat: ListAdapter, DiffUtil 적용하여 상태에 따른 Item 디자인 변경 구현

--- android/app/src/main/java/com/ddangddangddang/android/feature/register/category/SelectCategoryActivity.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/register/category/SelectCategoryActivity.kt
@@ -1,6 +1,7 @@
 package com.ddangddangddang.android.feature.register.category
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,29 +13,38 @@ import com.ddangddangddang.android.util.binding.BindingActivity
 class SelectCategoryActivity :
     BindingActivity<ActivitySelectCategoryBinding>(R.layout.activity_select_category) {
     private val viewModel by viewModels<SelectCategoryViewModel> { viewModelFactory }
+    private val mainAdapter by lazy {
+        MainCategoryAdapter { id ->
+            viewModel.setMainCategorySelection(id)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val dividerItemDecoration = DividerItemDecoration(this, LinearLayoutManager.VERTICAL)
         binding.rvMainCategory.addItemDecoration(dividerItemDecoration)
-        binding.rvMainCategory.adapter =
-            MainCategoryAdapter(
-                viewModel.mainCategories,
-            ) { id ->
-                viewModel.setMainCategorySelection(id)
-                binding.rvMainCategory.adapter?.notifyDataSetChanged()
-            }
+
+        // Adapter setting
+        // Main
+        binding.rvMainCategory.adapter = mainAdapter
+        mainAdapter.setCategories(viewModel.mainCategories)
+
+        // Sub
         binding.rvSubCategory.adapter =
             SubCategoryAdapter(listOf("컴퓨터", "컴퓨터", "컴퓨터", "컴퓨터", "컴퓨터", "컴퓨터"))
 
+        // Observe
         viewModel.event.observe(this) {
             handleEvent(it)
+            Log.d("test", it.toString())
         }
     }
 
     private fun handleEvent(event: SelectCategoryViewModel.SelectCategoryEvent) {
         when (event) {
             is SelectCategoryViewModel.SelectCategoryEvent.Exit -> finish()
+            is SelectCategoryViewModel.SelectCategoryEvent.MainCategoriesChanged -> mainAdapter.setCategories(viewModel.mainCategories)
         }
     }
 }
--- android/app/src/main/java/com/ddangddangddang/android/feature/register/category/SelectCategoryViewModel.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/register/category/SelectCategoryViewModel.kt
@@ -1,8 +1,6 @@
 package com.ddangddangddang.android.feature.register.category
 
-import android.util.Log
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ddangddangddang.android.model.CategoriesModel
 import com.ddangddangddang.android.util.livedata.SingleLiveEvent
@@ -12,36 +10,35 @@ class SelectCategoryViewModel : ViewModel() {
     val event: LiveData<SelectCategoryEvent>
         get() = _event
 
-    private var mainCategorySelection: Long = 0 // id
-    private var subCategorySelection: Long? = null
-
-    private val _mainCategories = MutableLiveData<List<CategoriesModel.CategoryModel>>(
-        listOf(
-            CategoriesModel.CategoryModel("가전 제품1", 0, true),
-            CategoriesModel.CategoryModel("가전 제품2", 1, false),
-            CategoriesModel.CategoryModel("가전 제품3", 2, false),
-            CategoriesModel.CategoryModel("가전 제품4", 3, false),
-            CategoriesModel.CategoryModel("가전 제품5", 4, false),
-        ),
+    private val _mainCategories = mutableListOf(
+        CategoriesModel.CategoryModel("가전 제품1", 0, true),
+        CategoriesModel.CategoryModel("가전 제품2", 1, false),
+        CategoriesModel.CategoryModel("가전 제품3", 2, false),
+        CategoriesModel.CategoryModel("가전 제품4", 3, false),
+        CategoriesModel.CategoryModel("가전 제품5", 4, false),
     )
-    val mainCategories: LiveData<List<CategoriesModel.CategoryModel>>
-        get() = _mainCategories
+    val mainCategories: List<CategoriesModel.CategoryModel>
+        get() = _mainCategories.toList() // DiffUtil은 리스트의 주소가 다르지 않으면 Difference를 검사 안함
 
     fun setExitEvent() {
         _event.value = SelectCategoryEvent.Exit
     }
 
     fun setMainCategorySelection(mainCategoryId: Long) {
-        val temp = _mainCategories.value?.toMutableList()
-        temp?.let {
-            val index = temp.indexOfFirst { it.id == mainCategoryId }
-            temp[index] = temp[index].copy(isChecked = true)
-            _mainCategories.value = temp.toList()
+        _mainCategories.replaceAll {
+            if (it.id == mainCategoryId) {
+                it.copy(isChecked = true)
+            } else if (it.isChecked) {
+                it.copy(isChecked = false)
+            } else {
+                it
+            }
         }
-        Log.d("test", mainCategories.value.toString())
+        _event.value = SelectCategoryEvent.MainCategoriesChanged
     }
 
     sealed class SelectCategoryEvent {
         object Exit : SelectCategoryEvent()
+        object MainCategoriesChanged : SelectCategoryEvent()
     }
 }
