083ec373efbd05d143c2d20c72338e8100746d0d
apptie <appti@naver.com>
2023-07-29 13:19:13 +0900

    feat: No-Offset 조회 결과를 Slice로 변환하는 기능 추가

---
+++ backend/ddang/src/main/java/com/ddang/ddang/configuration/QuerydslSliceHelper.java
@@ -0,0 +1,29 @@
+package com.ddang.ddang.configuration;
+
+import java.util.List;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+
+public class QuerydslSliceHelper {
+
+    public static <T> Slice<T> toSlice(List<T> contents, int size) {
+        final boolean hasNext = isContentSizeGreaterThanPageSize(contents, size);
+        final Pageable pageable = PageRequest.ofSize(size);
+
+        if (hasNext) {
+            return new SliceImpl<>(subListLastContent(contents, size), pageable, hasNext);
+        }
+
+        return new SliceImpl<>(contents, pageable, hasNext);
+    }
+
+    private static <T> boolean isContentSizeGreaterThanPageSize(List<T> content, int size) {
+        return content.size() > size;
+    }
+
+    private static <T> List<T> subListLastContent(List<T> content, int size) {
+        return content.subList(0, size);
+    }
+}
