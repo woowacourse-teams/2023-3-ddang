4e1a3ed1ebc2cc6bec4fcadd6d8ae6d583e23839
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-07-30 23:26:25 +0900

    feat: 요청에 대한 예외 처리 추가

--- backend/ddang/src/main/java/com/ddang/ddang/bid/presentation/dto/CreateBidRequest.java
+++ backend/ddang/src/main/java/com/ddang/ddang/bid/presentation/dto/CreateBidRequest.java
@@ -1,4 +1,13 @@
 package com.ddang.ddang.bid.presentation.dto;
 
-public record CreateBidRequest(Long auctionId, int bidPrice) {
+import jakarta.validation.constraints.NotNull;
+
+public record CreateBidRequest(
+        // TODO: 2023/07/30  아이디에 대한 notnull 오류 메시지가 있어야 할까?
+        @NotNull(message = "경매 아이디가 입력되지 않았습니다.")
+        Long auctionId,
+
+        @NotNull(message = "입찰 금액이 입력되지 않았습니다.")
+        int bidPrice
+) {
 }
