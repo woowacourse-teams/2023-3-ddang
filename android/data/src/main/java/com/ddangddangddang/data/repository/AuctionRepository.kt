cff1804b79fa65b5864c8145397bf31bb8bcd0d8
ippnsj <49394114+ippnsj@users.noreply.github.com>
2023-08-01 16:26:06 +0900

    feat: #175 경매 등록 시 갤러리에 있는 이미지를 서버로 전송하는 기능 구현 (#182)
    * feat: 경매 등록 시 이미지 파일을 서버로 전송하는 기능 구현
    
    * feat: 마지막 경매 아이템까지 로드한 경우 더 이상 로드하지 않는 코드 추가

--- android/data/src/main/java/com/ddangddangddang/data/repository/AuctionRepository.kt
+++ android/data/src/main/java/com/ddangddangddang/data/repository/AuctionRepository.kt
@@ -5,14 +5,20 @@ import com.ddangddangddang.data.model.request.RegisterAuctionRequest
 import com.ddangddangddang.data.model.response.AuctionDetailResponse
 import com.ddangddangddang.data.model.response.AuctionPreviewResponse
 import com.ddangddangddang.data.model.response.AuctionPreviewsResponse
-import com.ddangddangddang.data.model.response.RegisterAuctionResponse
 import com.ddangddangddang.data.remote.ApiResponse
 import java.io.File
 
 interface AuctionRepository {
     fun observeAuctionPreviews(): LiveData<List<AuctionPreviewResponse>>
 
-    suspend fun getAuctionPreviews(lastAuctionId: Long?, size: Int): ApiResponse<AuctionPreviewsResponse>
+    suspend fun getAuctionPreviews(
+        lastAuctionId: Long?,
+        size: Int,
+    ): ApiResponse<AuctionPreviewsResponse>
+
     suspend fun getAuctionDetail(id: Long): ApiResponse<AuctionDetailResponse>
-    suspend fun registerAuction(images: List<File>, auction: RegisterAuctionRequest): ApiResponse<RegisterAuctionResponse>
+    suspend fun registerAuction(
+        images: List<File>,
+        auction: RegisterAuctionRequest,
+    ): ApiResponse<AuctionPreviewResponse>
 }
