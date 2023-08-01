a41658f340e79d4f8c77e28870d6ff0b2d9aaee3
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-08-01 12:34:29 +0900

    refactor: toString에 대해 exclude 대신 of로 수정

--- backend/ddang/src/main/java/com/ddang/ddang/bid/domain/Bid.java
+++ backend/ddang/src/main/java/com/ddang/ddang/bid/domain/Bid.java
@@ -25,7 +25,7 @@ import lombok.ToString;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @EqualsAndHashCode(of = "id")
-@ToString(exclude = {"auction", "bidder"})
+@ToString(of = {"id", "price"})
 public class Bid extends BaseCreateTimeEntity {
 
     @Id
