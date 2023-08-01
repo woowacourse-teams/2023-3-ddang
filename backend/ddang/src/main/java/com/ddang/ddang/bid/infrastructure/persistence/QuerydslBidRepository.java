35881dba79e75680d559078c193798fd62896745
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-08-01 16:01:57 +0900

    refactor: querydsl 형식을 jpql로 변환

--- backend/ddang/src/main/java/com/ddang/ddang/bid/infrastructure/persistence/QuerydslBidRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ddang.ddang.bid.infrastructure.persistence;
-
-import com.ddang.ddang.bid.domain.Bid;
-
-public interface QuerydslBidRepository {
-
-    Bid findLastBidByAuctionId(final Long id);
-}
