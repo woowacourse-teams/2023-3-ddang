31ff5d4cfd3a32a3da81ce61bf1fa22057ce9f41
apptie <appti@naver.com>
2023-08-01 09:49:06 +0900

    refactor: 직거래 지역 조회 api 반환 타입 수정

--- backend/ddang/src/main/java/com/ddang/ddang/region/presentation/RegionController.java
+++ backend/ddang/src/main/java/com/ddang/ddang/region/presentation/RegionController.java
@@ -3,15 +3,13 @@ package com.ddang.ddang.region.presentation;
 import com.ddang.ddang.region.application.RegionService;
 import com.ddang.ddang.region.application.dto.ReadRegionDto;
 import com.ddang.ddang.region.presentation.dto.response.ReadRegionResponse;
-import com.ddang.ddang.region.presentation.dto.response.ReadRegionsResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/regions")
@@ -21,29 +19,27 @@ public class RegionController {
     private final RegionService regionService;
 
     @GetMapping
-    public ResponseEntity<ReadRegionsResponse> readAllFirst() {
+    public ResponseEntity<List<ReadRegionResponse>> readAllFirst() {
         final List<ReadRegionDto> readRegionDtos = regionService.readAllFirst();
         final List<ReadRegionResponse> readRegionResponses = readRegionDtos.stream()
                                                                            .map(ReadRegionResponse::from)
                                                                            .toList();
-        final ReadRegionsResponse response = new ReadRegionsResponse(readRegionResponses);
 
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(readRegionResponses);
     }
 
     @GetMapping("/{firstId}")
-    public ResponseEntity<ReadRegionsResponse> readAllSecond(@PathVariable final Long firstId) {
+    public ResponseEntity<List<ReadRegionResponse>> readAllSecond(@PathVariable final Long firstId) {
         final List<ReadRegionDto> readRegionDtos = regionService.readAllSecondByFirstRegionId(firstId);
         final List<ReadRegionResponse> readRegionResponses = readRegionDtos.stream()
                                                                            .map(ReadRegionResponse::from)
                                                                            .toList();
-        final ReadRegionsResponse response = new ReadRegionsResponse(readRegionResponses);
 
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(readRegionResponses);
     }
 
     @GetMapping("/{firstId}/{secondId}")
-    public ResponseEntity<ReadRegionsResponse> readAllSecond(
+    public ResponseEntity<List<ReadRegionResponse>> readAllSecond(
             @PathVariable final Long firstId,
             @PathVariable final Long secondId
     ) {
@@ -54,8 +50,7 @@ public class RegionController {
         final List<ReadRegionResponse> readRegionResponses = readRegionDtos.stream()
                                                                            .map(ReadRegionResponse::from)
                                                                            .toList();
-        final ReadRegionsResponse response = new ReadRegionsResponse(readRegionResponses);
 
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(readRegionResponses);
     }
 }
