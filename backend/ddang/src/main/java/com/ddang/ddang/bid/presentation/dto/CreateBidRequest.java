889a3c27315b880dbd1e9a7bcace7b1c3ee30787
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-08-01 10:14:54 +0900

    refactor: 요청 dto 검증 조건 추가

--- backend/ddang/src/main/java/com/ddang/ddang/bid/presentation/dto/CreateBidRequest.java
+++ backend/ddang/src/main/java/com/ddang/ddang/bid/presentation/dto/CreateBidRequest.java
@@ -1,13 +1,15 @@
 package com.ddang.ddang.bid.presentation.dto;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record CreateBidRequest(
-        // TODO: 2023/07/30  아이디에 대한 notnull 오류 메시지가 있어야 할까?
         @NotNull(message = "경매 아이디가 입력되지 않았습니다.")
+        @Positive
         Long auctionId,
 
         @NotNull(message = "입찰 금액이 입력되지 않았습니다.")
+        @Positive
         int bidPrice
 ) {
 }
