311593f9af65571dd281472484bbe38be1494f20
kwonyj1022 <kwonyj1022@naver.com>
2023-08-03 01:13:09 +0900

    refactor: 파라미터에 final 키워드 추가

--- backend/ddang/src/main/java/com/ddang/ddang/chat/presentation/dto/response/ReadAuctionResponse.java
+++ backend/ddang/src/main/java/com/ddang/ddang/chat/presentation/dto/response/ReadAuctionResponse.java
@@ -4,7 +4,7 @@ import com.ddang.ddang.auction.application.dto.ReadAuctionDto;
 
 public record ReadAuctionResponse(Long id, String title, String image, int price) {
 
-    public static ReadAuctionResponse of(ReadAuctionDto dto, String baseUrl) {
+    public static ReadAuctionResponse of(final ReadAuctionDto dto, final String baseUrl) {
         final Long thumbNailImageId = dto.auctionImageIds().get(0);
         final String imageUrl = baseUrl.concat(String.valueOf(thumbNailImageId));
         return new ReadAuctionResponse(dto.id(), dto.title(), imageUrl, dto.lastBidPrice());
--- backend/ddang/src/main/java/com/ddang/ddang/chat/presentation/dto/response/ReadChatPartnerResponse.java
+++ backend/ddang/src/main/java/com/ddang/ddang/chat/presentation/dto/response/ReadChatPartnerResponse.java
@@ -3,7 +3,7 @@ package com.ddang.ddang.chat.presentation.dto.response;
 import com.ddang.ddang.chat.application.dto.ReadUserDto;
 
 public record ReadChatPartnerResponse(Long id, String name, String profileImage) {
-    public static ReadChatPartnerResponse from(ReadUserDto dto) {
+    public static ReadChatPartnerResponse from(final ReadUserDto dto) {
         return new ReadChatPartnerResponse(dto.id(), dto.name(), dto.profileImage());
     }
 }
