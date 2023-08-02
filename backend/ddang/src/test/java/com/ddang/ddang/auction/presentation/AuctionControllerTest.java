45e05622fbef63e765992d60b0f84795d41c5a82
apptie <appti@naver.com>
2023-08-02 21:33:22 +0900

    test: AuctionController 예외 핸들링 테스트 추가

--- backend/ddang/src/test/java/com/ddang/ddang/auction/presentation/AuctionControllerTest.java
+++ backend/ddang/src/test/java/com/ddang/ddang/auction/presentation/AuctionControllerTest.java
@@ -6,6 +6,7 @@ import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.BDDMockito.willThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
@@ -21,8 +22,15 @@ import com.ddang.ddang.auction.application.dto.ReadAuctionDto;
 import com.ddang.ddang.auction.application.dto.ReadAuctionsDto;
 import com.ddang.ddang.auction.application.dto.ReadRegionDto;
 import com.ddang.ddang.auction.application.dto.ReadRegionsDto;
+import com.ddang.ddang.auction.application.exception.AuctionNotFoundException;
 import com.ddang.ddang.auction.presentation.dto.request.CreateAuctionRequest;
+import com.ddang.ddang.bid.application.exception.UserNotFoundException;
+import com.ddang.ddang.category.application.exception.CategoryNotFoundException;
 import com.ddang.ddang.exception.GlobalExceptionHandler;
+import com.ddang.ddang.image.infrastructure.local.exception.EmptyImageException;
+import com.ddang.ddang.image.infrastructure.local.exception.StoreImageFailureException;
+import com.ddang.ddang.image.infrastructure.local.exception.UnsupportedImageFileExtensionException;
+import com.ddang.ddang.region.application.exception.RegionNotFoundException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -81,16 +89,17 @@ class AuctionControllerTest {
                 2L,
                 List.of(3L)
         );
-        final CreateInfoAuctionDto createInfoAuctionDto = new CreateInfoAuctionDto(
-                1L,
-                "title",
-                1L,
-                1_000);
         final MockMultipartFile request = new MockMultipartFile(
                 "request",
                 "request",
                 MediaType.APPLICATION_JSON_VALUE,
                 objectMapper.writeValueAsBytes(createAuctionRequest)
+        );
+        final CreateInfoAuctionDto createInfoAuctionDto = new CreateInfoAuctionDto(
+                1L,
+                "title",
+                1L,
+                1_000
         );
 
         given(auctionService.create(any(CreateAuctionDto.class))).willReturn(createInfoAuctionDto);
@@ -105,6 +114,250 @@ class AuctionControllerTest {
                        status().isCreated(),
                        header().string(HttpHeaders.LOCATION, is("/auctions/1")),
                        jsonPath("$.id", is(1L), Long.class)
+               );
+    }
+
+    @Test
+    void 경매_등록시_유효한_회원이_아니라면_404을_반환한다() throws Exception {
+        // given
+        final MockMultipartFile auctionImage = new MockMultipartFile(
+                "images",
+                "image.png",
+                MediaType.IMAGE_PNG_VALUE,
+                new byte[]{1}
+        );
+        final CreateAuctionRequest createAuctionRequest = new CreateAuctionRequest(
+                "경매 상품 1",
+                "이것은 경매 상품 1 입니다.",
+                1_000,
+                1_000,
+                LocalDateTime.now().plusDays(3L),
+                2L,
+                List.of(3L)
+        );
+        final MockMultipartFile request = new MockMultipartFile(
+                "request",
+                "request",
+                MediaType.APPLICATION_JSON_VALUE,
+                objectMapper.writeValueAsBytes(createAuctionRequest)
+        );
+
+        given(auctionService.create(any())).willThrow(new UserNotFoundException("지정한 판매자를 찾을 수 없습니다."));
+
+        // when & then
+        mockMvc.perform(multipart("/auctions")
+                       .file(auctionImage)
+                       .file(request)
+                       .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+               )
+               .andExpectAll(
+                       status().isNotFound(),
+                       jsonPath("$.message").exists()
+               );
+    }
+
+    @Test
+    void 경매_등록시_유효한_하위_카테고리가_아니라면_400을_반환한다() throws Exception {
+        // given
+        final MockMultipartFile auctionImage = new MockMultipartFile(
+                "images",
+                "image.png",
+                MediaType.IMAGE_PNG_VALUE,
+                new byte[]{1}
+        );
+        final CreateAuctionRequest createAuctionRequest = new CreateAuctionRequest(
+                "경매 상품 1",
+                "이것은 경매 상품 1 입니다.",
+                1_000,
+                1_000,
+                LocalDateTime.now().plusDays(3L),
+                2L,
+                List.of(3L)
+        );
+        final MockMultipartFile request = new MockMultipartFile(
+                "request",
+                "request",
+                MediaType.APPLICATION_JSON_VALUE,
+                objectMapper.writeValueAsBytes(createAuctionRequest)
+        );
+
+        given(auctionService.create(any())).willThrow(
+                new CategoryNotFoundException("지정한 판매자를 찾을 수 없습니다.")
+        );
+
+        // when & then
+        mockMvc.perform(multipart("/auctions")
+                       .file(auctionImage)
+                       .file(request)
+                       .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+               )
+               .andExpectAll(
+                       status().isNotFound(),
+                       jsonPath("$.message").exists()
+               );
+    }
+
+    @Test
+    void 경매_등록시_유효한_세번째_지역이_아니라면_404을_반환한다() throws Exception {
+        // given
+        final MockMultipartFile auctionImage = new MockMultipartFile(
+                "images",
+                "image.png",
+                MediaType.IMAGE_PNG_VALUE,
+                new byte[]{1}
+        );
+        final CreateAuctionRequest createAuctionRequest = new CreateAuctionRequest(
+                "경매 상품 1",
+                "이것은 경매 상품 1 입니다.",
+                1_000,
+                1_000,
+                LocalDateTime.now().plusDays(3L),
+                2L,
+                List.of(3L)
+        );
+        final MockMultipartFile request = new MockMultipartFile(
+                "request",
+                "request",
+                MediaType.APPLICATION_JSON_VALUE,
+                objectMapper.writeValueAsBytes(createAuctionRequest)
+        );
+
+        given(auctionService.create(any())).willThrow(
+                new RegionNotFoundException("지정한 세 번째 지역이 없거나 세 번째 지역이 아닙니다.")
+        );
+
+        // when & then
+        mockMvc.perform(multipart("/auctions")
+                       .file(auctionImage)
+                       .file(request)
+                       .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+               )
+               .andExpectAll(
+                       status().isNotFound(),
+                       jsonPath("$.message").exists()
+               );
+    }
+
+    @Test
+    void 경매_등록시_이미지_파일의_데이터가_비어있다면_400을_반환한다() throws Exception {
+        // given
+        final MockMultipartFile auctionImage = new MockMultipartFile(
+                "images",
+                "image.png",
+                MediaType.IMAGE_PNG_VALUE,
+                new byte[]{1}
+        );
+        final CreateAuctionRequest createAuctionRequest = new CreateAuctionRequest(
+                "경매 상품 1",
+                "이것은 경매 상품 1 입니다.",
+                1_000,
+                1_000,
+                LocalDateTime.now().plusDays(3L),
+                2L,
+                List.of(3L)
+        );
+        final MockMultipartFile request = new MockMultipartFile(
+                "request",
+                "request",
+                MediaType.APPLICATION_JSON_VALUE,
+                objectMapper.writeValueAsBytes(createAuctionRequest)
+        );
+
+        given(auctionService.create(any())).willThrow(
+                new EmptyImageException("이미지 파일의 데이터가 비어 있습니다.")
+        );
+
+        // when & then
+        mockMvc.perform(multipart("/auctions")
+                       .file(auctionImage)
+                       .file(request)
+                       .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+               )
+               .andExpectAll(
+                       status().isBadRequest(),
+                       jsonPath("$.message").exists()
+               );
+    }
+
+    @Test
+    void 경매_등록시_이미지_저장에_실패하면_500을_반환한다() throws Exception {
+        // given
+        final MockMultipartFile auctionImage = new MockMultipartFile(
+                "images",
+                "image.png",
+                MediaType.IMAGE_PNG_VALUE,
+                new byte[]{1}
+        );
+        final CreateAuctionRequest createAuctionRequest = new CreateAuctionRequest(
+                "경매 상품 1",
+                "이것은 경매 상품 1 입니다.",
+                1_000,
+                1_000,
+                LocalDateTime.now().plusDays(3L),
+                2L,
+                List.of(3L)
+        );
+        final MockMultipartFile request = new MockMultipartFile(
+                "request",
+                "request",
+                MediaType.APPLICATION_JSON_VALUE,
+                objectMapper.writeValueAsBytes(createAuctionRequest)
+        );
+
+        given(auctionService.create(any())).willThrow(
+                new StoreImageFailureException("이미지 저장에 실패했습니다.", null)
+        );
+
+        // when & then
+        mockMvc.perform(multipart("/auctions")
+                       .file(auctionImage)
+                       .file(request)
+                       .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+               )
+               .andExpectAll(
+                       status().isInternalServerError(),
+                       jsonPath("$.message").exists()
+               );
+    }
+
+    @Test
+    void 경매_등록시_지원하지_않는_확장자의_이미지면_400을_반환한다() throws Exception {
+        // given
+        final MockMultipartFile auctionImage = new MockMultipartFile(
+                "images",
+                "image.png",
+                MediaType.IMAGE_PNG_VALUE,
+                new byte[]{1}
+        );
+        final CreateAuctionRequest createAuctionRequest = new CreateAuctionRequest(
+                "경매 상품 1",
+                "이것은 경매 상품 1 입니다.",
+                1_000,
+                1_000,
+                LocalDateTime.now().plusDays(3L),
+                2L,
+                List.of(3L)
+        );
+        final MockMultipartFile request = new MockMultipartFile(
+                "request",
+                "request",
+                MediaType.APPLICATION_JSON_VALUE,
+                objectMapper.writeValueAsBytes(createAuctionRequest)
+        );
+
+        given(auctionService.create(any())).willThrow(
+                new UnsupportedImageFileExtensionException("지원하지 않는 확장자입니다. : ")
+        );
+
+        // when & then
+        mockMvc.perform(multipart("/auctions")
+                       .file(auctionImage)
+                       .file(request)
+                       .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+               )
+               .andExpectAll(
+                       status().isBadRequest(),
+                       jsonPath("$.message").exists()
                );
     }
 
@@ -149,6 +402,23 @@ class AuctionControllerTest {
                        jsonPath("$.auction.startPrice", is(auction.startPrice())),
                        jsonPath("$.auction.registerTime").exists(),
                        jsonPath("$.auction.closingTime").exists()
+               );
+    }
+
+    @Test
+    void 경매_조회시_지정한_아이디에_해당하는_경매가_없다면_404를_반환한다() throws Exception {
+        // given
+        final Long invalidAuctionId = -999L;
+
+        given(auctionService.readByAuctionId(anyLong())).willThrow(new AuctionNotFoundException(
+                "지정한 아이디에 대한 경매를 찾을 수 없습니다."
+        ));
+
+        // when & then
+        mockMvc.perform(get("/auctions/{auctionId}", invalidAuctionId).contentType(MediaType.APPLICATION_JSON))
+               .andExpectAll(
+                       status().isNotFound(),
+                       jsonPath("$.message").exists()
                );
     }
 
@@ -229,5 +499,38 @@ class AuctionControllerTest {
         // when & then
         mockMvc.perform(delete("/auctions/{auctionId}", 1L).contentType(MediaType.APPLICATION_JSON))
                .andExpectAll(status().isNoContent());
+    }
+
+    @Test
+    void 경매_삭제시_지정한_아이디에_해당하는_경매가_없다면_404를_반환한다() throws Exception {
+        // given
+        final Long invalidAuctionId = -999L;
+
+        willThrow(new AuctionNotFoundException("지정한 아이디에 대한 경매를 찾을 수 없습니다."))
+                .given(auctionService).deleteByAuctionId(anyLong(), anyLong());
+
+        // when & then
+        mockMvc.perform(delete("/auctions/{auctionId}", invalidAuctionId).contentType(MediaType.APPLICATION_JSON))
+               .andExpectAll(
+                       status().isNotFound(),
+                       jsonPath("$.message").exists()
+               );
+    }
+
+    @Test
+    void 경매_삭제시_유효한_회원이_아니라면_404를_반환한다() throws Exception {
+        // given
+        final Long invalidAuctionId = -999L;
+
+        willThrow(new UserNotFoundException("회원 정보를 찾을 수 없습니다."))
+                .given(auctionService).deleteByAuctionId(anyLong(), anyLong());
+
+        // when & then
+        mockMvc.perform(delete("/auctions/{auctionId}", invalidAuctionId)
+                       .contentType(MediaType.APPLICATION_JSON))
+               .andExpectAll(
+                       status().isNotFound(),
+                       jsonPath("$.message").exists()
+               );
     }
 }
