7ba14261c2bc1d564cb041825a22b9b57c540470
Mendel <67176829+rhthrhrl0@users.noreply.github.com>
2023-08-03 18:00:10 +0900

    refactor: #205 경매 입찰 응답 메시지 수정 (#206)

--- android/app/src/main/java/com/ddangddangddang/android/feature/detail/bid/SubmitBidFailureResponse.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/detail/bid/SubmitBidFailureResponse.kt
@@ -3,7 +3,7 @@ package com.ddangddangddang.android.feature.detail.bid
 enum class SubmitBidFailureResponse(private val message: String) {
     FINISH("이미 종료된 경매입니다"),
     DELETED("삭제된 경매입니다"),
-    UNDER_PRICE("마지막 입찰 금액보다 낮은 금액을 입력했습니다"),
+    UNDER_PRICE("가능 입찰액보다 낮은 금액을 입력했습니다"),
     ALREADY_HIGHEST_BIDDER("이미 최고 입찰자입니다"),
     ELSE("기타"),
     ;
