c190f61027b3b21f2f70e8227a1cd49bb6d04485
apptie <appti@naver.com>
2023-08-08 16:42:48 +0900

    feat: OauthId를 통해 회원을 조회하는 기능 추가

--- backend/ddang/src/main/java/com/ddang/ddang/user/infrastructure/persistence/JpaUserRepository.java
+++ backend/ddang/src/main/java/com/ddang/ddang/user/infrastructure/persistence/JpaUserRepository.java
@@ -1,7 +1,10 @@
 package com.ddang.ddang.user.infrastructure.persistence;
 
 import com.ddang.ddang.user.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaUserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByOauthId(final Long oauthId);
 }
