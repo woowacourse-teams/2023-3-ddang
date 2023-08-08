18a35f8224d31f7d5055e84f2b41a47b083c86ec
swonny <wonny921@gmail.com>
2023-08-08 11:22:17 +0900

    style: 불필요한 주석 제거

--- backend/ddang/src/main/java/com/ddang/ddang/chat/presentation/dto/CreateMessageRequest.java
+++ backend/ddang/src/main/java/com/ddang/ddang/chat/presentation/dto/CreateMessageRequest.java
@@ -3,10 +3,6 @@ package com.ddang.ddang.chat.presentation.dto;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateMessageRequest(
-        @NotNull(message = "경매 아이디가 입력되지 않았습니다.")
-        Long auctionId,
-        @NotNull(message = "채팅방 아이디가 입력되지 않았습니다.")
-        Long chatRoomId,
         @NotNull(message = "메시지 발신자 아이디가 입력되지 않았습니다.")
         Long writerId,
 
