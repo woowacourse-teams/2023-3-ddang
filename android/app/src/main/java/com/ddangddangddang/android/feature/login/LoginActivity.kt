12f609da59abf3cf9153573d9b6cf4a75d94fc6e
ippnsj <49394114+ippnsj@users.noreply.github.com>
2023-08-08 15:23:10 +0900

    feat: #227 카카오 소셜로그인 기능 구현 (#229)
    * feat: kakao sdk 추가
    
    * feat: 로그인 페이지 레이아웃 작성
    
    * feat: 카카오 소셜로그인 코드 작성

---
+++ android/app/src/main/java/com/ddangddangddang/android/feature/login/LoginActivity.kt
@@ -0,0 +1,78 @@
+package com.ddangddangddang.android.feature.login
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import com.ddangddangddang.android.R
+import com.ddangddangddang.android.databinding.ActivityLoginBinding
+import com.ddangddangddang.android.feature.common.viewModelFactory
+import com.ddangddangddang.android.global.AnalyticsDelegate
+import com.ddangddangddang.android.global.AnalyticsDelegateImpl
+import com.ddangddangddang.android.util.binding.BindingActivity
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
+
+class LoginActivity :
+    BindingActivity<ActivityLoginBinding>(R.layout.activity_login),
+    AnalyticsDelegate by AnalyticsDelegateImpl() {
+    private val viewModel: LoginViewModel by viewModels { viewModelFactory }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        registerAnalytics(javaClass.simpleName, lifecycle)
+
+        binding.viewModel = viewModel
+        setupViewModel()
+    }
+
+    private fun setupViewModel() {
+        viewModel.event.observe(this) {
+            when (it) {
+                LoginViewModel.LoginEvent.KakaoLoginEvent -> loginByKakao()
+            }
+        }
+    }
+
+    private fun loginByKakao() {
+        // 카카오톡 설치 확인
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) return loginByKakaoTalk()
+        loginByKakaoAccount()
+    }
+
+    private fun loginByKakaoTalk() {
+        UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+            if (error != null) {
+                if (isClientCancelError(error)) {
+                    logLoginError(error)
+                    return@loginWithKakaoTalk
+                }
+                loginByKakaoAccount()
+            } else if (token != null) {
+                completeLogin(token)
+            }
+        }
+    }
+
+    private fun isClientCancelError(error: Throwable?) =
+        error is ClientError && error.reason == ClientErrorCause.Cancelled
+
+    private fun loginByKakaoAccount() {
+        UserApiClient.instance.loginWithKakaoAccount(this) { token, error ->
+            if (error != null) {
+                logLoginError(error)
+            } else if (token != null) {
+                completeLogin(token)
+            }
+        }
+    }
+
+    private fun logLoginError(error: Throwable) {
+        Log.d("test", "로그인 실패 $error")
+    }
+
+    private fun completeLogin(token: OAuthToken) {
+        Log.d("test", "로그인 성공 ${token.accessToken}")
+    }
+}
--- android/app/src/main/java/com/ddangddangddang/android/global/DdangDdangDdang.kt
+++ android/app/src/main/java/com/ddangddangddang/android/global/DdangDdangDdang.kt
@@ -1,12 +1,16 @@
 package com.ddangddangddang.android.global
 
 import android.app.Application
+import com.ddangddangddang.android.BuildConfig
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.kakao.sdk.common.KakaoSdk
 
 class DdangDdangDdang : Application() {
     override fun onCreate() {
         super.onCreate()
         _firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
+        KakaoSdk.init(this, BuildConfig.KEY_KAKAO)
     }
 
     companion object {
