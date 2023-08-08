b891883473aaa80dff69e659915dad18f4cf4bca
ippnsj <ippnsj@gmail.com>
2023-08-08 12:39:24 +0900

    feat: 로그인 페이지 레이아웃 작성

---
+++ android/app/src/main/java/com/ddangddangddang/android/feature/login/LoginActivity.kt
@@ -0,0 +1,7 @@
+package com.ddangddangddang.android.feature.login
+
+import com.ddangddangddang.android.R
+import com.ddangddangddang.android.databinding.ActivityLoginBinding
+import com.ddangddangddang.android.util.binding.BindingActivity
+
+class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login)
