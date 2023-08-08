71304bf8e2f2f6f3be4f29de2e979c337e2e5a5b
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-08-08 12:15:28 +0900

    feat: 경매 신고 엔티티 추가

---
+++ backend/ddang/src/main/java/com/ddang/ddang/report/domain/AuctionReport.java
@@ -0,0 +1,48 @@
+package com.ddang.ddang.report.domain;
+
+import com.ddang.ddang.auction.domain.Auction;
+import com.ddang.ddang.common.entity.BaseCreateTimeEntity;
+import com.ddang.ddang.user.domain.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EqualsAndHashCode(of = "id")
+@ToString(of = {"id"})
+public class AuctionReport extends BaseCreateTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reporter_id", foreignKey = @ForeignKey(name = "fk_auction_report_reporter"))
+    private User reporter;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "auction_id", foreignKey = @ForeignKey(name = "fk_auction_report_auction"))
+    private Auction auction;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+    public AuctionReport(final User reporter, final Auction auction, final String description) {
+        this.reporter = reporter;
+        this.auction = auction;
+        this.description = description;
+    }
+}
