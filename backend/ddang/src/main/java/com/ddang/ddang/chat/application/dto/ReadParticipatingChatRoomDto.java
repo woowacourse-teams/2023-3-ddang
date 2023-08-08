af1df6ba3a4b57792156296a3619cb7b6deb16c4
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2023-08-08 12:10:16 +0900

    feat: #145 채팅방 상세 정보 조회 api 추가 (#212)
    * feat: 채팅방 상세 조회 서비스 추가
    
    * feat: 채팅방 상세 조회 api 추가
    
    * test: 테스트에서 객체 생성 시 필드가 무엇을 의미하는지 알 수 있도록 명시
    
    * style: return문 앞에 개행 추가
    
    * feat: 채팅방 접근 권한에 대한 커스텀 예외 추가
    
    * refactor: 불필요한 static 키워드 제거 및 일부 메서드 객체지향적으로 수정
    
    * refactor: 채팅 관련 컨트롤러의 메서드명을 명확하게 변경
    
    * feat: 채팅 가능 여부를 응답하도록 추가
    
    * feat: 채팅 패키지에 경매 정보를 나타내는 dto 추가
    
    * fix: 잘못된 import로 인한 오류 해결

--- backend/ddang/src/main/java/com/ddang/ddang/chat/application/dto/ReadParticipatingChatRoomDto.java
+++ backend/ddang/src/main/java/com/ddang/ddang/chat/application/dto/ReadParticipatingChatRoomDto.java
@@ -1,16 +1,25 @@
 package com.ddang.ddang.chat.application.dto;
 
-import com.ddang.ddang.auction.application.dto.ReadAuctionDto;
 import com.ddang.ddang.chat.domain.ChatRoom;
 import com.ddang.ddang.user.domain.User;
 
-public record ReadParticipatingChatRoomDto(Long id, ReadAuctionDto auctionDto, ReadUserDto partnerDto) {
+public record ReadParticipatingChatRoomDto(
+        Long id,
+        ReadAuctionDto auctionDto,
+        ReadUserDto partnerDto,
+        boolean isChatAvailable
+) {
 
-    public static ReadParticipatingChatRoomDto of(final User partner, final ChatRoom chatRoom) {
+    public static ReadParticipatingChatRoomDto of(
+            final User partner,
+            final ChatRoom chatRoom,
+            final boolean isChatAvailable
+    ) {
         return new ReadParticipatingChatRoomDto(
                 chatRoom.getId(),
                 ReadAuctionDto.from(chatRoom.getAuction()),
-                ReadUserDto.from(partner)
+                ReadUserDto.from(partner),
+                isChatAvailable
         );
     }
 }
