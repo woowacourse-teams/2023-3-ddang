249094a35761ebc0f60b8d852d6d0384faedb99d
apptie <appti@naver.com>
2023-08-09 14:22:23 +0900

    !hotfix: flyway script 오타 수정

--- backend/ddang/src/main/resources/db/migration/V5__alter_user_tables.sql
+++ backend/ddang/src/main/resources/db/migration/V5__alter_user_tables.sql
@@ -1,4 +1,4 @@
-alter table users add oauth_id varchat(50);
+alter table users add oauth_id varchar(50);
 
 alter table users add constraint uq_oauth_id unique(oauth_id);
 alter table users add constraint uq_name unique(name);
