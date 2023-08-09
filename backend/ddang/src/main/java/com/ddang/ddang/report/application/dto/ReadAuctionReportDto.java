2730b0f5cbc8e02ebbfad75b7c89e9f6b9b7345c
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-08-09 15:55:16 +0900

    refactor: final 추가

--- backend/ddang/src/main/java/com/ddang/ddang/report/application/dto/ReadAuctionReportDto.java
+++ backend/ddang/src/main/java/com/ddang/ddang/report/application/dto/ReadAuctionReportDto.java
@@ -13,7 +13,7 @@ public record ReadAuctionReportDto(
 ) {
 
     // TODO: 2023/08/08 [고민] ReadAuctionDto 클래스가 같은 이름으로 여러 패키지에 존재함. 이때, import를 할 때 헷갈리거나 실수하지는 않을까요?
-    public static ReadAuctionReportDto from(AuctionReport auctionReport) {
+    public static ReadAuctionReportDto from(final AuctionReport auctionReport) {
         return new ReadAuctionReportDto(
                 auctionReport.getId(),
                 ReadReporterDto.from(auctionReport.getReporter()),
