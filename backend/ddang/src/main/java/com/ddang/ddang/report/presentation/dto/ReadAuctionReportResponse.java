0ad9e12485acf66adf63687fcc9b929848156c6b
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-08-09 15:55:59 +0900

    style: 개행 추가

--- backend/ddang/src/main/java/com/ddang/ddang/report/presentation/dto/ReadAuctionReportResponse.java
+++ backend/ddang/src/main/java/com/ddang/ddang/report/presentation/dto/ReadAuctionReportResponse.java
@@ -7,12 +7,14 @@ import java.time.LocalDateTime;
 
 public record ReadAuctionReportResponse(
         Long id,
+
         ReadReporterResponse reporter,
 
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
         LocalDateTime createdTime,
 
         ReadAuctionResponse auction,
+
         String description
 ) {
 
--- backend/ddang/src/main/java/com/ddang/ddang/report/presentation/dto/ReadAuctionReportsResponse.java
+++ backend/ddang/src/main/java/com/ddang/ddang/report/presentation/dto/ReadAuctionReportsResponse.java
@@ -5,6 +5,7 @@ import com.ddang.ddang.report.application.dto.ReadAuctionReportDto;
 import java.util.List;
 
 public record ReadAuctionReportsResponse(List<ReadAuctionReportResponse> reports) {
+
     public static ReadAuctionReportsResponse from(final List<ReadAuctionReportDto> auctionReportDtos) {
         final List<ReadAuctionReportResponse> reportResponses = auctionReportDtos.stream()
                                                                                  .map(ReadAuctionReportResponse::from)
