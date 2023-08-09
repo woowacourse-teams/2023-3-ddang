84c67b1590f93d21c7f6b708ffa91eb04796be02
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-08-09 15:26:53 +0900

    refactor: querydsl을 EntityGraph로 수정

--- backend/ddang/src/main/java/com/ddang/ddang/report/infrastructure/persistence/JpaAuctionReportRepository.java
+++ backend/ddang/src/main/java/com/ddang/ddang/report/infrastructure/persistence/JpaAuctionReportRepository.java
@@ -1,9 +1,16 @@
 package com.ddang.ddang.report.infrastructure.persistence;
 
 import com.ddang.ddang.report.domain.AuctionReport;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaAuctionReportRepository extends JpaRepository<AuctionReport, Long>, QuerydslAuctionReportRepository {
+import java.util.List;
+
+public interface JpaAuctionReportRepository extends JpaRepository<AuctionReport, Long> {
 
     boolean existsByAuctionIdAndReporterId(final Long auctionId, final Long reporterId);
+
+    @Override
+    @EntityGraph(attributePaths = {"reporter", "auction"})
+    List<AuctionReport> findAll();
 }
