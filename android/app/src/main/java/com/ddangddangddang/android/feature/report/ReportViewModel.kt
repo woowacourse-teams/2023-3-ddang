60fd17518d2f690bad09060943ff80085de5a398
Song Hyemin <hyemdooly@gmail.com>
2023-08-11 00:27:34 +0900

    feat: #242 신고 페이지 UI/구조 작성, 삭제 버튼 UI추가 (#267)
    * feat: 신고 페이지 UI 작성
    
    * refactor: BindingActivity 사용하는 것으로 수정
    
    * feat: ViewModel 코드 작성
    
    * feat: 경매 상세 페이지 신고버튼 영역 잡아두기
    
    * refactor: 함수명 변경
    
    * refactor: 충돌 해결

---
+++ android/app/src/main/java/com/ddangddangddang/android/feature/report/ReportViewModel.kt
@@ -0,0 +1,29 @@
+package com.ddangddangddang.android.feature.report
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.ddangddangddang.android.util.livedata.SingleLiveEvent
+import com.ddangddangddang.data.repository.AuctionRepository
+
+class ReportViewModel(private val repository: AuctionRepository) : ViewModel() {
+    private val _event = SingleLiveEvent<ReportEvent>()
+    val event: LiveData<ReportEvent>
+        get() = _event
+
+    val reportContents = MutableLiveData<String>()
+
+    fun setExitEvent() {
+        _event.value = ReportEvent.ExitEvent
+    }
+
+    fun submit() {
+        // 통신
+        _event.value = ReportEvent.SubmitEvent
+    }
+
+    sealed class ReportEvent {
+        object ExitEvent : ReportEvent()
+        object SubmitEvent : ReportEvent()
+    }
+}
