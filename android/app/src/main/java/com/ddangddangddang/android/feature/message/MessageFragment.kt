6603b47bad409ccc69b4e05225085e953503166d
mendel <rhthrhrl0@naver.com>
2023-08-10 10:31:47 +0900

    feat: 쪽지 목록 페이지 서버 연동

--- android/app/src/main/java/com/ddangddangddang/android/feature/message/MessageFragment.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/message/MessageFragment.kt
@@ -1,7 +1,38 @@
 package com.ddangddangddang.android.feature.message
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
 import com.ddangddangddang.android.R
 import com.ddangddangddang.android.databinding.FragmentMessageBinding
+import com.ddangddangddang.android.feature.common.viewModelFactory
 import com.ddangddangddang.android.util.binding.BindingFragment
 
-class MessageFragment : BindingFragment<FragmentMessageBinding>(R.layout.fragment_message)
+class MessageFragment : BindingFragment<FragmentMessageBinding>(R.layout.fragment_message) {
+    private val viewModel: MessageViewModel by viewModels { viewModelFactory }
+    private val adapter: MessageRoomAdapter = MessageRoomAdapter { messageRoomModel ->
+        viewModel.navigateToMessageRoom(messageRoomModel.roomId)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViewModel()
+        if (viewModel.messageRooms.value == null) {
+            viewModel.loadMessageRooms()
+        }
+    }
+
+    private fun setupViewModel() {
+        viewModel.event.observe(viewLifecycleOwner) { handleEvent(it) }
+        viewModel.messageRooms.observe(viewLifecycleOwner) { adapter.setMessageRooms(it) }
+    }
+
+    private fun handleEvent(event: MessageViewModel.MessageEvent) {
+        when (event) {
+            is MessageViewModel.MessageEvent.NavigateToMessageRoom -> navigateToMessageRoom(event.roomId)
+        }
+    }
+
+    private fun navigateToMessageRoom(roomId: Long) {
+    }
+}
---
+++ android/app/src/main/java/com/ddangddangddang/android/feature/message/MessageViewModel.kt
@@ -0,0 +1,47 @@
+package com.ddangddangddang.android.feature.message
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ddangddangddang.android.model.MessageRoomModel
+import com.ddangddangddang.android.model.mapper.MessageRoomModelMapper.toPresentation
+import com.ddangddangddang.android.util.livedata.SingleLiveEvent
+import com.ddangddangddang.data.remote.ApiResponse
+import com.ddangddangddang.data.repository.ChatRepository
+import kotlinx.coroutines.launch
+
+class MessageViewModel(
+    private val repository: ChatRepository,
+) : ViewModel() {
+    private val _event: SingleLiveEvent<MessageEvent> = SingleLiveEvent()
+    val event: LiveData<MessageEvent>
+        get() = _event
+
+    private val _messageRooms: MutableLiveData<List<MessageRoomModel>> = MutableLiveData()
+    val messageRooms: LiveData<List<MessageRoomModel>>
+        get() = _messageRooms
+
+    fun loadMessageRooms() {
+        viewModelScope.launch {
+            when (val response = repository.getChatRoomPreviews()) {
+                is ApiResponse.Success -> {
+                    _messageRooms.value =
+                        response.body.map { it.toPresentation() }
+                }
+
+                is ApiResponse.Failure -> TODO()
+                is ApiResponse.NetworkError -> TODO()
+                is ApiResponse.Unexpected -> TODO()
+            }
+        }
+    }
+
+    fun navigateToMessageRoom(roomId: Long) {
+        _event.value = MessageEvent.NavigateToMessageRoom(roomId)
+    }
+
+    sealed class MessageEvent {
+        data class NavigateToMessageRoom(val roomId: Long) : MessageEvent()
+    }
+}
