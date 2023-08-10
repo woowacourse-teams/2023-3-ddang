08e2ef349398d16d7c480765fec12ff6b42c77c5
hyemdooly <hyemdooly@gmail.com>
2023-08-10 11:48:47 +0900

    refactor: BindingActivity 사용하는 것으로 수정

--- android/app/src/main/java/com/ddangddangddang/android/feature/report/ReportActivity.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/report/ReportActivity.kt
@@ -1,12 +1,12 @@
 package com.ddangddangddang.android.feature.report
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.ddangddangddang.android.R
+import com.ddangddangddang.android.databinding.ActivityReportBinding
+import com.ddangddangddang.android.util.binding.BindingActivity
 
-class ReportActivity : AppCompatActivity() {
+class ReportActivity : BindingActivity<ActivityReportBinding>(R.layout.activity_report) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_report)
     }
 }
