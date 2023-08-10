6a1881c36ca979380a093f743bc67599a3233984
swonny <wonny921@gmail.com>
2023-08-11 02:40:03 +0900

    fix: API 명세에 맞춰 응답값 변경

--- backend/ddang/src/main/java/com/ddang/ddang/chat/presentation/dto/response/ReadMessageResponse.java
+++ backend/ddang/src/main/java/com/ddang/ddang/chat/presentation/dto/response/ReadMessageResponse.java
@@ -1,10 +1,20 @@
 package com.ddang.ddang.chat.presentation.dto.response;
 
 import com.ddang.ddang.chat.application.dto.ReadMessageDto;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 
-public record ReadMessageResponse(Long id, LocalDateTime createdAt, boolean isMyMessage, String contents) {
+public record ReadMessageResponse(
+        Long id,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+        LocalDateTime createdAt,
+
+        boolean isMyMessage,
+
+        String contents
+) {
 
     public static ReadMessageResponse of(final ReadMessageDto readMessageDto, final boolean isMyMessage) {
         return new ReadMessageResponse(
