de00897a1680e5578a782960ef07f99c56666dde
kwonyj1022 <kwonyj1022@naver.com>
2023-08-10 22:22:17 +0900

    refactor: 채팅방 dto에 담을 정보 수정

--- backend/ddang/src/main/java/com/ddang/ddang/chat/application/dto/ReadAuctionInChatRoomDto.java
+++ backend/ddang/src/main/java/com/ddang/ddang/chat/application/dto/ReadAuctionInChatRoomDto.java
@@ -1,26 +1,16 @@
 package com.ddang.ddang.chat.application.dto;
 
-import com.ddang.ddang.auction.application.dto.ReadRegionsDto;
 import com.ddang.ddang.auction.domain.Auction;
 import com.ddang.ddang.bid.domain.Bid;
 import com.ddang.ddang.image.domain.AuctionImage;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public record ReadAuctionInChatRoomDto(
         Long id,
         String title,
-        String description,
-        int bidUnit,
-        int startPrice,
         Integer lastBidPrice,
-        boolean deleted,
-        LocalDateTime registerTime,
-        LocalDateTime closingTime,
-        List<ReadRegionsDto> auctionRegions,
         List<Long> auctionImageIds,
-        int auctioneerCount,
         String mainCategory,
         String subCategory,
         Long sellerId,
@@ -33,16 +23,8 @@ public record ReadAuctionInChatRoomDto(
         return new ReadAuctionInChatRoomDto(
                 auction.getId(),
                 auction.getTitle(),
-                auction.getDescription(),
-                auction.getBidUnit().getValue(),
-                auction.getStartPrice().getValue(),
                 convertPrice(auction.getLastBid()),
-                auction.isDeleted(),
-                auction.getCreatedTime(),
-                auction.getClosingTime(),
-                convertReadRegionsDto(auction),
                 convertImageUrls(auction),
-                auction.getAuctioneerCount(),
                 auction.getSubCategory().getMainCategory().getName(),
                 auction.getSubCategory().getName(),
                 auction.getSeller().getId(),
@@ -66,12 +48,5 @@ public record ReadAuctionInChatRoomDto(
 
         return bid.getPrice()
                   .getValue();
-    }
-
-    private static List<ReadRegionsDto> convertReadRegionsDto(final Auction auction) {
-        return auction.getAuctionRegions()
-                      .stream()
-                      .map(ReadRegionsDto::from)
-                      .toList();
     }
 }
