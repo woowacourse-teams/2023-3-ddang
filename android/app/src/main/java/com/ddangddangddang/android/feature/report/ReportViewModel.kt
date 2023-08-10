136fbd880ad45eafe4e572f0bbbcc02ab17ffa0c
hyemdooly <hyemdooly@gmail.com>
2023-08-11 00:58:11 +0900

    feat: ViewModel 코드 작성

--- android/app/src/main/java/com/ddangddangddang/android/feature/report/ReportViewModel.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/report/ReportViewModel.kt
@@ -3,22 +3,30 @@ package com.ddangddangddang.android.feature.report
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ddangddangddang.android.util.livedata.SingleLiveEvent
 import com.ddangddangddang.data.repository.AuctionRepository
+import kotlinx.coroutines.launch
 
 class ReportViewModel(private val repository: AuctionRepository) : ViewModel() {
     private val _event = SingleLiveEvent<ReportEvent>()
     val event: LiveData<ReportEvent>
         get() = _event
 
+    private var auctionId: Long? = null
     val reportContents = MutableLiveData<String>()
+    fun setAuctionId(id: Long) {
+        auctionId = id
+    }
 
     fun setExitEvent() {
         _event.value = ReportEvent.ExitEvent
     }
 
     fun submit() {
-        // 통신
+        viewModelScope.launch {
+            repository.reportAuction(auctionId ?: return@launch, reportContents.value ?: "")
+        }
         _event.value = ReportEvent.SubmitEvent
     }
 
