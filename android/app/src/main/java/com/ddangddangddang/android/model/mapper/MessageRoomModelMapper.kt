6c7fe789f4970ff3da35a7b14460201d9c9f7cd6
mendel <rhthrhrl0@naver.com>
2023-08-10 12:40:42 +0900

    refactor: Iso8601 포맷을 LocalDateTime으로 변환하는데 사용하던 DateTimeFormatter 제거

--- android/app/src/main/java/com/ddangddangddang/android/model/mapper/MessageRoomModelMapper.kt
+++ android/app/src/main/java/com/ddangddangddang/android/model/mapper/MessageRoomModelMapper.kt
@@ -6,8 +6,6 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 object MessageRoomModelMapper : Mapper<MessageRoomModel, ChatRoomPreviewResponse> {
-    private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
-
     override fun ChatRoomPreviewResponse.toPresentation(): MessageRoomModel {
         return MessageRoomModel(
             id,
@@ -15,7 +13,7 @@ object MessageRoomModelMapper : Mapper<MessageRoomModel, ChatRoomPreviewResponse
             chatPartner.profileImage,
             chatPartner.name,
             "마지막 메시지 내용", // 수정 필요
-            formatLastMessageTime(LocalDateTime.now()), // 수정 필요
+            formatLastMessageTime(LocalDateTime.now()), // 수정 필요 LocalDateTimeFormatter.parse 사용할 것
             isChatAvailable,
         )
     }
