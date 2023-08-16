49c5f9d477a3100ec3b3d28b5485d3b204427202
지토 <57691173+apptie@users.noreply.github.com>
2023-08-16 09:48:43 +0900

    chore: #301 운영 서버 관련 script 변경 (#302)
    * chore: 운영 서버 script 수정
    
    * chore: flyway version 5 script 수정

--- backend/ddang/src/main/resources/db/migration/V5__alter_user_tables.sql
+++ backend/ddang/src/main/resources/db/migration/V5__alter_user_tables.sql
@@ -1,6 +1,12 @@
 alter table users add oauth_id varchar(50);
-alter table users add created_time datetime(6) not null;
-alter table users add last_modified_time datetime(6) not null;
+alter table users add created_time datetime(6);
+alter table users add last_modified_time datetime(6);
+
+update users set created_time = now() where created_time is null;
+update users set last_modified_time = now() where last_modified_time is null;
+
+alter table users modify created_time datetime(6) not null;
+alter table users modify last_modified_time datetime(6) not null;
 
 alter table users add constraint uq_oauth_id unique(oauth_id);
 alter table users add constraint uq_name unique(name);
