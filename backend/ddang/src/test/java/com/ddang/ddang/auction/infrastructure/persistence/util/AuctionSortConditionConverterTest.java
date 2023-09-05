4c493eee3d4612f0505fd2f00ea5467e50b7b19a
apptie <appti@naver.com>
2023-09-06 00:41:19 +0900

    test: 누락된 예외 테스트 케이스 추가

--- backend/ddang/src/test/java/com/ddang/ddang/auction/infrastructure/persistence/util/AuctionSortConditionConverterTest.java
+++ backend/ddang/src/test/java/com/ddang/ddang/auction/infrastructure/persistence/util/AuctionSortConditionConverterTest.java
@@ -2,9 +2,12 @@ package com.ddang.ddang.auction.infrastructure.persistence.util;
 
 import static com.ddang.ddang.auction.domain.QAuction.auction;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.ddang.ddang.auction.infrastructure.persistence.util.exception.UnsupportedSortConditionException;
 import com.querydsl.core.types.OrderSpecifier;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -30,5 +33,14 @@ class AuctionSortConditionConverterTest {
                 Arguments.arguments("closingTime", auction.closingTime.asc()),
                 Arguments.arguments("reliability", auction.seller.reliability.asc())
         );
+    }
+
+    @Test
+    void 유효하지_않은_정렬_조건을_전달하면_예외가_발생한다() {
+        final String invalidSortCondition = "invalidSortCondition";
+
+        assertThatThrownBy(() -> AuctionSortConditionConverter.convert(Order.asc(invalidSortCondition)))
+                .isInstanceOf(UnsupportedSortConditionException.class)
+                .hasMessageContaining("지원하지 않는 정렬 방식입니다.");
     }
 }
