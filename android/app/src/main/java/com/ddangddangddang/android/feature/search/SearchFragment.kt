7389826fbfc533ae84229adf85c0b9017c1ce973
hyemdooly <hyemdooly@gmail.com>
2023-09-06 11:02:56 +0900

    feat: recyclerview 어댑터 연결

--- android/app/src/main/java/com/ddangddangddang/android/feature/search/SearchFragment.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/search/SearchFragment.kt
@@ -1,7 +1,46 @@
 package com.ddangddangddang.android.feature.search
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
 import com.ddangddangddang.android.R
 import com.ddangddangddang.android.databinding.FragmentSearchBinding
+import com.ddangddangddang.android.feature.common.viewModelFactory
+import com.ddangddangddang.android.feature.detail.AuctionDetailActivity
+import com.ddangddangddang.android.feature.home.AuctionAdapter
+import com.ddangddangddang.android.feature.home.AuctionSpaceItemDecoration
+import com.ddangddangddang.android.model.AuctionHomeModel
+import com.ddangddangddang.android.model.AuctionHomeStatusModel
 import com.ddangddangddang.android.util.binding.BindingFragment
 
-class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_search)
+class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+    private val viewModel: SearchViewModel by viewModels { viewModelFactory }
+    private val auctionAdapter = AuctionAdapter { auctionId ->
+        navigateToAuctionDetail(auctionId)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupAuctionRecyclerView()
+    }
+
+    private fun setupAuctionRecyclerView() {
+        binding.rvSearchAuctions.adapter = auctionAdapter.apply {
+            submitList(
+                listOf(
+                    AuctionHomeModel(0, "ihi", "", 1000, AuctionHomeStatusModel.UNBIDDEN, 0),
+                    AuctionHomeModel(1, "ihi", "", 1000, AuctionHomeStatusModel.UNBIDDEN, 0),
+                    AuctionHomeModel(2, "ihi", "", 1000, AuctionHomeStatusModel.SUCCESS, 0),
+                ),
+            )
+        }
+        binding.rvSearchAuctions.addItemDecoration(AuctionSpaceItemDecoration(2, 20))
+    }
+
+    private fun navigateToAuctionDetail(auctionId: Long) {
+        val intent = AuctionDetailActivity.getIntent(requireContext(), auctionId)
+        intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+        startActivity(intent)
+    }
+}
---
+++ android/app/src/main/java/com/ddangddangddang/android/feature/search/SearchViewModel.kt
@@ -0,0 +1,6 @@
+package com.ddangddangddang.android.feature.search
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel() {
+}
