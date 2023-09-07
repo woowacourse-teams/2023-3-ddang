7bfe21318491605ead4accb3a73e5392f37684ed
ippnsj <ippnsj@gmail.com>
2023-09-07 22:45:18 +0900

    feat: 알림 수신 시 메세지방으로 이동할 수 있는 알림 띄우는 기능 구현

--- android/app/src/main/java/com/ddangddangddang/android/notification/DdangDdangDdangFirebaseMessagingService.kt
+++ android/app/src/main/java/com/ddangddangddang/android/notification/DdangDdangDdangFirebaseMessagingService.kt
@@ -1,11 +1,52 @@
 package com.ddangddangddang.android.notification
 
+import android.Manifest
+import android.app.Notification
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.pm.PackageManager
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.ddangddangddang.android.R
+import com.ddangddangddang.android.feature.messageRoom.MessageRoomActivity
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class DdangDdangDdangFirebaseMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         // TODO send FCM registration token to your app server.
         Log.d("test", "Refreshed token: $token")
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        if (remoteMessage.data.isNotEmpty()) {
+            if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+                Log.d("test", "Message data payload: ${remoteMessage.data}")
+                val notification = createMessageReceivedNotification(remoteMessage)
+                NotificationManagerCompat.from(applicationContext)
+                    .notify(System.currentTimeMillis().toInt(), notification)
+            }
+        }
+    }
+
+    private fun createMessageReceivedNotification(remoteMessage: RemoteMessage): Notification {
+        val roomId = remoteMessage.data["redirectUrl"]?.split("/")?.last()?.toLong() ?: -1
+        val intent = MessageRoomActivity.getIntent(applicationContext, roomId)
+        val pendingIntent =
+            PendingIntent.getActivity(
+                applicationContext,
+                0,
+                intent,
+                FLAG_UPDATE_CURRENT or FLAG_IMMUTABLE,
+            )
+
+        return NotificationCompat.Builder(applicationContext, CHANNEL_ID).apply {
+            setSmallIcon(R.drawable.img_logo)
+            setContentTitle(remoteMessage.data["title"])
+            setContentText(remoteMessage.data["body"])
+            setContentIntent(pendingIntent)
+        }.build()
     }
 }
--- android/app/src/main/java/com/ddangddangddang/android/notification/NotificationChannel.kt
+++ android/app/src/main/java/com/ddangddangddang/android/notification/NotificationChannel.kt
@@ -6,13 +6,13 @@ import android.content.Context
 import android.content.Context.NOTIFICATION_SERVICE
 import com.ddangddangddang.android.R
 
-private const val CNANNEL_ID = "DDANGDDANGDDANG_CHANNEL_ID"
+const val CHANNEL_ID = "DDANGDDANGDDANG_CHANNEL_ID"
 
 fun createNotificationChannel(context: Context) {
     val name = context.getString(R.string.alarm_channel_name)
     val descriptionText = context.getString(R.string.alarm_channel_description)
     val importance = NotificationManager.IMPORTANCE_HIGH
-    val mChannel = NotificationChannel(CNANNEL_ID, name, importance)
+    val mChannel = NotificationChannel(CHANNEL_ID, name, importance)
     mChannel.description = descriptionText
     val notificationManager = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
     notificationManager.createNotificationChannel(mChannel)
