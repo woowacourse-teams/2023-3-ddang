694cfba35edba746d1da17e3b9944c3191f80854
apptie <57691173+apptie@users.noreply.github.com>
2023-09-08 16:52:06 +0900

    feat: #349 경매 목록 검색 시 제목 기반의 검색 기능 추가 (#350)
    * feat: 경매 목록 검색 DTO 추가
    
    * feat: 경매 목록 조회 시 제목 검색 기능 추가
    
    * rename: 경매 목록 조회 시 검색 DTO SearchCondition 네이밍 변경
    
    * feat: ReadAuctionSearchCondition ArgumentResolver 관련 기능 추가
    
    * test: 경매 목록 조회 시 제목 검색에 대한 더미 데이터와 테스트 케이스 추가
    
    * test: 경매 목록 조회 시 제목 검색에 대한 더미 데이터와 테스트 케이스 추가

--- backend/ddang/src/main/java/com/ddang/ddang/auction/infrastructure/persistence/QuerydslAuctionRepository.java
+++ backend/ddang/src/main/java/com/ddang/ddang/auction/infrastructure/persistence/QuerydslAuctionRepository.java
@@ -1,13 +1,18 @@
 package com.ddang.ddang.auction.infrastructure.persistence;
 
 import com.ddang.ddang.auction.domain.Auction;
+import com.ddang.ddang.auction.presentation.dto.request.ReadAuctionSearchCondition;
 import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 public interface QuerydslAuctionRepository {
 
-    Slice<Auction> findAuctionsAllByLastAuctionId(final Long lastAuctionId, final Pageable pageable);
+    Slice<Auction> findAuctionsAllByLastAuctionId(
+            final Long lastAuctionId,
+            final Pageable pageable,
+            final ReadAuctionSearchCondition readAuctionSearchCondition
+    );
 
     Optional<Auction> findAuctionById(final Long auctionId);
 }
