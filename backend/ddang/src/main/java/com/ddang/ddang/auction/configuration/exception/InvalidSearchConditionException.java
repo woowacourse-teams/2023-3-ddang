91ea2dbf01dba876d9c76429e8e6d2e0d013adba
apptie <appti@naver.com>
2023-09-08 15:21:47 +0900

    feat: ReadAuctionSearchCondition ArgumentResolver 관련 기능 추가

---
+++ backend/ddang/src/main/java/com/ddang/ddang/auction/configuration/exception/InvalidSearchConditionException.java
@@ -0,0 +1,8 @@
+package com.ddang.ddang.auction.configuration.exception;
+
+public class InvalidSearchConditionException extends IllegalArgumentException {
+
+    public InvalidSearchConditionException(final String s) {
+        super(s);
+    }
+}
