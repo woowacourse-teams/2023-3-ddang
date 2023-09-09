8a0729f185fa11e52ff71db54c2f7472cc16ba14
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-09-09 22:04:04 +0900

    feat: 이미지 조회 서비스 추가

--- backend/ddang/src/main/java/com/ddang/ddang/image/application/ImageService.java
+++ backend/ddang/src/main/java/com/ddang/ddang/image/application/ImageService.java
@@ -1,15 +1,18 @@
 package com.ddang.ddang.image.application;
 
+import com.ddang.ddang.image.domain.Image;
 import com.ddang.ddang.image.application.exception.ImageNotFoundException;
 import com.ddang.ddang.image.domain.AuctionImage;
 import com.ddang.ddang.image.infrastructure.persistence.JpaAuctionImageRepository;
-import java.net.MalformedURLException;
+import com.ddang.ddang.image.infrastructure.persistence.JpaImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.net.MalformedURLException;
 
 @Service
 @RequiredArgsConstructor
@@ -19,7 +22,18 @@ public class ImageService {
     @Value("${image.store.dir}")
     private String imageStoreDir;
 
+    private final JpaImageRepository imageRepository;
     private final JpaAuctionImageRepository auctionImageRepository;
+
+    public Resource readImage(final Long id) throws MalformedURLException {
+        final Image image = imageRepository.findById(id)
+                                           .orElseThrow(() -> new ImageNotFoundException(
+                                                   "지정한 이미지를 찾을 수 없습니다."
+                                           ));
+        final String fullPath = findFullPath(image.getStoreName());
+
+        return new UrlResource("file:" + fullPath);
+    }
 
     public Resource readAuctionImage(final Long id) throws MalformedURLException {
         final AuctionImage auctionImage = auctionImageRepository.findById(id)
--- backend/ddang/src/test/java/com/ddang/ddang/image/application/ImageServiceTest.java
+++ backend/ddang/src/test/java/com/ddang/ddang/image/application/ImageServiceTest.java
@@ -6,7 +6,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.ddang.ddang.image.application.exception.ImageNotFoundException;
 import com.ddang.ddang.image.domain.AuctionImage;
+import com.ddang.ddang.image.domain.Image;
 import com.ddang.ddang.image.infrastructure.persistence.JpaAuctionImageRepository;
+import com.ddang.ddang.image.infrastructure.persistence.JpaImageRepository;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -25,7 +27,35 @@ class ImageServiceTest {
     ImageService imageService;
 
     @Autowired
+    JpaImageRepository imageRepository;
+
+    @Autowired
     JpaAuctionImageRepository auctionImageRepository;
+
+    @Test
+    void 지정한_아이디에_해당하는_이미지를_조회한다() throws Exception {
+        // given
+        final Image image = new Image("image.png", "image.png");
+
+        imageRepository.save(image);
+
+        // when
+        final Resource actual = imageService.readImage(image.getId());
+
+        // then
+        assertThat(actual.getFilename()).isEqualTo("image.png");
+    }
+
+    @Test
+    void 지정한_아이디에_해당하는_이미지가_없는_경우_예외가_발생한다() {
+        // given
+        final Long invalidImageId = -999L;
+
+        // when & then
+        assertThatThrownBy(() -> imageService.readImage(invalidImageId))
+                .isInstanceOf(ImageNotFoundException.class)
+                .hasMessage("지정한 이미지를 찾을 수 없습니다.");
+    }
 
     @Test
     void 지정한_아이디에_해당하는_경매_이미지를_조회한다() throws Exception {
