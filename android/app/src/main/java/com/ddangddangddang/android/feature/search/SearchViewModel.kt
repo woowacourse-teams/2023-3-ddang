7ee44f61d16560afa90ed895ee3ff9c28dd2b825
hyemdooly <hyemdooly@gmail.com>
2023-09-10 22:25:04 +0900

    feat: 레포지토리, 뷰모델 연동

--- android/app/src/main/java/com/ddangddangddang/android/feature/search/SearchViewModel.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/search/SearchViewModel.kt
@@ -3,47 +3,145 @@ package com.ddangddangddang.android.feature.search
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ddangddangddang.android.model.AuctionHomeModel
-import com.ddangddangddang.android.model.AuctionHomeStatusModel
+import com.ddangddangddang.android.model.mapper.AuctionHomeModelMapper.toPresentation
 import com.ddangddangddang.android.util.livedata.SingleLiveEvent
+import com.ddangddangddang.data.model.response.AuctionPreviewsResponse
+import com.ddangddangddang.data.remote.ApiResponse
+import com.ddangddangddang.data.repository.AuctionRepository
+import kotlinx.coroutines.launch
+import com.ddangddangddang.android.feature.search.SearchViewModel.SearchEvent as SearchEvent1
 
-class SearchViewModel : ViewModel() {
-    private val _event: SingleLiveEvent<SearchEvent> = SingleLiveEvent()
-    val event: LiveData<SearchEvent>
+class SearchViewModel(private val repository: AuctionRepository) : ViewModel() {
+    private val _event: SingleLiveEvent<SearchEvent1> = SingleLiveEvent()
+    val event: LiveData<SearchEvent1>
         get() = _event
 
     val keyword: MutableLiveData<String> = MutableLiveData("")
 
-    private val _auctions: MutableLiveData<List<AuctionHomeModel>> = MutableLiveData()
+    private val _auctions: MutableLiveData<List<AuctionHomeModel>> = MutableLiveData(emptyList())
     val auctions: LiveData<List<AuctionHomeModel>>
         get() = _auctions
 
-    private val _searchStatus: MutableLiveData<SearchStatus> = MutableLiveData(SearchStatus.BeforeSearch)
+    private var _loadingAuctionInProgress = false
+    val loadingAuctionInProgress: Boolean
+        get() = _loadingAuctionInProgress
+
+    private var _isLast = false
+    val isLast: Boolean
+        get() = _isLast
+
+    private var _lastAuctionId: Long? = null
+    val lastAuctionId: Long?
+        get() = _lastAuctionId
+
+    private val _searchStatus: MutableLiveData<SearchStatus> =
+        MutableLiveData(SearchStatus.BeforeSearch)
     val searchStatus: LiveData<SearchStatus>
         get() = _searchStatus
 
     fun submitKeyword() {
-        // repository에서 검색 결과 List를 가져오는 코드
-        val dummy = listOf(
-            AuctionHomeModel(0, "ihi", "", 1000, AuctionHomeStatusModel.UNBIDDEN, 0),
-            AuctionHomeModel(1, "ihi", "", 1000, AuctionHomeStatusModel.UNBIDDEN, 0),
-            AuctionHomeModel(2, "ihi", "", 1000, AuctionHomeStatusModel.SUCCESS, 0),
-        )
+        if (!checkKeywordLimit()) return
+        if (!_loadingAuctionInProgress) {
+            _loadingAuctionInProgress = true
+            viewModelScope.launch {
+                keyword.value?.let {
+                    when (
+                        val response =
+                            repository.getAuctionPreviews(_lastAuctionId, SIZE_AUCTION_LOAD, it)
+                    ) {
+                        is ApiResponse.Success -> {
+                            initAuctions()
+                            updateAuctions(response)
+                        }
+                        is ApiResponse.Failure -> {
+                            _event.value = SearchEvent.LoadFailureNotice(response.error)
+                        }
+                        is ApiResponse.NetworkError -> {
+                            _event.value = SearchEvent.LoadFailureNotice(response.exception.message)
+                        }
+                        is ApiResponse.Unexpected -> {
+                            _event.value = SearchEvent.LoadFailureNotice(response.t?.message)
+                        }
+                    }
+                    _loadingAuctionInProgress = false
+                }
+            }
+        }
 
-        _auctions.value = dummy
-
-        if (dummy.isEmpty()) {
+        if (_auctions.value.isNullOrEmpty()) {
             _searchStatus.value = SearchStatus.NoData
             return
         }
         _searchStatus.value = SearchStatus.ExistData
     }
 
-    sealed class SearchEvent
+    private fun checkKeywordLimit(): Boolean {
+        keyword.value?.let {
+            if (it.length !in 2..20) {
+                _event.value = SearchEvent.KeywordLimit
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun initAuctions() {
+        _lastAuctionId = null
+        _auctions.value = emptyList()
+    }
+
+    private fun updateAuctions(response: ApiResponse.Success<AuctionPreviewsResponse>) {
+        _auctions.value?.let { items ->
+            _auctions.value = items + response.body.auctions.map { it.toPresentation() }
+            _isLast = response.body.isLast
+            _lastAuctionId = response.body.auctions.maxOf { it.id }
+        }
+    }
+
+    fun loadAuctions() {
+        // repository에서 검색 결과 List를 가져오는 코드
+        if (!_loadingAuctionInProgress) {
+            _loadingAuctionInProgress = true
+            viewModelScope.launch {
+                keyword.value?.let {
+                    when (
+                        val response =
+                            repository.getAuctionPreviews(_lastAuctionId, SIZE_AUCTION_LOAD, it)
+                    ) {
+                        is ApiResponse.Success -> {
+                            updateAuctions(response)
+                        }
+
+                        is ApiResponse.Failure -> {
+                            _event.value = SearchEvent.LoadFailureNotice(response.error)
+                        }
+                        is ApiResponse.NetworkError -> {
+                            _event.value = SearchEvent.LoadFailureNotice(response.exception.message)
+                        }
+                        is ApiResponse.Unexpected -> {
+                            _event.value = SearchEvent.LoadFailureNotice(response.t?.message)
+                        }
+                    }
+                    _loadingAuctionInProgress = false
+                }
+            }
+        }
+    }
+
+    sealed class SearchEvent {
+        object KeywordLimit : SearchEvent()
+        class LoadFailureNotice(val message: String?) : SearchEvent()
+    }
 
     sealed class SearchStatus {
         object BeforeSearch : SearchStatus()
         object NoData : SearchStatus()
         object ExistData : SearchStatus()
+    }
+
+    companion object {
+        private const val SIZE_AUCTION_LOAD = 10
     }
 }
