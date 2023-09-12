6298c4b5c5456118c252834c61bc7680ae808332
mendel <rhthrhrl0@naver.com>
2023-09-13 03:07:18 +0900

    feat: 유저 정보 수정 페이지 UI 작성

--- android/app/src/main/java/com/ddangddangddang/android/feature/userInfoChange/UserInfoChangeViewModel.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/userInfoChange/UserInfoChangeViewModel.kt
@@ -1,8 +1,21 @@
 package com.ddangddangddang.android.feature.userInfoChange
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ddangddangddang.android.model.ProfileModel
 import com.ddangddangddang.data.repository.UserRepository
 
 class UserInfoChangeViewModel(
     private val userRepository: UserRepository,
-) : ViewModel()
+) : ViewModel() {
+    private val _profile: MutableLiveData<ProfileModel> = MutableLiveData()
+    val profile: LiveData<ProfileModel>
+        get() = _profile
+
+    fun setExitEvent() {
+    }
+
+    fun selectProfileImage() {
+    }
+}
