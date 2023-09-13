2dd2a851430c2e851ffdcbc83028d984ac6d3f91
ippnsj <ippnsj@gmail.com>
2023-09-13 21:05:56 +0900

    feat: 새로 발급된 디바이스 토큰을 서버로 전송하는 기능 추가

--- android/data/src/main/java/com/ddangddangddang/data/repository/UserRepository.kt
+++ android/data/src/main/java/com/ddangddangddang/data/repository/UserRepository.kt
@@ -1,8 +1,11 @@
 package com.ddangddangddang.data.repository
 
+import com.ddangddangddang.data.model.request.UpdateDeviceTokenRequest
 import com.ddangddangddang.data.model.response.ProfileResponse
 import com.ddangddangddang.data.remote.ApiResponse
 
 interface UserRepository {
     suspend fun getProfile(): ApiResponse<ProfileResponse>
+
+    suspend fun updateDeviceToken(deviceTokenRequest: UpdateDeviceTokenRequest): ApiResponse<Unit>
 }
--- android/data/src/main/java/com/ddangddangddang/data/repository/UserRepositoryImpl.kt
+++ android/data/src/main/java/com/ddangddangddang/data/repository/UserRepositoryImpl.kt
@@ -1,12 +1,17 @@
 package com.ddangddangddang.data.repository
 
 import com.ddangddangddang.data.datasource.UserRemoteDataSource
+import com.ddangddangddang.data.model.request.UpdateDeviceTokenRequest
 import com.ddangddangddang.data.model.response.ProfileResponse
 import com.ddangddangddang.data.remote.ApiResponse
 import com.ddangddangddang.data.remote.AuctionService
 
 class UserRepositoryImpl(private val remoteDataSource: UserRemoteDataSource) : UserRepository {
     override suspend fun getProfile(): ApiResponse<ProfileResponse> = remoteDataSource.getProfile()
+
+    override suspend fun updateDeviceToken(deviceTokenRequest: UpdateDeviceTokenRequest): ApiResponse<Unit> {
+        return remoteDataSource.updateDeviceToken(deviceTokenRequest)
+    }
 
     companion object {
         @Volatile
