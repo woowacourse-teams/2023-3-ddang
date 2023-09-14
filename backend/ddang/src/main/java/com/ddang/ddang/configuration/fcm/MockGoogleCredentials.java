e90f4e349d49c9f0e0181c563e50cd630cd4c889
kwonyj1022 <kwonyj1022@naver.com>
2023-09-14 12:47:19 +0900

    refactor: mock 객체의 equals() and hashCode() 재정의를 롬복 사용하도록 수정

--- backend/ddang/src/main/java/com/ddang/ddang/configuration/fcm/MockGoogleCredentials.java
+++ backend/ddang/src/main/java/com/ddang/ddang/configuration/fcm/MockGoogleCredentials.java
@@ -2,11 +2,12 @@ package com.ddang.ddang.configuration.fcm;
 
 import com.google.auth.oauth2.AccessToken;
 import com.google.auth.oauth2.GoogleCredentials;
+import lombok.EqualsAndHashCode;
 
 import java.util.Date;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
+@EqualsAndHashCode(callSuper = false)
 public class MockGoogleCredentials extends GoogleCredentials {
 
     private final String tokenValue;
@@ -24,19 +25,5 @@ public class MockGoogleCredentials extends GoogleCredentials {
     @Override
     public AccessToken refreshAccessToken() {
         return new AccessToken(tokenValue, new Date(expiryTime));
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        final MockGoogleCredentials that = (MockGoogleCredentials) o;
-        return expiryTime == that.expiryTime && Objects.equals(tokenValue, that.tokenValue);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), tokenValue, expiryTime);
     }
 }
