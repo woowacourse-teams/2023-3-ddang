70614a65b6cfaa2bf8c72e334be68b7679546d96
apptie <appti@naver.com>
2023-09-15 18:06:56 +0900

    !hotfix: flyway 문법 수정

--- backend/ddang/src/main/resources/db/migration/V11__create_profile_image_tables.sql
+++ backend/ddang/src/main/resources/db/migration/V11__create_profile_image_tables.sql
@@ -6,4 +6,5 @@ create table profile_image (
 );
 
 alter table users drop profile_image;
+alter table users add profile_image_id bigint;
 alter table users add constraint fk_user_profile_image foreign key (profile_image_id) references profile_image (id);
