684d60469b2127ec43d9b02c8f6cd5ff71d2818b
apptie <57691173+apptie@users.noreply.github.com>
2023-09-15 15:25:17 +0900

    feat: #366 사용자 관련 경매 목록 조회 기능 추가 (#368)
    * feat: 회원이 등록한 경매 목록을 조회하는 쿼리 추가
    
    * feat: 회원이 등록한 경매 목록을 조회하는 기능 추가
    
    * feat: 회원이 등록한 경매 목록을 조회하는 api 추가
    
    * feat: 회원이 참여한 경매 목록을 조회하는 쿼리 추가
    
    * test: 테스트 클래스 네이밍 변경
    
    * feat: 회원이 참여한 경매 목록을 조회하는 기능 추가
    
    * feat: 회원이 참여한 경매 목록을 조회하는 api 추가
    
    * docs: 문서 최신화
    
    * style: 불필요한 개행 제거
    
    * style: 메서드 순서 변경
    
    * refactor: 사용자가 호출하는 경매 목록 조회 Controller 분리
    
    * docs: 문서 최신화

--- backend/ddang/src/main/java/com/ddang/ddang/auction/infrastructure/persistence/QuerydslAuctionRepository.java
+++ backend/ddang/src/main/java/com/ddang/ddang/auction/infrastructure/persistence/QuerydslAuctionRepository.java
@@ -14,4 +14,8 @@ public interface QuerydslAuctionRepository {
     );
 
     Optional<Auction> findAuctionById(final Long auctionId);
+
+    Slice<Auction> findAuctionsAllByUserId(final Long userId, final Pageable pageable);
+
+    Slice<Auction> findAuctionsAllByBidderId(final Long bidderId, final Pageable pageable);
 }
