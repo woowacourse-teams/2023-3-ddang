f148c0001143124ffd49d98e28c8537e4b563819
apptie <57691173+apptie@users.noreply.github.com>
2023-09-16 22:59:05 +0900

    refactor: #386 프로덕션 환경의 FCM 설정과 로컬 환경의 FCM 분리 (#393)
    * refactor: production 환경에서의 FCM Configuration 클래스 네이밍 변경 및 profile 설정 추가
    
    * refactor: local 환경에서의 FCM Configuration 클래스 추가

--- backend/ddang/src/main/java/com/ddang/ddang/configuration/fcm/ProdFcmConfiguration.java
+++ backend/ddang/src/main/java/com/ddang/ddang/configuration/fcm/ProdFcmConfiguration.java
@@ -16,8 +16,8 @@ import java.util.List;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("!test")
-public class FcmConfiguration {
+@Profile("!test && !local")
+public class ProdFcmConfiguration {
 
     @Value("${fcm.key.path}")
     private String FCM_PRIVATE_KEY_PATH;
