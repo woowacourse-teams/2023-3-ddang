24795f88eeafd672a2d4c4f4608991cf705b7182
kwonyj1022 <kwonyj1022@naver.com>
2023-09-18 18:18:27 +0900

    feat: 알림 dto에 `@NonNull` 제약 조건 추가

--- backend/ddang/src/main/java/com/ddang/ddang/notification/application/dto/CreateNotificationDto.java
+++ backend/ddang/src/main/java/com/ddang/ddang/notification/application/dto/CreateNotificationDto.java
@@ -1,13 +1,25 @@
 package com.ddang.ddang.notification.application.dto;
 
 import com.ddang.ddang.notification.domain.NotificationType;
+import org.springframework.lang.NonNull;
 
 public record CreateNotificationDto(
+        @NonNull
         NotificationType notificationType,
+
+        @NonNull
         Long targetUserId,
+
+        @NonNull
         String title,
+
+        @NonNull
         String body,
+
+        @NonNull
         String redirectUrl,
+
+        @NonNull
         String image
 ) {
 }
