ca9aef4907a11d2ed3b268f4113fc259ec1449b0
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2023-09-19 19:35:52 +0900

    채팅방 목록 조회 시 경매 이미지 개수만큼 데이터가 중복되는 현상 해결 및 쿼리 최적화 (#411)
    * fix: 이미지 개수만큼 채팅방이 중복 조회 되는 문제 해결
    
    * rename: dto 이름 변경
    
    * refactor: 채팅방 아이디로 채팅방 조회할 때 쿼리 개선
    
    * style: 나중에 하기로 한 것 todo 추가

--- backend/ddang/src/main/java/com/ddang/ddang/chat/infrastructure/persistence/QuerydslChatRoomRepository.java
+++ backend/ddang/src/main/java/com/ddang/ddang/chat/infrastructure/persistence/QuerydslChatRoomRepository.java
@@ -1,12 +1,8 @@
 package com.ddang.ddang.chat.infrastructure.persistence;
 
-import com.ddang.ddang.chat.domain.ChatRoom;
-
 import java.util.Optional;
 
 public interface QuerydslChatRoomRepository {
-
-    Optional<ChatRoom> findChatRoomById(final Long chatRoomId);
 
     Optional<Long> findChatRoomIdByAuctionId(final Long auctionId);
 }
--- backend/ddang/src/main/java/com/ddang/ddang/chat/infrastructure/persistence/dto/ChatRoomAndMessageDto.java
+++
@@ -1,7 +0,0 @@
-package com.ddang.ddang.chat.infrastructure.persistence.dto;
-
-import com.ddang.ddang.chat.domain.ChatRoom;
-import com.ddang.ddang.chat.domain.Message;
-
-public record ChatRoomAndMessageDto(ChatRoom chatRoom, Message message) {
-}
