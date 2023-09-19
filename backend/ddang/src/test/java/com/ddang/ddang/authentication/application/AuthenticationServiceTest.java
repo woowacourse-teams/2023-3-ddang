0a1dbc75dc267c2217f01a8a5fbdaebc2deeed36
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2023-09-19 20:03:48 +0900

    프로필 이미지가 null일 경우 알림 전송에 실패하는 오류 해결 (#412)
    * feat: 로그인 시 프로필 이미지를 null이 아닌 기본이미지로 세팅하는 기능 추가
    
    * feat: 사용자 엔티티의 프로필 이미지에 널 불가 제약조건 추가
    
    * feat: 알림 dto에 `@NonNull` 제약 조건 추가
    
    * refactor: 알림 전송 성공 및 예외 발생 시 로그 추가
    
    * refactor: 기본 프로필 이미지 조회 시 이름으로 조회하도록 변경
    
    * feat: 로그인 시 기본이미지를 찾을 수 없으면 예외가 발생하는 기능 추가
    
    * refactor: 프로필 이미지 경로 생성을 유틸 클래스에서 하도록 변경
    
    * refactor: 기본 프로필 이미지 이름을 엔티티에서 관리하도록 변경
    
    * fix: flyway 스크립트 수정
    
    * refactor: 알림 dto에서 사용하는 `@NonNull`을 롬복의 어노테이션을 사용하도록 변경

--- backend/ddang/src/test/java/com/ddang/ddang/authentication/application/AuthenticationServiceTest.java
+++ backend/ddang/src/test/java/com/ddang/ddang/authentication/application/AuthenticationServiceTest.java
@@ -16,7 +16,9 @@ import com.ddang.ddang.configuration.IsolateDatabase;
 import com.ddang.ddang.device.application.DeviceTokenService;
 import com.ddang.ddang.device.application.dto.PersistDeviceTokenDto;
 import com.ddang.ddang.device.infrastructure.persistence.JpaDeviceTokenRepository;
+import com.ddang.ddang.image.application.exception.ImageNotFoundException;
 import com.ddang.ddang.image.domain.ProfileImage;
+import com.ddang.ddang.image.infrastructure.persistence.JpaProfileImageRepository;
 import com.ddang.ddang.user.domain.User;
 import com.ddang.ddang.user.infrastructure.persistence.JpaUserRepository;
 import org.assertj.core.api.SoftAssertions;
@@ -62,6 +64,9 @@ class AuthenticationServiceTest {
     JpaUserRepository userRepository;
 
     @Autowired
+    JpaProfileImageRepository profileImageRepository;
+
+    @Autowired
     JpaDeviceTokenRepository userDeviceTokenRepository;
 
     @Autowired
@@ -92,6 +97,7 @@ class AuthenticationServiceTest {
                 deviceTokenService,
                 mockProviderComposite,
                 userRepository,
+                profileImageRepository,
                 tokenEncoder,
                 tokenDecoder,
                 mockBlackListTokenService
@@ -130,6 +136,8 @@ class AuthenticationServiceTest {
     @Test
     void 가입한_회원이_소셜_로그인을_할_경우_accessToken과_refreshToken을_반환한다() {
         // given
+        profileImageRepository.save(new ProfileImage("default_profile_image.png", "default_profile_image.png"));
+
         final User user = User.builder()
                               .name("kakao12345")
                               .profileImage(new ProfileImage("upload.png", "store.png"))
@@ -155,8 +163,24 @@ class AuthenticationServiceTest {
     }
 
     @Test
+    void 가입하지_않은_회원이_소셜_로그인을_할_때_기본_프로필_이미지를_찾을_수_없으면_예외가_발생한다() {
+        // given
+        final UserInformationDto userInformationDto = new UserInformationDto(12345L);
+
+        given(mockProviderComposite.findProvider(Oauth2Type.KAKAO)).willReturn(mockProvider);
+        given(mockProvider.findUserInformation(anyString())).willReturn(userInformationDto);
+
+        // when & then
+        assertThatThrownBy(() -> authenticationService.login(Oauth2Type.KAKAO, "accessToken", "deviceToken"))
+                .isInstanceOf(ImageNotFoundException.class)
+                .hasMessage("기본 이미지를 찾을 수 없습니다.");
+    }
+
+    @Test
     void 가입하지_않은_회원이_소셜_로그인을_할_경우_accessToken과_refreshToken을_반환한다() {
         // given
+        profileImageRepository.save(new ProfileImage("default_profile_image.png", "default_profile_image.png"));
+
         final UserInformationDto userInformationDto = new UserInformationDto(12345L);
 
         given(mockProviderComposite.findProvider(Oauth2Type.KAKAO)).willReturn(mockProvider);
@@ -175,6 +199,8 @@ class AuthenticationServiceTest {
     @Test
     void 탈퇴한_회원이_소셜_로그인을_할_경우_accessToken과_refreshToken을_반환한다() {
         // given
+        profileImageRepository.save(new ProfileImage("default_profile_image.png", "default_profile_image.png"));
+
         final User user = User.builder()
                               .name("kakao12345")
                               .profileImage(new ProfileImage("upload.png", "store.png"))
