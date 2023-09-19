02212912115a4e3efd686cbd2c0faee0d506fbcf
kwonyj1022 <kwonyj1022@naver.com>
2023-09-19 18:21:47 +0900

    fix: flyway 스크립트 수정

--- backend/ddang/src/main/resources/db/migration/V13__alter_user_tables.sql
+++ backend/ddang/src/main/resources/db/migration/V13__alter_user_tables.sql
@@ -1,2 +1,2 @@
-UPDATE users SET profile_image_id = 1 WHERE profile_image_id = null;
+UPDATE users SET profile_image_id = 1 WHERE profile_image_id is null;
 ALTER TABLE users MODIFY profile_image_id bigint NOT NULL;
