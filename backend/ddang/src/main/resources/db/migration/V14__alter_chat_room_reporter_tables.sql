04575648919194f81b3275f1684691e9bc3d5acb
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-09-21 15:46:39 +0900

    fix: 쪽지 신고 fk 수정

---
+++ backend/ddang/src/main/resources/db/migration/V14__alter_chat_room_reporter_tables.sql
@@ -0,0 +1,2 @@
+alter table chat_room_report drop constraint fk_chat_room_report_auction;
+alter table chat_room_report add constraint fk_chat_room_report_chat_room foreign key (chat_room_id) references chat_room (id);
