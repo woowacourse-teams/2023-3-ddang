e56ad8ab14980e66bbe465596ae96f0332557bb1
Mendel <67176829+rhthrhrl0@users.noreply.github.com>
2023-09-21 17:51:07 +0900

    feat: #435 인앱 자동 업데이트 기능 추가 (#436)
    * chore: 앱 업데이트 매니저 빌드 그레이들 추가
    
    * feat: 업데이트 매니저 로직 추가
    
    * feat: 즉시 업데이트 하지 못하는 상황에서 보여줄 토스트 추가

--- android/app/src/main/java/com/ddangddangddang/android/feature/splash/SplashActivity.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/splash/SplashActivity.kt
@@ -2,21 +2,79 @@ package com.ddangddangddang.android.feature.splash
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import com.ddangddangddang.android.R
 import com.ddangddangddang.android.databinding.ActivitySplashBinding
 import com.ddangddangddang.android.feature.login.LoginActivity
 import com.ddangddangddang.android.feature.main.MainActivity
 import com.ddangddangddang.android.util.binding.BindingActivity
+import com.ddangddangddang.android.util.view.Toaster
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.appupdate.AppUpdateOptions
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
     private val viewModel: SplashViewModel by viewModels()
+
+    private val updateResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                Log.d("mendel", "업데이트 완료")
+                viewModel.checkTokenExist()
+            }
+        }
+
+    private val appUpdateManager by lazy {
+        AppUpdateManagerFactory.create(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupObserve()
-        viewModel.checkTokenExist()
+
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE &&
+                appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+            ) {
+                Log.d("mendel", "업데이트 할거 있고, 지금 당장 할 수 있음")
+                appUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    updateResultLauncher,
+                    AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE).build(),
+                )
+            } else if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE &&
+                !appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+            ) {
+                Log.d("mendel", "업데이트 할게 있지만, 현재 즉시 업데이트 하지 못하는 상황")
+                Toaster.showShort(this, getString(R.string.all_must_update_to_latest_version))
+                finish()
+            } else {
+                Log.d("mendel", "업데이트 할거 없음. 통과")
+                viewModel.checkTokenExist()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability()
+                == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS
+            ) {
+                Log.d("mendel", "업데이트 도중 돌아옴")
+                // If an in-app update is already running, resume the update.
+                appUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    updateResultLauncher,
+                    AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE).build(),
+                )
+            }
+        }
     }
 
     private fun setupObserve() {
