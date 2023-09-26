30b2e75c937ba385069674c0b51f4508510bfec8
mendel <rhthrhrl0@naver.com>
2023-09-26 18:29:24 +0900

    refactor: 스플래시 화면에서 인앱 업데이트 대신 업데이트 페이지 이동으로 수정

--- android/app/src/main/java/com/ddangddangddang/android/feature/splash/SplashActivity.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/splash/SplashActivity.kt
@@ -1,9 +1,8 @@
 package com.ddangddangddang.android.feature.splash
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import com.ddangddangddang.android.R
 import com.ddangddangddang.android.databinding.ActivitySplashBinding
@@ -11,23 +10,14 @@ import com.ddangddangddang.android.feature.login.LoginActivity
 import com.ddangddangddang.android.feature.main.MainActivity
 import com.ddangddangddang.android.util.binding.BindingActivity
 import com.ddangddangddang.android.util.view.Toaster
+import com.ddangddangddang.android.util.view.showDialog
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
-import com.google.android.play.core.appupdate.AppUpdateOptions
-import com.google.android.play.core.install.model.AppUpdateType
 import com.google.android.play.core.install.model.UpdateAvailability
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
     private val viewModel: SplashViewModel by viewModels()
-
-    private val updateResultLauncher =
-        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
-            if (result.resultCode == RESULT_OK) {
-                Log.d("mendel", "업데이트 완료")
-                viewModel.checkTokenExist()
-            }
-        }
 
     private val appUpdateManager by lazy {
         AppUpdateManagerFactory.create(this)
@@ -36,47 +26,14 @@ class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupObserve()
-
         appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
-            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE &&
-                appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
-            ) {
-                Log.d("mendel", "업데이트 할거 있고, 지금 당장 할 수 있음")
-                appUpdateManager.startUpdateFlowForResult(
-                    appUpdateInfo,
-                    updateResultLauncher,
-                    AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE).build(),
-                )
-            } else if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE &&
-                !appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
-            ) {
-                Log.d("mendel", "업데이트 할게 있지만, 현재 즉시 업데이트 하지 못하는 상황")
-                Toaster.showShort(this, getString(R.string.all_must_update_to_latest_version))
-                finish()
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
+                requestUpdate()
             } else {
-                Log.d("mendel", "업데이트 할거 없음. 통과")
                 viewModel.checkTokenExist()
             }
-        }.addOnFailureListener { e ->
-            Log.d("mendel", "업데이트 매니저 실패 다운 받은 전적이 없거나 등: $e")
+        }.addOnFailureListener {
             viewModel.checkTokenExist()
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
-            if (appUpdateInfo.updateAvailability()
-                == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS
-            ) {
-                Log.d("mendel", "업데이트 도중 돌아옴")
-                // If an in-app update is already running, resume the update.
-                appUpdateManager.startUpdateFlowForResult(
-                    appUpdateInfo,
-                    updateResultLauncher,
-                    AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE).build(),
-                )
-            }
         }
     }
 
@@ -100,5 +57,27 @@ class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_
     private fun navigateToLogin() {
         startActivity(Intent(this, LoginActivity::class.java))
         finish()
+    }
+
+    private fun requestUpdate() {
+        showDialog(
+            titleId = R.string.splash_app_update_request_dialog_title,
+            messageId = R.string.splash_app_update_request_dialog_message,
+            negativeStringId = R.string.all_dialog_default_negative_button,
+            positiveStringId = R.string.all_dialog_default_positive_button,
+            actionPositive = {
+                navigateToPlayStore()
+                finish()
+            },
+            actionNegative = {
+                Toaster.showShort(this, getString(R.string.splash_app_update_denied))
+                finish()
+            },
+            isCancelable = false,
+        )
+    }
+
+    private fun navigateToPlayStore() {
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$packageName")))
     }
 }
