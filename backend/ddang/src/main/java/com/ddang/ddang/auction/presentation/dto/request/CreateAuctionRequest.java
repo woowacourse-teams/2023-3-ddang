2e2fed2c3987f935f342436a70f6937dd8abd1fd
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2023-09-26 22:46:33 +0900

    fix: 예외 처리 방식 및 메시지 수정 (#465)

--- backend/ddang/src/main/java/com/ddang/ddang/auction/presentation/dto/request/CreateAuctionRequest.java
+++ backend/ddang/src/main/java/com/ddang/ddang/auction/presentation/dto/request/CreateAuctionRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -15,11 +16,11 @@ public record CreateAuctionRequest(
         String description,
 
         @NotNull(message = "입찰 단위가 입력되지 않았습니다.")
-        @Positive(message = "금액은 음수를 입력할 수 없습니다.")
+        @Positive(message = "금액은 양수로 입력해주세요.")
         Integer bidUnit,
 
         @NotNull(message = "시작가가 입력되지 않았습니다.")
-        @Positive(message = "금액은 음수를 입력할 수 없습니다.")
+        @Positive(message = "금액은 양수로 입력해주세요.")
         Integer startPrice,
 
         @NotNull(message = "마감 시간이 입력되지 않았습니다.")
@@ -27,7 +28,7 @@ public record CreateAuctionRequest(
         LocalDateTime closingTime,
 
         @NotNull(message = "하위 카테고리가 입력되지 않았습니다.")
-        @Positive(message = "카테고리 아이디는 음수 또는 0을 입력할 수 없습니다.")
+        @Positive(message = "잘못된 카테고리 입니다.")
         Long subCategoryId,
 
         List<Long> thirdRegionIds
