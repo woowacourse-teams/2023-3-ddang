979e1dc52f172627de6b8ba4c05657b8a0860d14
apptie <appti@naver.com>
2023-09-28 21:03:52 +0900

    test: 사용하지 않는 필드 제거와 final 제거

--- backend/ddang/src/test/java/com/ddang/ddang/authentication/infrastructure/jwt/fixture/JwtDecoderFixture.java
+++ backend/ddang/src/test/java/com/ddang/ddang/authentication/infrastructure/jwt/fixture/JwtDecoderFixture.java
@@ -8,17 +8,17 @@ import java.util.Map;
 
 public class JwtDecoderFixture {
 
-    protected final String 유효하지_않은_길이의_토큰 = "abcde";
-    protected final String 유효하지_않은_토큰 = "Bearer abcde";
-    protected final String 유효하지_않은_타입의_토큰 = "Basic12 abcde";
-    protected final JwtConfigurationProperties 토큰_설정 = new JwtConfigurationProperties(
+    protected String 유효하지_않은_길이의_토큰 = "abcde";
+    protected String 유효하지_않은_토큰 = "Bearer abcde";
+    protected String 유효하지_않은_타입의_토큰 = "Basic12 abcde";
+    protected JwtConfigurationProperties 토큰_설정 = new JwtConfigurationProperties(
             "thisistoolargeaccesstokenkeyfordummykeydataforlocal",
             "thisistoolargerefreshtokenkeyfordummykeydataforlocal",
             12L,
             1460L
     );
-    protected final JwtEncoder 토큰_암호화_생성기 = new JwtEncoder(토큰_설정);
-    protected final Map<String, Object> 토큰_내용 = Map.of("userId", 1L);
-    protected final String 유효한_토큰 = 토큰_암호화_생성기.encode(LocalDateTime.now(), TokenType.ACCESS, 토큰_내용);
-    protected final String 회원_아이디_키 = "userId";
+    protected JwtEncoder 토큰_암호화_생성기 = new JwtEncoder(토큰_설정);
+    protected Map<String, Object> 토큰_내용 = Map.of("userId", 1L);
+    protected String 유효한_토큰 = 토큰_암호화_생성기.encode(LocalDateTime.now(), TokenType.ACCESS, 토큰_내용);
+    protected String 회원_아이디_키 = "userId";
 }
