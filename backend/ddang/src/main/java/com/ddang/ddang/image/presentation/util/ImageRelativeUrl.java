9bc7010693882f763bce16f649a90b8c5e0d10a3
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-09-29 00:51:55 +0900

    style: todo 제거

--- backend/ddang/src/main/java/com/ddang/ddang/image/presentation/util/ImageRelativeUrl.java
+++ backend/ddang/src/main/java/com/ddang/ddang/image/presentation/util/ImageRelativeUrl.java
@@ -2,7 +2,6 @@ package com.ddang.ddang.image.presentation.util;
 
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-// TODO: 2023-09-28 [고민] ImageRelativeUrl vs RelativeImageUrl
 public enum ImageRelativeUrl {
 
     AUCTION("/auctions/images/"),
