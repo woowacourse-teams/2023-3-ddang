a776946edf3bbad03cc3363cd646c549aec9a213
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-09-28 17:32:04 +0900

    test: 테스트 추가

--- backend/ddang/src/test/java/com/ddang/ddang/image/presentation/util/ImageUrlCalculatorTest.java
+++ backend/ddang/src/test/java/com/ddang/ddang/image/presentation/util/ImageUrlCalculatorTest.java
@@ -1,14 +1,10 @@
 package com.ddang.ddang.image.presentation.util;
 
-import com.ddang.ddang.image.domain.AuctionImage;
-import com.ddang.ddang.image.domain.ProfileImage;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
@@ -17,30 +13,52 @@ class ImageUrlCalculatorTest {
     @Test
     void 프로필_사진의_URL을_계산한다() {
         // given
-        final ProfileImage mockProfileImage = mock(ProfileImage.class);
-        final long profileImageId = 1L;
-        BDDMockito.given(mockProfileImage.getId()).willReturn(profileImageId);
+        final Long profileImageId = 1L;
         final String absoluteUrl = "http://3-ddang.store/users/images/";
 
         // when
-        final String actual = ImageUrlCalculator.calculateBy(absoluteUrl, mockProfileImage.getId());
+        final String actual = ImageUrlCalculator.calculateBy(absoluteUrl, profileImageId);
 
         // then
         assertThat(actual).isEqualTo(absoluteUrl + profileImageId);
     }
 
     @Test
-    void 경매_대표_이미지의_URL을_계산한다() {
+    void 이미지_아이디가_null인_경우_프로필_사진의_URL로_null을_반환한다() {
         // given
-        final AuctionImage mockAuctionImage = mock(AuctionImage.class);
-        final long auctionImageId = 1L;
-        BDDMockito.given(mockAuctionImage.getId()).willReturn(auctionImageId);
+        final Long emptyProfileImage = null;
         final String absoluteUrl = "http://3-ddang.store/users/images/";
 
         // when
-        final String actual = ImageUrlCalculator.calculateBy(absoluteUrl, mockAuctionImage.getId());
+        final String actual = ImageUrlCalculator.calculateBy(absoluteUrl, emptyProfileImage);
+
+        // then
+        assertThat(actual).isNull();
+    }
+
+    @Test
+    void 경매_대표_이미지의_URL을_계산한다() {
+        // given
+        final Long auctionImageId = 1L;
+        final String absoluteUrl = "http://3-ddang.store/users/images/";
+
+        // when
+        final String actual = ImageUrlCalculator.calculateBy(absoluteUrl, auctionImageId);
 
         // then
         assertThat(actual).isEqualTo(absoluteUrl + auctionImageId);
+    }
+
+    @Test
+    void 이미지_아이디가_null인_경우_경매_대표_이미지의_URL로_null을_반환한다() {
+        // given
+        final Long emptyAuctionImageId = null;
+        final String absoluteUrl = "http://3-ddang.store/users/images/";
+
+        // when
+        final String actual = ImageUrlCalculator.calculateBy(absoluteUrl, emptyAuctionImageId);
+
+        // then
+        assertThat(actual).isNull();
     }
 }
