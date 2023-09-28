33eb5a76c3b118d3479cc5a1f3314beece965b43
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-09-29 01:07:53 +0900

    refactor: 필요없는 로직 제거

--- backend/ddang/src/main/java/com/ddang/ddang/image/presentation/util/ImageUrlCalculator.java
+++ backend/ddang/src/main/java/com/ddang/ddang/image/presentation/util/ImageUrlCalculator.java
@@ -6,20 +6,12 @@ public final class ImageUrlCalculator {
     }
 
     public static String calculateBy(final ImageRelativeUrl imageRelativeUrl, final Long id) {
-        if (id == null) {
-            return null;
-        }
-
         final String absoluteUrl = imageRelativeUrl.getAbsoluteUrl();
 
         return absoluteUrl.concat(String.valueOf(id));
     }
 
     public static String calculateBy(final String imageAbsoluteUrl, final Long id) {
-        if (id == null) {
-            return null;
-        }
-
         return imageAbsoluteUrl.concat(String.valueOf(id));
     }
 }
