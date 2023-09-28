1a7523d9cf23fcf0927fce89930b4712cdf78c85
apptie <appti@naver.com>
2023-09-28 21:26:37 +0900

    test: KakaoUserInformationProviderTest Fixture 추가 및 테스트 케이스 리펙토링

--- backend/ddang/src/test/java/com/ddang/ddang/authentication/infrastructure/oauth2/kakao/KakaoUserInformationProviderTest.java
+++ backend/ddang/src/test/java/com/ddang/ddang/authentication/infrastructure/oauth2/kakao/KakaoUserInformationProviderTest.java
@@ -1,22 +1,5 @@
 package com.ddang.ddang.authentication.infrastructure.oauth2.kakao;
 
-import com.ddang.ddang.authentication.configuration.KakaoProvidersConfigurationProperties;
-import com.ddang.ddang.authentication.configuration.Oauth2PropertiesConfiguration;
-import com.ddang.ddang.authentication.domain.dto.UserInformationDto;
-import com.ddang.ddang.authentication.infrastructure.oauth2.Oauth2Type;
-import com.ddang.ddang.configuration.RestTemplateConfiguration;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.web.client.RestTemplate;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.hamcrest.Matchers.matchesPattern;
@@ -24,30 +7,21 @@ import static org.springframework.test.web.client.match.MockRestRequestMatchers.
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withUnauthorizedRequest;
 
-@RestClientTest({KakaoUserInformationProvider.class})
-@Import({RestTemplateConfiguration.class, Oauth2PropertiesConfiguration.class})
+import com.ddang.ddang.authentication.domain.dto.UserInformationDto;
+import com.ddang.ddang.authentication.infrastructure.oauth2.Oauth2Type;
+import com.ddang.ddang.authentication.infrastructure.oauth2.kakao.fixture.KakaoUserInformationProviderFixture;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
-class KakaoUserInformationProviderTest {
-
-    MockRestServiceServer mockRestServiceServer;
-
-    @Autowired
-    RestTemplate restTemplate;
+class KakaoUserInformationProviderTest extends KakaoUserInformationProviderFixture {
 
     @Autowired
     KakaoUserInformationProvider provider;
-
-    @Autowired
-    KakaoProvidersConfigurationProperties kakaoProperties;
-
-    @Autowired
-    ObjectMapper objectMapper;
-
-    @BeforeEach
-    void setUp() {
-        mockRestServiceServer = MockRestServiceServer.createServer(restTemplate);
-    }
 
     @Test
     void 지원하는_소셜_로그인_타입을_반환한다() {
@@ -59,35 +33,29 @@ class KakaoUserInformationProviderTest {
     @Test
     void 유효한_카카오_토큰을_전달한_경우_회원_정보를_조회한다() throws Exception {
         // given
-        final UserInformationDto userInformationDto = new UserInformationDto(12345L);
-
-        mockRestServiceServer.expect(requestTo(matchesPattern(kakaoProperties.userInfoUri())))
+        카카오_인증_서버.expect(requestTo(matchesPattern(카카오_소셜_로그인_설정.userInfoUri())))
                              .andRespond(
                                      withSuccess(
-                                             objectMapper.writeValueAsString(userInformationDto),
+                                             objectMapper.writeValueAsString(회원_정보),
                                              MediaType.APPLICATION_JSON
                                      )
                              );
 
-        final String accessToken = "Bearer accessToken";
-
         // when
-        final UserInformationDto actual = provider.findUserInformation(accessToken);
+        final UserInformationDto actual = provider.findUserInformation(유효한_토큰);
 
         // then
-        assertThat(actual.id()).isEqualTo(userInformationDto.id());
+        assertThat(actual.id()).isEqualTo(회원_정보.id());
     }
 
     @Test
     void 유효하지_않은_카카오_토큰을_전달한_경우_예외가_발생한다() {
         // given
-        final String invalidAccessToken = "Bearer accessToken";
-
-        mockRestServiceServer.expect(requestTo(matchesPattern(kakaoProperties.userInfoUri())))
+        카카오_인증_서버.expect(requestTo(matchesPattern(카카오_소셜_로그인_설정.userInfoUri())))
                              .andRespond(withUnauthorizedRequest());
 
         // when & then
-        assertThatThrownBy(() -> provider.findUserInformation(invalidAccessToken))
+        assertThatThrownBy(() -> provider.findUserInformation(유효하지_않은_토큰))
                 .isInstanceOf(RuntimeException.class)
                 .hasMessage("401 Unauthorized");
     }
@@ -95,22 +63,18 @@ class KakaoUserInformationProviderTest {
     @Test
     void 유효한_카카오_토큰을_전달한_경우_카카오_연결을_끊는다() throws Exception {
         // given
-        final UserInformationDto userInformationDto = new UserInformationDto(12345L);
-
-        mockRestServiceServer.expect(requestTo(matchesPattern(kakaoProperties.userUnlinkUri())))
+        카카오_인증_서버.expect(requestTo(matchesPattern(카카오_소셜_로그인_설정.userUnlinkUri())))
                              .andRespond(
                                      withSuccess(
-                                             objectMapper.writeValueAsString(userInformationDto),
+                                             objectMapper.writeValueAsString(회원_정보),
                                              MediaType.APPLICATION_JSON
                                      )
                              );
 
-        final String oauthId = "12345";
-
         // when
-        final UserInformationDto actual = provider.unlinkUserBy(oauthId);
+        final UserInformationDto actual = provider.unlinkUserBy(카카오_회원_식별자);
 
         // then
-        assertThat(actual.id()).isEqualTo(userInformationDto.id());
+        assertThat(actual.id()).isEqualTo(회원_정보.id());
     }
 }
---
+++ backend/ddang/src/test/java/com/ddang/ddang/authentication/infrastructure/oauth2/kakao/fixture/KakaoUserInformationProviderFixture.java
@@ -0,0 +1,37 @@
+package com.ddang.ddang.authentication.infrastructure.oauth2.kakao.fixture;
+
+import com.ddang.ddang.authentication.configuration.KakaoProvidersConfigurationProperties;
+import com.ddang.ddang.authentication.configuration.Oauth2PropertiesConfiguration;
+import com.ddang.ddang.authentication.domain.dto.UserInformationDto;
+import com.ddang.ddang.authentication.infrastructure.oauth2.kakao.KakaoUserInformationProvider;
+import com.ddang.ddang.configuration.RestTemplateConfiguration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+
+@RestClientTest({KakaoUserInformationProvider.class})
+@Import({RestTemplateConfiguration.class, Oauth2PropertiesConfiguration.class})
+public class KakaoUserInformationProviderFixture {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    protected KakaoProvidersConfigurationProperties 카카오_소셜_로그인_설정;
+
+    protected ObjectMapper objectMapper = new ObjectMapper();
+    protected MockRestServiceServer 카카오_인증_서버;
+    protected String 카카오_회원_식별자 = "12345";
+    protected UserInformationDto 회원_정보 = new UserInformationDto(12345L);
+    protected String 유효한_토큰 = "Bearer accessToken";
+    protected String 유효하지_않은_토큰 = "Bearer invalidAccessToken";
+
+    @BeforeEach
+    void setUp() {
+        카카오_인증_서버 = MockRestServiceServer.createServer(restTemplate);
+    }
+}
