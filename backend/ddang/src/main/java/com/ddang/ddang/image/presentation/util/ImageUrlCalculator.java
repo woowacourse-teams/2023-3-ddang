c90c49f318274772b6b26034c633ca06debc9e65
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-09-29 18:39:55 +0900

    refactor: 타입 재수정 및 todo 추가

--- backend/ddang/src/main/java/com/ddang/ddang/image/presentation/util/ImageUrlCalculator.java
+++ backend/ddang/src/main/java/com/ddang/ddang/image/presentation/util/ImageUrlCalculator.java
@@ -5,13 +5,14 @@ public final class ImageUrlCalculator {
     private ImageUrlCalculator() {
     }
 
-    public static String calculateBy(final ImageRelativeUrl imageRelativeUrl, final long id) {
+    // TODO: 9/29/23 id 타입을 long으로 변경 및 이미지는 null이 되는 경우가 없도록 할 것
+    public static String calculateBy(final ImageRelativeUrl imageRelativeUrl, final Long id) {
         final String absoluteUrl = imageRelativeUrl.calculateAbsoluteUrl();
 
         return absoluteUrl + id;
     }
 
-    public static String calculateBy(final String imageAbsoluteUrl, final long id) {
+    public static String calculateBy(final String imageAbsoluteUrl, final Long id) {
         return imageAbsoluteUrl + id;
     }
 }
