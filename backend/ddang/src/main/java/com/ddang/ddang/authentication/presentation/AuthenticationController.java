c641416943e1a6ec058066d907b642f99dd1252e
apptie <appti@naver.com>
2023-09-29 12:59:55 +0900

    refactor: 로그인 메서드 네이밍 변경

--- backend/ddang/src/main/java/com/ddang/ddang/authentication/presentation/AuthenticationController.java
+++ backend/ddang/src/main/java/com/ddang/ddang/authentication/presentation/AuthenticationController.java
@@ -31,7 +31,7 @@ public class AuthenticationController {
     private final BlackListTokenService blackListTokenService;
 
     @PostMapping("/login/{oauth2Type}")
-    public ResponseEntity<Object> validate(
+    public ResponseEntity<Object> login(
             @PathVariable final Oauth2Type oauth2Type,
             @RequestBody final LoginTokenRequest request
     ) {
