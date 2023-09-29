476c98654a9a14c4b06f609e56480fe1dd668f80
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-09-29 16:43:07 +0900

    refactor: 메서드 파라미터 타입을 명확하게 수정

--- backend/ddang/src/main/java/com/ddang/ddang/image/presentation/util/ImageUrlCalculator.java
+++ backend/ddang/src/main/java/com/ddang/ddang/image/presentation/util/ImageUrlCalculator.java
@@ -5,13 +5,13 @@ public final class ImageUrlCalculator {
     private ImageUrlCalculator() {
     }
 
-    public static String calculateBy(final ImageRelativeUrl imageRelativeUrl, final Long id) {
+    public static String calculateBy(final ImageRelativeUrl imageRelativeUrl, final long id) {
         final String absoluteUrl = imageRelativeUrl.calculateAbsoluteUrl();
 
         return absoluteUrl + id;
     }
 
-    public static String calculateBy(final String imageAbsoluteUrl, final Long id) {
+    public static String calculateBy(final String imageAbsoluteUrl, final long id) {
         return imageAbsoluteUrl + id;
     }
 }
