b0f54dcb06fd689ded3c00e6c18d864277f157a8
apptie <appti@naver.com>
2023-09-29 11:40:18 +0900

    feat: MdcFilter가 test 환경에서는 빈으로 등록되지 않도록 어노테이션 추가

--- backend/ddang/src/main/java/com/ddang/ddang/configuration/log/MdcFilter.java
+++ backend/ddang/src/main/java/com/ddang/ddang/configuration/log/MdcFilter.java
@@ -11,11 +11,13 @@ import java.io.IOException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.MDC;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
+@Profile("!test")
 @RequiredArgsConstructor
 public class MdcFilter extends OncePerRequestFilter {
 
