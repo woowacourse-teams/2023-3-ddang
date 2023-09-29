5109d2d21ae1bf2803d4e9b415528eac6514ae9c
kwonyj1022 <kwonyj1022@naver.com>
2023-09-30 03:23:42 +0900

    test: QuerydslChatRoomAndImageRepositoryImplTest Fixture 추가 및 테스트 케이스 리팩토링

---
+++ backend/ddang/src/test/java/com/ddang/ddang/chat/infrastructure/persistence/fixture/QuerydslChatRoomAndImageRepositoryImplFixture.java
@@ -0,0 +1,102 @@
+package com.ddang.ddang.chat.infrastructure.persistence.fixture;
+
+import com.ddang.ddang.auction.domain.Auction;
+import com.ddang.ddang.auction.domain.BidUnit;
+import com.ddang.ddang.auction.domain.Price;
+import com.ddang.ddang.auction.infrastructure.persistence.JpaAuctionRepository;
+import com.ddang.ddang.bid.domain.Bid;
+import com.ddang.ddang.bid.domain.BidPrice;
+import com.ddang.ddang.bid.infrastructure.persistence.JpaBidRepository;
+import com.ddang.ddang.category.domain.Category;
+import com.ddang.ddang.category.infrastructure.persistence.JpaCategoryRepository;
+import com.ddang.ddang.chat.domain.ChatRoom;
+import com.ddang.ddang.chat.infrastructure.persistence.JpaChatRoomRepository;
+import com.ddang.ddang.image.domain.AuctionImage;
+import com.ddang.ddang.image.domain.ProfileImage;
+import com.ddang.ddang.image.infrastructure.persistence.JpaAuctionImageRepository;
+import com.ddang.ddang.image.infrastructure.persistence.JpaProfileImageRepository;
+import com.ddang.ddang.user.domain.User;
+import com.ddang.ddang.user.infrastructure.persistence.JpaUserRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@SuppressWarnings("NonAsciiCharacters")
+public class QuerydslChatRoomAndImageRepositoryImplFixture {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Autowired
+    private JpaCategoryRepository categoryRepository;
+
+    @Autowired
+    private JpaProfileImageRepository profileImageRepository;
+
+    @Autowired
+    private JpaUserRepository userRepository;
+
+    @Autowired
+    private JpaAuctionImageRepository auctionImageRepository;
+
+    @Autowired
+    private JpaAuctionRepository auctionRepository;
+
+    @Autowired
+    private JpaBidRepository bidRepository;
+
+    @Autowired
+    private JpaChatRoomRepository chatRoomRepository;
+
+    private Category 전자기기_카테고리 = new Category("전자기기");
+    private Category 전자기기_서브_노트북_카테고리 = new Category("노트북 카테고리");
+    private ProfileImage 프로필_이미지 = new ProfileImage("upload.png", "store.png");
+    private User 판매자 = User.builder()
+                           .name("판매자")
+                           .profileImage(프로필_이미지)
+                           .reliability(4.7d)
+                           .oauthId("12345")
+                           .build();
+    private User 구매자 = User.builder()
+                             .name("구매자")
+                             .profileImage(프로필_이미지)
+                             .reliability(4.7d)
+                             .oauthId("12346")
+                             .build();
+    private AuctionImage 대표_이미지가_아닌_경매_이미지 =
+            new AuctionImage("대표 이미지가_아닌_경매_이미지.png", "대표 이미지가_아닌_경매_이미지.png");
+    private Auction 경매 = Auction.builder()
+                                  .seller(판매자)
+                                  .title("맥북")
+                                  .description("맥북 팔아요")
+                                  .subCategory(전자기기_서브_노트북_카테고리)
+                                  .startPrice(new Price(10_000))
+                                  .bidUnit(new BidUnit(1_000))
+                                  .closingTime(LocalDateTime.now())
+                                  .build();
+    private Bid 입찰 = new Bid(경매, 구매자, new BidPrice(15_000));
+
+    protected AuctionImage 경매_대표_이미지 = new AuctionImage("경매_대표_이미지.png", "경매_대표_이미지.png");
+    protected ChatRoom 채팅방 = new ChatRoom(경매, 구매자);
+
+    @BeforeEach
+    void setUp() {
+        전자기기_카테고리.addSubCategory(전자기기_서브_노트북_카테고리);
+        categoryRepository.save(전자기기_카테고리);
+        profileImageRepository.save(프로필_이미지);
+        userRepository.saveAll(List.of(판매자, 구매자));
+        auctionImageRepository.saveAll(List.of(경매_대표_이미지, 대표_이미지가_아닌_경매_이미지));
+        경매.addAuctionImages(List.of(경매_대표_이미지, 대표_이미지가_아닌_경매_이미지));
+        auctionRepository.save(경매);
+        bidRepository.save(입찰);
+        경매.updateLastBid(입찰);
+        chatRoomRepository.save(채팅방);
+
+        em.flush();
+        em.clear();
+    }
+}
