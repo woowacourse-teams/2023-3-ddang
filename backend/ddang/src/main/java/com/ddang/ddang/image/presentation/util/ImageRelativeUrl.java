e54fcabcc240fe34f1d44d6206aa46d0c4797f48
제이미 <63184334+JJ503@users.noreply.github.com>
2023-09-29 19:00:33 +0900

    refactor: #469 이미지 url 관련 네이밍 수정 (#470)
    * refactor: 반환되는 이미지 타입을 jpeg로 변경
    
    * refactor: baseUrl에 대한 네이밍을 보다 명확하게 수정
    
    * refactor: ImageUrlCalculator를 사용하도록 수정
    
    * refactor: 메서드 네이밍을 보다 명확하게 수정
    
    * test: 실패하는 테스트 수정
    
    * refactor: 중복된 메서드 통합하기
    
    * test: 테스트 추가
    
    * refactor: 변수 명을 명확하게 수정
    
    * style: todo 제거
    
    * refactor: 필요없는 로직 제거
    
    * refactor: 메서드명을 명확하게 수정
    
    * refactor: 메서드 분리가 필요 없는 경우에 대해 통합하도록 수정
    
    * refactor: concat 대신 +를 사용하도록 수정
    
    * docs: 추후 리팩토링이 필요한 내용에 대한 todo 작성
    
    * test: 필요 없어진 테스트 제거
    
    * style: 정렬 수정
    
    * refactor: 메서드 파라미터 명을 명확하게 수정
    
    * refactor: 메서드 파라미터 타입을 명확하게 수정
    
    * refactor: 타입 재수정 및 todo 추가

---
+++ backend/ddang/src/main/java/com/ddang/ddang/image/presentation/util/ImageRelativeUrl.java
@@ -0,0 +1,23 @@
+package com.ddang.ddang.image.presentation.util;
+
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+public enum ImageRelativeUrl {
+
+    AUCTION("/auctions/images/"),
+    USER("/users/images/");
+
+    private final String value;
+
+    ImageRelativeUrl(final String value) {
+        this.value = value;
+    }
+
+    public String calculateAbsoluteUrl() {
+        final String imageBaseUrl = ServletUriComponentsBuilder.fromCurrentContextPath()
+                                                               .build()
+                                                               .toUriString();
+
+        return imageBaseUrl + value;
+    }
+}
