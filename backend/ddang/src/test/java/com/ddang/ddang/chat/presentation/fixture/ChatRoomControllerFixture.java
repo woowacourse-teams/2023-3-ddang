43e1cf10c795c3d7e6ef3f053619939d7f423d45
swonny <wonny921@gmail.com>
2023-09-30 16:33:47 +0900

    style: 네이밍 및 final 사용 통일

--- backend/ddang/src/test/java/com/ddang/ddang/chat/presentation/fixture/ChatRoomControllerFixture.java
+++ backend/ddang/src/test/java/com/ddang/ddang/chat/presentation/fixture/ChatRoomControllerFixture.java
@@ -20,14 +20,14 @@ import static org.mockito.Mockito.mock;
 public class ChatRoomControllerFixture extends CommonControllerSliceTest {
 
     protected TokenDecoder tokenDecoder = mock(TokenDecoder.class);
-    protected final PrivateClaims 비공개_클레임 = new PrivateClaims(1L);
+    protected PrivateClaims 비공개_클레임 = new PrivateClaims(1L);
     protected ReadUserInChatRoomDto 판매자 = new ReadUserInChatRoomDto(1L, "사용자1", 1L, 5.0d, false);
     private ReadUserInChatRoomDto 구매자1 = new ReadUserInChatRoomDto(2L, "사용자2", 2L, 5.0d, false);
     private ReadUserInChatRoomDto 구매자2 = new ReadUserInChatRoomDto(3L, "사용자3", 3L, 5.0d, false);
     private ReadAuctionInChatRoomDto 조회용_경매1 = new ReadAuctionInChatRoomDto(1L, "경매1", 10_000, 1L);
-    private final ReadAuctionInChatRoomDto 조회용_경매_dto2 = new ReadAuctionInChatRoomDto(2L, "경매2", 20_000, 1L);
+    private ReadAuctionInChatRoomDto 조회용_경매2 = new ReadAuctionInChatRoomDto(2L, "경매2", 20_000, 1L);
     protected ReadChatRoomWithLastMessageDto 조회용_채팅방1 = new ReadChatRoomWithLastMessageDto(1L, 조회용_경매1, 구매자1, new ReadLastMessageDto(1L, LocalDateTime.now(), 판매자, 구매자1, "메시지1"), true);
-    protected ReadChatRoomWithLastMessageDto 조회용_채팅방2 = new ReadChatRoomWithLastMessageDto(2L, 조회용_경매_dto2, 구매자2, new ReadLastMessageDto(1L, LocalDateTime.now(), 판매자, 구매자2, "메시지2"), true);
+    protected ReadChatRoomWithLastMessageDto 조회용_채팅방2 = new ReadChatRoomWithLastMessageDto(2L, 조회용_경매2, 구매자2, new ReadLastMessageDto(1L, LocalDateTime.now(), 판매자, 구매자2, "메시지2"), true);
     protected CreateMessageRequest 메시지_생성_요청 = new CreateMessageRequest(1L, "메시지 내용");
     protected CreateMessageRequest 유효하지_않은_발신자의_메시지_생성_요청 = new CreateMessageRequest(-999L, "메시지 내용");
     protected ReadMessageDto 조회용_메시지 = new ReadMessageDto(1L, LocalDateTime.now(), 1L, 1L, 1L, "메시지내용");
