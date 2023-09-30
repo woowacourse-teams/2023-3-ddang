6f30f6343cd58efcfcbaa74341ff2f0a6f19771f
kwonyj1022 <kwonyj1022@naver.com>
2023-09-30 13:18:33 +0900

    test: 모든 픽스처 객체를 `@BeforeEach`로 세팅하도록 변경

--- backend/ddang/src/test/java/com/ddang/ddang/region/infrastructure/api/fixture/RestTemplateInitRegionProcessorFixture.java
+++ backend/ddang/src/test/java/com/ddang/ddang/region/infrastructure/api/fixture/RestTemplateInitRegionProcessorFixture.java
@@ -3,6 +3,7 @@ package com.ddang.ddang.region.infrastructure.api.fixture;
 import com.ddang.ddang.region.infrastructure.api.dto.ApiAccessTokenResponse;
 import com.ddang.ddang.region.infrastructure.api.dto.ResultApiRegionResponse;
 import com.ddang.ddang.region.infrastructure.api.dto.TotalApiRegionResponse;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.util.Collections;
 import java.util.List;
@@ -17,20 +18,33 @@ public class RestTemplateInitRegionProcessorFixture {
     private String 단계별_지역_목록_조회_URI = "https://sgisapi.kostat.go.kr/OpenAPI3/addr/stage.json";
 
     protected String 액세스_토큰_요청_URI_패턴 = "https://sgisapi.kostat.go.kr/OpenAPI3/auth/authentication.json.*";
-    protected ApiAccessTokenResponse 유효한_액세스_토큰 = new ApiAccessTokenResponse(
-            Collections.singletonMap("accessToken", 액세스_토큰)
-    );
-    protected ApiAccessTokenResponse 유효하지_않은_액세스_토큰 = new ApiAccessTokenResponse(null);
     protected String 단계별_지역_목록_조회_URI_패턴 = 단계별_지역_목록_조회_URI + ".*";
     protected String 첫번쪠_지역_목록_조회_URI = 단계별_지역_목록_조회_URI + "?accessToken=" + 액세스_토큰;
     protected String 서울특별시_하위_지역_목록_조회_URI =
             단계별_지역_목록_조회_URI + "?accessToken=" + 액세스_토큰 + "&cd=" + 서울특별시_지역_코드;
     protected String 서울특별시_강남구_하위_지역_목록_조회_URI =
             단계별_지역_목록_조회_URI + "?accessToken=" + 액세스_토큰 + "&cd=" + 강남구_지역_코드;
-    protected ResultApiRegionResponse 서울특별시 = new ResultApiRegionResponse("서울특별시", 서울특별시_지역_코드);
-    protected ResultApiRegionResponse 강남구 = new ResultApiRegionResponse("강남구", 강남구_지역_코드);
-    protected ResultApiRegionResponse 개포1동 = new ResultApiRegionResponse("개포1동", 개포1동_지역_코드);
-    protected TotalApiRegionResponse 첫번째_지역_목록_조회_응답 = new TotalApiRegionResponse(List.of(서울특별시));
-    protected TotalApiRegionResponse 서울특별시_하위_지역_목록_조회_응답 = new TotalApiRegionResponse(List.of(강남구));
-    protected TotalApiRegionResponse 서울특별시_강남구_하위_지역_목록_조회_응답 = new TotalApiRegionResponse(List.of(개포1동));
+    protected ApiAccessTokenResponse 유효한_액세스_토큰;
+    protected ApiAccessTokenResponse 유효하지_않은_액세스_토큰;
+    protected ResultApiRegionResponse 서울특별시;
+    protected ResultApiRegionResponse 강남구;
+    protected ResultApiRegionResponse 개포1동;
+    protected TotalApiRegionResponse 첫번째_지역_목록_조회_응답;
+    protected TotalApiRegionResponse 서울특별시_하위_지역_목록_조회_응답;
+    protected TotalApiRegionResponse 서울특별시_강남구_하위_지역_목록_조회_응답;
+
+    @BeforeEach
+    void setUp() {
+        유효한_액세스_토큰 = new ApiAccessTokenResponse(
+                Collections.singletonMap("accessToken", 액세스_토큰)
+        );
+        유효하지_않은_액세스_토큰 = new ApiAccessTokenResponse(null);
+
+        서울특별시 = new ResultApiRegionResponse("서울특별시", 서울특별시_지역_코드);
+        강남구 = new ResultApiRegionResponse("강남구", 강남구_지역_코드);
+        개포1동 = new ResultApiRegionResponse("개포1동", 개포1동_지역_코드);
+        첫번째_지역_목록_조회_응답 = new TotalApiRegionResponse(List.of(서울특별시));
+        서울특별시_하위_지역_목록_조회_응답 = new TotalApiRegionResponse(List.of(강남구));
+        서울특별시_강남구_하위_지역_목록_조회_응답 = new TotalApiRegionResponse(List.of(개포1동));
+    }
 }
