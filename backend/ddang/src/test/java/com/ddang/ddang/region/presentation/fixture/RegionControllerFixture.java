46f8903b0d5726b3d7ce1b98927ff4953a9500ed
kwonyj1022 <kwonyj1022@naver.com>
2023-09-30 14:01:08 +0900

    test: 실패하는 테스트 케이스 수정

--- backend/ddang/src/test/java/com/ddang/ddang/region/presentation/fixture/RegionControllerFixture.java
+++ backend/ddang/src/test/java/com/ddang/ddang/region/presentation/fixture/RegionControllerFixture.java
@@ -2,31 +2,16 @@ package com.ddang.ddang.region.presentation.fixture;
 
 import com.ddang.ddang.configuration.CommonControllerSliceTest;
 import com.ddang.ddang.region.application.dto.ReadRegionDto;
-import org.junit.jupiter.api.BeforeEach;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class RegionControllerFixture extends CommonControllerSliceTest {
 
-    protected ReadRegionDto 서울특별시;
-    protected ReadRegionDto 부산광역시;
-    protected ReadRegionDto 두번째_지역이_없는_첫번째_지역;
-    protected ReadRegionDto 서울특별시_강남구;
-    protected ReadRegionDto 서울특별시_강동구;
-    protected ReadRegionDto 세번째_지역이_없는_두번째_지역;
-    protected ReadRegionDto 서울특별시_강남구_개포1동;
-    protected ReadRegionDto 서울특별시_강남구_개포2동;
-
-    @BeforeEach
-    void setUp() {
-        서울특별시 = new ReadRegionDto(1L, "서울특별시");
-        부산광역시 = new ReadRegionDto(2L, "부산광역시");
-        두번째_지역이_없는_첫번째_지역 = new ReadRegionDto(3L, "두번째 지역이 없는 첫번째 지역");
-
-        서울특별시_강남구 = new ReadRegionDto(4L, "서울특별시 강남구");
-        서울특별시_강동구 = new ReadRegionDto(5L, "서울특별시 강동구");
-        세번째_지역이_없는_두번째_지역 = new ReadRegionDto(6L, "세번째 지역이 없는 두번째 지역");
-
-        서울특별시_강남구_개포1동 = new ReadRegionDto(7L, "서울특별시 강남구 개포1동");
-        서울특별시_강남구_개포2동 = new ReadRegionDto(8L, "서울특별시 강남구 개포2동");
-    }
+    protected ReadRegionDto 서울특별시 = new ReadRegionDto(1L, "서울특별시");
+    protected ReadRegionDto 부산광역시 = new ReadRegionDto(2L, "부산광역시");
+    protected ReadRegionDto 두번째_지역이_없는_첫번째_지역 = new ReadRegionDto(3L, "두번째 지역이 없는 첫번째 지역");
+    protected ReadRegionDto 서울특별시_강남구 = new ReadRegionDto(4L, "서울특별시 강남구");
+    protected ReadRegionDto 서울특별시_강동구 = new ReadRegionDto(5L, "서울특별시 강동구");
+    protected ReadRegionDto 세번째_지역이_없는_두번째_지역 = new ReadRegionDto(6L, "세번째 지역이 없는 두번째 지역");
+    protected ReadRegionDto 서울특별시_강남구_개포1동 = new ReadRegionDto(7L, "서울특별시 강남구 개포1동");
+    protected ReadRegionDto 서울특별시_강남구_개포2동 = new ReadRegionDto(8L, "서울특별시 강남구 개포2동");
 }
