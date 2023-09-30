69ea8cd6cb2c3dadea58fa92a9033aac560246cf
swonny <wonny921@gmail.com>
2023-09-30 18:24:45 +0900

    style: 변수명 변경 요청을 위한 todo 작성

--- backend/ddang/src/main/java/com/ddang/ddang/notification/application/util/NotificationProperty.java
+++ backend/ddang/src/main/java/com/ddang/ddang/notification/application/util/NotificationProperty.java
@@ -1,5 +1,6 @@
 package com.ddang.ddang.notification.application.util;
 
+// TODO: 2023/09/30 안드로이드분들께 image -> imageUrl로 변경 가능한지 여쭤보기
 public enum NotificationProperty {
 
     NOTIFICATION_TYPE("type"),
