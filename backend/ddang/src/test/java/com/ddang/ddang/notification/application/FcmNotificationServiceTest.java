abe2467e3901ccface09d3d470b36b9ba6f23a49
swonny <wonny921@gmail.com>
2023-10-01 12:59:58 +0900

    refactor: 알림 서비스 테스트 픽스처 분리

--- backend/ddang/src/test/java/com/ddang/ddang/notification/application/FcmNotificationServiceTest.java
+++ backend/ddang/src/test/java/com/ddang/ddang/notification/application/FcmNotificationServiceTest.java
@@ -1,14 +1,8 @@
 package com.ddang.ddang.notification.application;
 
 import com.ddang.ddang.configuration.IsolateDatabase;
-import com.ddang.ddang.device.domain.DeviceToken;
-import com.ddang.ddang.device.infrastructure.persistence.JpaDeviceTokenRepository;
-import com.ddang.ddang.image.domain.ProfileImage;
-import com.ddang.ddang.notification.application.dto.CreateNotificationDto;
+import com.ddang.ddang.notification.application.fixture.FcmNotificationServiceFixture;
 import com.ddang.ddang.notification.domain.NotificationStatus;
-import com.ddang.ddang.notification.domain.NotificationType;
-import com.ddang.ddang.user.domain.User;
-import com.ddang.ddang.user.infrastructure.persistence.JpaUserRepository;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
@@ -27,7 +21,7 @@ import static org.mockito.BDDMockito.given;
 @IsolateDatabase
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
-class FcmNotificationServiceTest {
+class FcmNotificationServiceTest extends FcmNotificationServiceFixture {
 
     @MockBean
     FirebaseMessaging firebaseMessaging;
@@ -35,40 +29,17 @@ class FcmNotificationServiceTest {
     @Autowired
     NotificationService notificationService;
 
-    @Autowired
-    JpaDeviceTokenRepository userDeviceTokenRepository;
-
-    @Autowired
-    JpaUserRepository userRepository;
 
     @Test
     void 알림을_전송한다() throws FirebaseMessagingException {
         // given
-        final User user = User.builder()
-                              .name("회원")
-                              .profileImage(new ProfileImage("upload.png", "store.png"))
-                              .reliability(4.7d)
-                              .oauthId("12345")
-                              .build();
-        userRepository.save(user);
-        final DeviceToken deviceToken = new DeviceToken(user, "deviceToken");
-        userDeviceTokenRepository.save(deviceToken);
-        final CreateNotificationDto createNotificationDto = new CreateNotificationDto(
-                NotificationType.MESSAGE,
-                user.getId(),
-                "제목",
-                "내용",
-                "/redirectUrl",
-                "image.png"
-        );
-
-        given(mockFirebaseMessaging.send(any(Message.class))).willReturn("returnMessage");
+        given(firebaseMessaging.send(any(Message.class))).willReturn(알림_메시지_아이디);
 
         TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
             @Override
             public void afterCommit() {
                 // when
-                final NotificationStatus actual = notificationService.send(createNotificationDto);
+                final NotificationStatus actual = notificationService.send(알림_생성_DTO);
 
                 // then
                 assertThat(actual).isEqualTo(NotificationStatus.SUCCESS);
@@ -78,53 +49,23 @@ class FcmNotificationServiceTest {
 
     @Test
     void 알림을_전송시_알림을_받을_사용자_기기_토큰을_찾을_수_없다면_실패를_반환한다() {
-        // given
-        final Long invalidUserId = -999L;
-        final CreateNotificationDto createNotificationDto = new CreateNotificationDto(
-                NotificationType.MESSAGE,
-                invalidUserId,
-                "제목",
-                "내용",
-                "/redirectUrl",
-                "image.png"
-        );
-
         // when
-        final NotificationStatus actual = notificationService.send(createNotificationDto);
+        final NotificationStatus actual = notificationService.send(기기토큰이_없는_사용자의_알림_DTO);
 
         // then
         assertThat(actual).isEqualTo(NotificationStatus.FAIL);
     }
 
     @Test
-    void 알림을_전송을_실패한다() throws FirebaseMessagingException {
+    void Fcm에_의한_알림_전송_실패시_실패를_반환한다() throws FirebaseMessagingException {
         // given
-        final User user = User.builder()
-                              .name("회원")
-                              .profileImage(new ProfileImage("upload.png", "store.png"))
-                              .reliability(4.7d)
-                              .oauthId("12345")
-                              .build();
-        userRepository.save(user);
-        final DeviceToken deviceToken = new DeviceToken(user, "deviceToken");
-        userDeviceTokenRepository.save(deviceToken);
-
-        final CreateNotificationDto invalidDto = new CreateNotificationDto(
-                NotificationType.MESSAGE,
-                user.getId(),
-                "제목",
-                "내용",
-                "/redirectUrl",
-                "image.png"
-        );
-
-        given(mockFirebaseMessaging.send(any(Message.class))).willThrow(FirebaseMessagingException.class);
+        given(firebaseMessaging.send(any(Message.class))).willThrow(FirebaseMessagingException.class);
 
         TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
             @Override
             public void afterCommit() {
                 // when
-                final NotificationStatus actual = notificationService.send(invalidDto);
+                final NotificationStatus actual = notificationService.send(알림_생성_DTO);
 
                 // then
                 assertThat(actual).isEqualTo(NotificationStatus.FAIL);
