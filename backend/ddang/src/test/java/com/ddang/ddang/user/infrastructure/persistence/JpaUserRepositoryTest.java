1d643616dcb46c4f421a6150209660809b48d94d
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-10-01 15:22:01 +0900

    test: JpaUserRepositoryTest 픽스처 추가 및 리팩토링

--- backend/ddang/src/test/java/com/ddang/ddang/user/infrastructure/persistence/JpaUserRepositoryTest.java
+++ backend/ddang/src/test/java/com/ddang/ddang/user/infrastructure/persistence/JpaUserRepositoryTest.java
@@ -1,16 +1,11 @@
 package com.ddang.ddang.user.infrastructure.persistence;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.ddang.ddang.configuration.JpaConfiguration;
 import com.ddang.ddang.configuration.QuerydslConfiguration;
-import com.ddang.ddang.image.domain.ProfileImage;
 import com.ddang.ddang.user.domain.User;
+import com.ddang.ddang.user.infrastructure.persistence.fixture.JpaUserRepositoryFixture;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-
-import java.util.Optional;
-
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -18,11 +13,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DataJpaTest
+@Import({JpaConfiguration.class, QuerydslConfiguration.class})
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
-@Import({JpaConfiguration.class, QuerydslConfiguration.class})
-class JpaUserRepositoryTest {
+class JpaUserRepositoryTest extends JpaUserRepositoryFixture {
 
     @PersistenceContext
     EntityManager em;
@@ -31,34 +30,38 @@ class JpaUserRepositoryTest {
     JpaUserRepository userRepository;
 
     @Test
-    void 존재하는_oauthId를_전달하면_해당_회원을_Optional로_감싸_반환한다() {
+    void 사용자를_저장한다() {
         // given
         final User user = User.builder()
-                              .name("회원")
-                              .profileImage(new ProfileImage("upload.png", "store.png"))
+                              .name("새로운 사용자")
+                              .profileImage(프로필_이미지)
                               .reliability(4.7d)
                               .oauthId("12345")
                               .build();
 
+        // when
         userRepository.save(user);
 
+        // then
         em.flush();
         em.clear();
 
+        assertThat(user.getId()).isPositive();
+    }
+
+    @Test
+    void 존재하는_oauthId를_전달하면_해당_회원을_Optional로_감싸_반환한다() {
         // when
-        final Optional<User> actual = userRepository.findByOauthIdAndDeletedIsFalse(user.getOauthId());
+        final Optional<User> actual = userRepository.findByOauthIdAndDeletedIsFalse(사용자.getOauthId());
 
         // then
-        assertThat(actual).isPresent();
+        assertThat(actual).contains(사용자);
     }
 
     @Test
     void 존재하지_않는_oauthId를_전달하면_해당_회원을_빈_Optional로_반환한다() {
-        // given
-        final String invalidOauthId = "invalidOauthId";
-
         // when
-        final Optional<User> actual = userRepository.findByOauthIdAndDeletedIsFalse(invalidOauthId);
+        final Optional<User> actual = userRepository.findByOauthIdAndDeletedIsFalse(존재하지_않는_oauth_아이디);
 
         // then
         assertThat(actual).isEmpty();
@@ -66,44 +69,17 @@ class JpaUserRepositoryTest {
 
     @Test
     void 회원가입과_탈퇴하지_않은_회원_id를_전달하면_해당_회원을_Optional로_감싸_반환한다() {
-        // given
-        final User user = User.builder()
-                              .name("회원")
-                              .profileImage(new ProfileImage("upload.png", "store.png"))
-                              .reliability(4.7d)
-                              .oauthId("12345")
-                              .build();
-
-        userRepository.save(user);
-
-        em.flush();
-        em.clear();
-
         // when
-        final Optional<User> actual = userRepository.findByIdAndDeletedIsFalse(user.getId());
+        final Optional<User> actual = userRepository.findByIdAndDeletedIsFalse(사용자.getId());
 
         // then
-        assertThat(actual).isPresent();
+        assertThat(actual).contains(사용자);
     }
 
     @Test
     void 회원탈퇴한_회원의_id를_전달하면_빈_Optional을_반환한다() {
-        // given
-        final User user = User.builder()
-                              .name("회원")
-                              .profileImage(new ProfileImage("upload.png", "store.png"))
-                              .reliability(4.7d)
-                              .oauthId("12345")
-                              .build();
-
-        user.withdrawal();
-        userRepository.save(user);
-
-        em.flush();
-        em.clear();
-
         // when
-        final Optional<User> actual = userRepository.findByIdAndDeletedIsFalse(user.getId());
+        final Optional<User> actual = userRepository.findByIdAndDeletedIsFalse(탈퇴한_사용자.getId());
 
         // then
         assertThat(actual).isEmpty();
@@ -111,11 +87,8 @@ class JpaUserRepositoryTest {
 
     @Test
     void 없는_id를_전달하면_빈_Optional을_반환한다() {
-        // given
-        final Long invalidUserId = -999L;
-
         // when
-        final Optional<User> actual = userRepository.findByIdAndDeletedIsFalse(invalidUserId);
+        final Optional<User> actual = userRepository.findByIdAndDeletedIsFalse(존재하지_않는_사용자_아이디);
 
         // then
         assertThat(actual).isEmpty();
@@ -123,22 +96,8 @@ class JpaUserRepositoryTest {
 
     @Test
     void 회원탈퇴한_회원의_id를_전달하면_참을_반환한다() {
-        // given
-        final User user = User.builder()
-                              .name("회원")
-                              .profileImage(new ProfileImage("upload.png", "store.png"))
-                              .reliability(4.7d)
-                              .oauthId("12345")
-                              .build();
-
-        user.withdrawal();
-        userRepository.save(user);
-
-        em.flush();
-        em.clear();
-
         // when
-        final boolean actual = userRepository.existsByIdAndDeletedIsTrue(user.getId());
+        final boolean actual = userRepository.existsByIdAndDeletedIsTrue(탈퇴한_사용자.getId());
 
         // then
         assertThat(actual).isTrue();
@@ -146,21 +105,8 @@ class JpaUserRepositoryTest {
 
     @Test
     void 회원탈퇴하지_않거나_회원가입하지_않은_회원의_id를_전달하면_거짓을_반환한다() {
-        // given
-        final User user = User.builder()
-                              .name("회원")
-                              .profileImage(new ProfileImage("upload.png", "store.png"))
-                              .reliability(4.7d)
-                              .oauthId("12345")
-                              .build();
-
-        userRepository.save(user);
-
-        em.flush();
-        em.clear();
-
         // when
-        final boolean actual = userRepository.existsByIdAndDeletedIsTrue(user.getId());
+        final boolean actual = userRepository.existsByIdAndDeletedIsTrue(사용자.getId());
 
         // then
         assertThat(actual).isFalse();
@@ -168,11 +114,8 @@ class JpaUserRepositoryTest {
 
     @Test
     void 이름이_아직_없다면_거짓을_반환한다() {
-        // given
-        final String name = "12345";
-
         // when
-        final boolean actual = userRepository.existsByNameEndingWith(name);
+        final boolean actual = userRepository.existsByNameEndingWith(존재하지_않는_사용자_이름);
 
         // then
         assertThat(actual).isFalse();
@@ -180,22 +123,8 @@ class JpaUserRepositoryTest {
 
     @Test
     void 이름이_있다면_참을_반환한다() {
-        // given
-        final String randomNumber = "54321";
-        final User user = User.builder()
-                              .name("kakao" + randomNumber)
-                              .profileImage(new ProfileImage("upload.png", "store.png"))
-                              .reliability(4.7d)
-                              .oauthId("12345")
-                              .build();
-
-        userRepository.save(user);
-
-        em.flush();
-        em.clear();
-
         // when
-        final boolean actual = userRepository.existsByNameEndingWith(randomNumber);
+        final boolean actual = userRepository.existsByNameEndingWith(존재하는_사용자_이름);
 
         // then
         assertThat(actual).isTrue();
