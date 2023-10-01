98984d3ce5f6f6c84f506643b82ddc1e950c5b83
swonny <wonny921@gmail.com>
2023-10-01 11:25:07 +0900

    refactor: 조회하는 사용자 아이디를 의미하는 변수명 명확하게 수정

--- backend/ddang/src/main/java/com/ddang/ddang/chat/infrastructure/persistence/QuerydslMessageRepositoryImpl.java
+++ backend/ddang/src/main/java/com/ddang/ddang/chat/infrastructure/persistence/QuerydslMessageRepositoryImpl.java
@@ -17,15 +17,15 @@ public class QuerydslMessageRepositoryImpl implements QuerydslMessageRepository 
     private final JPAQueryFactory queryFactory;
 
     public List<Message> findMessagesAllByLastMessageId(
-            final Long userId,
+            final Long readUserId,
             final Long chatRoomId,
             final Long lastMessageId
     ) {
         return queryFactory
                 .selectFrom(message)
                 .where(
-                        message.writer.id.eq(userId)
-                                         .or(message.receiver.id.eq(userId)),
+                        message.writer.id.eq(readUserId)
+                                         .or(message.receiver.id.eq(readUserId)),
                         message.chatRoom.id.eq(chatRoomId),
                         isGreaterThanLastId(lastMessageId)
                 )
