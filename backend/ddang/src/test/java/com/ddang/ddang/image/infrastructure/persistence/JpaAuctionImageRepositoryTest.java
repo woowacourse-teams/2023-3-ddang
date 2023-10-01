3e93f2eb43f84121a68279a07b470f9bb905cfe0
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-10-01 18:03:48 +0900

    test: JpaAuctionImageRepositoryTest 픽스처 추가 및 리팩토링

--- backend/ddang/src/test/java/com/ddang/ddang/image/infrastructure/persistence/JpaAuctionImageRepositoryTest.java
+++ backend/ddang/src/test/java/com/ddang/ddang/image/infrastructure/persistence/JpaAuctionImageRepositoryTest.java
@@ -1,13 +1,10 @@
 package com.ddang.ddang.image.infrastructure.persistence;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.ddang.ddang.configuration.QuerydslConfiguration;
 import com.ddang.ddang.image.domain.AuctionImage;
+import com.ddang.ddang.image.infrastructure.persistence.fixture.JpaAuctionImageRepositoryFixture;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import java.util.Optional;
-import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -15,11 +12,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DataJpaTest
+@Import(QuerydslConfiguration.class)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
-@Import(QuerydslConfiguration.class)
-class JpaAuctionImageRepositoryTest {
+class JpaAuctionImageRepositoryTest extends JpaAuctionImageRepositoryFixture {
 
     @PersistenceContext
     EntityManager em;
@@ -28,32 +29,33 @@ class JpaAuctionImageRepositoryTest {
     JpaAuctionImageRepository auctionImageRepository;
 
     @Test
-    void 지정한_아이디에_해당하는_경매_이미지를_조회한다() {
+    void 경매_이미지를_저장한다() {
         // given
-        final AuctionImage auctionImage = new AuctionImage("uploadName", "storeName");
+        final AuctionImage auctionImage = new AuctionImage(업로드_이미지_파일명, 저장된_이미지_파일명);
 
+        // when
         auctionImageRepository.save(auctionImage);
 
+        // then
         em.flush();
         em.clear();
 
+        assertThat(auctionImage.getId()).isPositive();
+    }
+
+    @Test
+    void 지정한_아이디에_해당하는_경매_이미지를_조회한다() {
         // when
-        final Optional<AuctionImage> actual = auctionImageRepository.findById(auctionImage.getId());
+        final Optional<AuctionImage> actual = auctionImageRepository.findById(경매_이미지.getId());
 
         // then
-        SoftAssertions.assertSoftly(softAssertions -> {
-            softAssertions.assertThat(actual).isPresent();
-            softAssertions.assertThat(actual.get()).isEqualTo(auctionImage);
-        });
+        assertThat(actual).contains(경매_이미지);
     }
 
     @Test
     void 지정한_아이디에_해당하는_경매_이미지가_없는_경우_빈_Optional을_반환한다() {
-        // given
-        final Long invalidAuctionImageId = -999L;
-
         // when
-        final Optional<AuctionImage> actual = auctionImageRepository.findById(invalidAuctionImageId);
+        final Optional<AuctionImage> actual = auctionImageRepository.findById(존재하지_않는_경매_이미지_아이디);
 
         // then
         assertThat(actual).isEmpty();
