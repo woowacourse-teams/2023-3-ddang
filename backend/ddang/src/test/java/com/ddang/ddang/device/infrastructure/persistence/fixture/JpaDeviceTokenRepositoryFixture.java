d64c2577d1bad30ab92f0450a5acec1fa8cccba2
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2023-10-03 01:06:03 +0900

    refactor: #480 디바이스 토큰 관련 테스트 코드 리팩토링 (#491)
    * test: DeviceTokenTest Fixture 추가 및 테스트 케이스 리팩토링
    
    * test: JpaDeviceTokenRepositoryTest Fixture 추가 및 테스트 케이스 리팩토링
    
    * test: DeviceTokenServiceTest Fixture 추가 및 테스트 케이스 리팩토링
    
    * test: DeviceTokenControllerTest Fixture 추가 및 테스트 케이스 리팩토링
    
    * test: 모든 픽스처 객체를 `@BeforeEach`로 세팅하도록 변경
    
    * test: 테스트 픽스처 추가, 네이밍 변경, 코드 스타일 개선
    
    * style: 불필요한 공백 제거
    
    * rename: 테스트하려는 결과의 변수명을 actual로 통일

---
+++ backend/ddang/src/test/java/com/ddang/ddang/device/infrastructure/persistence/fixture/JpaDeviceTokenRepositoryFixture.java
@@ -0,0 +1,45 @@
+package com.ddang.ddang.device.infrastructure.persistence.fixture;
+
+import com.ddang.ddang.device.domain.DeviceToken;
+import com.ddang.ddang.device.infrastructure.persistence.JpaDeviceTokenRepository;
+import com.ddang.ddang.image.domain.ProfileImage;
+import com.ddang.ddang.user.domain.User;
+import com.ddang.ddang.user.infrastructure.persistence.JpaUserRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SuppressWarnings("NonAsciiCharacters")
+public class JpaDeviceTokenRepositoryFixture {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Autowired
+    private JpaUserRepository userRepository;
+
+    @Autowired
+    private JpaDeviceTokenRepository deviceTokenRepository;
+
+    protected User 사용자;
+    protected DeviceToken 사용자의_디바이스_토큰;
+
+    @BeforeEach
+    void setUp() {
+        사용자 = User.builder()
+                    .name("사용자")
+                    .profileImage(new ProfileImage("upload.png", "store.png"))
+                    .reliability(4.7d)
+                    .oauthId("12345")
+                    .build();
+
+        사용자의_디바이스_토큰 = new DeviceToken(사용자, "deviceToken");
+
+        userRepository.save(사용자);
+        deviceTokenRepository.save(사용자의_디바이스_토큰);
+
+        em.flush();
+        em.clear();
+    }
+}
