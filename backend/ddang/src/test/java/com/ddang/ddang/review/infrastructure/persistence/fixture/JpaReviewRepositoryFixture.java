762a2abd69a1cff53fd9ac8dbdd3ae6d3619498e
kwonyj1022 <kwonyj1022@naver.com>
2023-10-03 16:58:25 +0900

    test: 픽스처 객체 생성을 `@beforeEach`에서 하도록 변경

--- backend/ddang/src/test/java/com/ddang/ddang/review/infrastructure/persistence/fixture/JpaReviewRepositoryFixture.java
+++ backend/ddang/src/test/java/com/ddang/ddang/review/infrastructure/persistence/fixture/JpaReviewRepositoryFixture.java
@@ -39,93 +39,105 @@ public class JpaReviewRepositoryFixture {
     private JpaReviewRepository reviewRepository;
 
 
-    private Category 전자기기_카테고리 = new Category("전자기기");
-    private ProfileImage 평가_안한_판매자_프로필_이미지 = new ProfileImage("no_review_seller_profile.png", "no_review_seller_profile.png");
-    private Category 전자기기_서브_노트북_카테고리 = new Category("노트북 카테고리");
-    private ProfileImage 판매자1_프로필_이미지 = new ProfileImage("seller1_profile.png", "seller1_profile.png");
-    private ProfileImage 판매자2_프로필_이미지 = new ProfileImage("seller2_profile.png", "seller2_profile.png");
-    private ProfileImage 구매자_프로필_이미지 = new ProfileImage("buyer_profile.png", "buyer_profile.png");
-    private AuctionImage 경매1_대표_이미지 = new AuctionImage("경매1_대표_이미지.png", "경매1_대표_이미지.png");
-    private AuctionImage 경매2_대표_이미지 = new AuctionImage("경매2_대표_이미지.png", "경매2_대표_이미지.png");
-    private AuctionImage 평가_안한_경매_대표_이미지 = new AuctionImage("평가_안한_경매_대표_이미지.png", "평가_안한_경매_대표_이미지.png");
-    protected User 판매자1 = User.builder()
-                              .name("판매자1")
-                              .profileImage(판매자1_프로필_이미지)
-                              .reliability(4.7d)
-                              .oauthId("12345")
-                              .build();
-    protected User 판매자2 = User.builder()
-                              .name("판매자2")
-                              .profileImage(판매자2_프로필_이미지)
-                              .reliability(4.7d)
-                              .oauthId("12346")
-                              .build();
-    protected User 평가_안한_경매_판매자 = User.builder()
-                                      .name("평가 안한 판매자")
-                                      .profileImage(평가_안한_판매자_프로필_이미지)
-                                      .reliability(4.7d)
-                                      .oauthId("12346")
-                                      .build();
-    protected User 구매자 = User.builder()
-                             .name("구매자")
-                             .profileImage(구매자_프로필_이미지)
-                             .reliability(4.7d)
-                             .oauthId("12347")
-                             .build();
-    protected Auction 판매자1이_평가한_경매 = Auction.builder()
-                                            .seller(판매자1)
-                                            .title("맥북")
-                                            .description("맥북 팔아요")
-                                            .subCategory(전자기기_서브_노트북_카테고리)
-                                            .startPrice(new Price(10_000))
-                                            .bidUnit(new BidUnit(1_000))
-                                            .closingTime(LocalDateTime.now())
-                                            .build();
-    protected Auction 판매자2가_평가한_경매 = Auction.builder()
-                                            .seller(판매자2)
-                                            .title("맥북")
-                                            .description("맥북 팔아요")
-                                            .subCategory(전자기기_서브_노트북_카테고리)
-                                            .startPrice(new Price(10_000))
-                                            .bidUnit(new BidUnit(1_000))
-                                            .closingTime(LocalDateTime.now())
-                                            .build();
-    protected Auction 평가_안한_경매 = Auction.builder()
-                                        .seller(판매자2)
-                                        .title("맥북")
-                                        .description("맥북 팔아요")
-                                        .subCategory(전자기기_서브_노트북_카테고리)
-                                        .startPrice(new Price(10_000))
-                                        .bidUnit(new BidUnit(1_000))
-                                        .closingTime(LocalDateTime.now())
-                                        .build();
-    protected Review 구매자가_판매자1에게_받은_평가 = Review.builder()
-                                               .auction(판매자1이_평가한_경매)
-                                               .writer(판매자1)
-                                               .target(구매자)
-                                               .content("친절하다.")
-                                               .score(5.0d)
-                                               .build();
-    protected Review 구매자가_판매자2에게_받은_평가 = Review.builder()
-                                               .auction(판매자2가_평가한_경매)
-                                               .writer(판매자2)
-                                               .target(구매자)
-                                               .content("별로다.")
-                                               .score(1.0d)
-                                               .build();
+    protected User 판매자1;
+    protected User 판매자2;
+    protected User 평가_안한_경매_판매자;
+    protected User 구매자;
+    protected Auction 판매자1이_평가한_경매;
+    protected Auction 판매자2가_평가한_경매;
+    protected Auction 평가_안한_경매;
+    protected Review 구매자가_판매자1에게_받은_평가;
+    protected Review 구매자가_판매자2에게_받은_평가;
 
     @BeforeEach
     void setUp() {
+        final Category 전자기기_카테고리 = new Category("전자기기");
+        final Category 전자기기_서브_노트북_카테고리 = new Category("노트북 카테고리");
         전자기기_카테고리.addSubCategory(전자기기_서브_노트북_카테고리);
         categoryRepository.save(전자기기_카테고리);
 
+        final ProfileImage 평가_안한_판매자_프로필_이미지 = new ProfileImage("no_review_seller_profile.png", "no_review_seller_profile.png");
+        final ProfileImage 판매자1_프로필_이미지 = new ProfileImage("seller1_profile.png", "seller1_profile.png");
+        final ProfileImage 판매자2_프로필_이미지 = new ProfileImage("seller2_profile.png", "seller2_profile.png");
+        final ProfileImage 구매자_프로필_이미지 = new ProfileImage("buyer_profile.png", "buyer_profile.png");
+
+        판매자1 = User.builder()
+                    .name("판매자1")
+                    .profileImage(판매자1_프로필_이미지)
+                    .reliability(4.7d)
+                    .oauthId("12345")
+                    .build();
+        판매자2 = User.builder()
+                    .name("판매자2")
+                    .profileImage(판매자2_프로필_이미지)
+                    .reliability(4.7d)
+                    .oauthId("12346")
+                    .build();
+        평가_안한_경매_판매자 = User.builder()
+                               .name("평가 안한 판매자")
+                               .profileImage(평가_안한_판매자_프로필_이미지)
+                               .reliability(4.7d)
+                               .oauthId("12346")
+                               .build();
+        구매자 = User.builder()
+                   .name("구매자")
+                   .profileImage(구매자_프로필_이미지)
+                   .reliability(4.7d)
+                   .oauthId("12347")
+                   .build();
         userRepository.saveAll(List.of(판매자1, 판매자2, 평가_안한_경매_판매자, 구매자));
+
+        final AuctionImage 경매1_대표_이미지 = new AuctionImage("경매1_대표_이미지.png", "경매1_대표_이미지.png");
+        final AuctionImage 경매2_대표_이미지 = new AuctionImage("경매2_대표_이미지.png", "경매2_대표_이미지.png");
+        final AuctionImage 평가_안한_경매_대표_이미지 = new AuctionImage("평가_안한_경매_대표_이미지.png", "평가_안한_경매_대표_이미지.png");
+
+        판매자1이_평가한_경매 = Auction.builder()
+                                  .seller(판매자1)
+                                  .title("맥북")
+                                  .description("맥북 팔아요")
+                                  .subCategory(전자기기_서브_노트북_카테고리)
+                                  .startPrice(new Price(10_000))
+                                  .bidUnit(new BidUnit(1_000))
+                                  .closingTime(LocalDateTime.now())
+                                  .build();
+        판매자2가_평가한_경매 = Auction.builder()
+                                  .seller(판매자2)
+                                  .title("맥북")
+                                  .description("맥북 팔아요")
+                                  .subCategory(전자기기_서브_노트북_카테고리)
+                                  .startPrice(new Price(10_000))
+                                  .bidUnit(new BidUnit(1_000))
+                                  .closingTime(LocalDateTime.now())
+                                  .build();
+        평가_안한_경매 = Auction.builder()
+                             .seller(판매자2)
+                             .title("맥북")
+                             .description("맥북 팔아요")
+                             .subCategory(전자기기_서브_노트북_카테고리)
+                             .startPrice(new Price(10_000))
+                             .bidUnit(new BidUnit(1_000))
+                             .closingTime(LocalDateTime.now())
+                             .build();
 
         판매자1이_평가한_경매.addAuctionImages(List.of(경매1_대표_이미지));
         판매자2가_평가한_경매.addAuctionImages(List.of(경매2_대표_이미지));
         평가_안한_경매.addAuctionImages(List.of(평가_안한_경매_대표_이미지));
         auctionRepository.saveAll(List.of(판매자1이_평가한_경매, 판매자2가_평가한_경매, 평가_안한_경매));
 
+        구매자가_판매자1에게_받은_평가 = Review.builder()
+                                        .auction(판매자1이_평가한_경매)
+                                        .writer(판매자1)
+                                        .target(구매자)
+                                        .content("친절하다.")
+                                        .score(5.0d)
+                                        .build();
+        구매자가_판매자2에게_받은_평가 = Review.builder()
+                                        .auction(판매자2가_평가한_경매)
+                                        .writer(판매자2)
+                                        .target(구매자)
+                                        .content("별로다.")
+                                        .score(1.0d)
+                                        .build();
         reviewRepository.saveAll(List.of(구매자가_판매자1에게_받은_평가, 구매자가_판매자2에게_받은_평가));
 
         em.flush();
--- backend/ddang/src/test/java/com/ddang/ddang/review/presentation/fixture/ReviewControllerFixture.java
+++ backend/ddang/src/test/java/com/ddang/ddang/review/presentation/fixture/ReviewControllerFixture.java
@@ -18,8 +18,10 @@ public class ReviewControllerFixture extends CommonControllerSliceTest {
     protected PrivateClaims 유효한_작성자_비공개_클레임 = new PrivateClaims(유효한_평가_작성자_아이디);
     protected Long 유효한_경매_아이디 = 1L;
     protected Long 사용자가_이미_평가한_경매_아이디 = 2L;
-    protected CreateReviewRequest 사용자_평가_등록_요청 = new CreateReviewRequest(유효한_경매_아이디, 유효한_평가_대상_아이디, "친절하다.", 5.0d);
-    protected CreateReviewRequest 중복된_평가_등록_요청 = new CreateReviewRequest(사용자가_이미_평가한_경매_아이디, 유효한_평가_대상_아이디, "친절하다.", 5.0d);
+    protected CreateReviewRequest 사용자_평가_등록_요청 =
+            new CreateReviewRequest(유효한_경매_아이디, 유효한_평가_대상_아이디, "친절하다.", 5.0d);
+    protected CreateReviewRequest 중복된_평가_등록_요청 =
+            new CreateReviewRequest(사용자가_이미_평가한_경매_아이디, 유효한_평가_대상_아이디, "친절하다.", 5.0d);
     protected Long 생성된_평가_아이디 = 1L;
     protected Long 판매자1_프로필_이미지_아이디 = 3L;
     protected Long 판매자2_프로필_이미지_아이디 = 4L;
@@ -29,8 +31,10 @@ public class ReviewControllerFixture extends CommonControllerSliceTest {
     protected ReadUserInReviewDto 구매자 = new ReadUserInReviewDto(5L, "구매자", 구매자_프로필_이미지_아이디, 5.0d, "12349");
     private Long 구매자가_판매자1에게_받은_평가_아이디 = 2L;
     private Long 구매자가_판매자2에게_받은_평가_아이디 = 3L;
-    protected ReadReviewDto 구매자가_판매자1에게_받은_평가 = new ReadReviewDto(구매자가_판매자1에게_받은_평가_아이디, 판매자1, "친절하다.", 5.0d, LocalDateTime.now());
-    protected ReadReviewDto 구매자가_판매자2에게_받은_평가 = new ReadReviewDto(구매자가_판매자2에게_받은_평가_아이디, 판매자2, "친절하다.", 5.0d, LocalDateTime.now());
+    protected ReadReviewDto 구매자가_판매자1에게_받은_평가 =
+            new ReadReviewDto(구매자가_판매자1에게_받은_평가_아이디, 판매자1, "친절하다.", 5.0d, LocalDateTime.now());
+    protected ReadReviewDto 구매자가_판매자2에게_받은_평가 =
+            new ReadReviewDto(구매자가_판매자2에게_받은_평가_아이디, 판매자2, "친절하다.", 5.0d, LocalDateTime.now());
     protected ReadReviewDetailDto 구매자가_판매자1에게_받은_평가_내용 = new ReadReviewDetailDto(
             구매자가_판매자1에게_받은_평가.score(),
             구매자가_판매자1에게_받은_평가.content()
