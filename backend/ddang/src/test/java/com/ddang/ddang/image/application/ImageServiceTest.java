19d9e1685ef029a1cbb26a7d7a1e2d456add1b38
제이미 <63184334+JJ503@users.noreply.github.com>
2023-10-03 12:33:21 +0900

    refactor: #487 이미지 관련 테스트 코드 리팩토링 (#501)
    * test: AuctionProfileImageTest 픽스처 추가 및 리팩토링
    
    * test: LocalStoreProfileImageProcessorTest 픽스처 추가 및 리팩토링
    
    * test: JpaAuctionImageRepositoryTest 픽스처 추가 및 리팩토링
    
    * test: JpaProfileImageRepositoryTest 픽스처 추가 및 리팩토링
    
    * test: ImageIdProcessorTest 픽스처 추가 및 리팩토링
    
    * test: ProfileImageServiceTest 픽스처 추가 및 리팩토링
    
    * test: ImageUrlCalculatorTest 픽스처 추가 및 리팩토링
    
    * test: ProfileImageControllerTest 픽스처 추가 및 리팩토링
    
    * test: 반환값을 통해 비교하도록 수정
    
    * test: 변수명 오탈자 수정
    
    * test: import 와일드카드 제거
    
    * test: 클래스 네이밍 수정
    
    * test: 픽스처 필드 접근 제어자 수정

---
+++ backend/ddang/src/test/java/com/ddang/ddang/image/application/ImageServiceTest.java
@@ -0,0 +1,59 @@
+package com.ddang.ddang.image.application;
+
+
+import com.ddang.ddang.configuration.IsolateDatabase;
+import com.ddang.ddang.image.application.fixture.ImageServiceFixture;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+
+import java.net.MalformedURLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@IsolateDatabase
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class ImageServiceTest extends ImageServiceFixture {
+
+    @Autowired
+    ImageService imageService;
+
+    @Test
+    void 지정한_아이디에_해당하는_프로필_이미지를_조회한다() throws Exception {
+        // when
+        final Resource actual = imageService.readProfileImage(프로필_이미지.getId());
+
+        // then
+        assertThat(actual.getFilename()).isEqualTo(프로필_이미지_파일명);
+    }
+
+    @Test
+    void 지정한_아이디에_해당하는_프로필_이미지가_없는_경우_null을_반환한다() throws MalformedURLException {
+        // when
+        final Resource actual = imageService.readProfileImage(존재하지_않는_프로필_이미지_아이디);
+
+        // then
+        assertThat(actual).isNull();
+    }
+
+    @Test
+    void 지정한_아이디에_해당하는_경매_이미지를_조회한다() throws Exception {
+        // when
+        final Resource actual = imageService.readAuctionImage(경매_이미지.getId());
+
+        // then
+        assertThat(actual.getFilename()).isEqualTo(경매_이미지_파일명);
+    }
+
+    @Test
+    void 지정한_아이디에_해당하는_경매_이미지가_없는_경우_null을_반환한다() throws MalformedURLException {
+        // when
+        final Resource actual = imageService.readAuctionImage(존재하지_않는_경매_이미지_아이디);
+
+        // then
+        assertThat(actual).isNull();
+    }
+}
