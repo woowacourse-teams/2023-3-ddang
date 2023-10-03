ae7fbe27ef217678c86fe992d8b1e8121d194bc1
kwonyj1022 <kwonyj1022@naver.com>
2023-10-03 23:02:21 +0900

    refactor: 사용하지 않는 `@EntityGraph` 제거

--- backend/ddang/src/main/java/com/ddang/ddang/review/infrastructure/persistence/JpaReviewRepository.java
+++ backend/ddang/src/main/java/com/ddang/ddang/review/infrastructure/persistence/JpaReviewRepository.java
@@ -1,7 +1,6 @@
 package com.ddang.ddang.review.infrastructure.persistence;
 
 import com.ddang.ddang.review.domain.Review;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,7 +11,6 @@ public interface JpaReviewRepository extends JpaRepository<Review, Long> {
 
     boolean existsByAuctionIdAndWriterId(final Long auctionId, final Long writerId);
 
-    @EntityGraph(attributePaths = {"writer", "target"})
     @Query("""
             SELECT r FROM Review r JOIN FETCH r.writer w JOIN FETCH r.target t 
             WHERE t.id = :targetId 
