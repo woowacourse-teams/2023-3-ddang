70c27a0ad0c64872e083b500c887faa9accf0953
apptie <57691173+apptie@users.noreply.github.com>
2023-10-03 18:40:42 +0900

    test: #494 경매 관련 테스트 코드 리펙토링 (#504)
    * test: AuctionTest Fixture 생성 및 테스트 케이스 리펙토링
    
    * test: BidUnitTest Fixture 생성 및 테스트 케이스 리펙토링
    
    * test: PriceTest Fixture 생성 및 테스트 케이스 리펙토링
    
    * test: SortParameterTest Fixture 생성 및 테스트 케이스 리펙토링
    
    * test: AuctionServiceTest Fixture 생성 및 테스트 케이스 리펙토링
    
    * test: AuctionControllerTest Fixture 생성 및 테스트 케이스 리펙토링
    
    * test: QuerydslAuctionAndImageRepositoryImplTest Fixture 생성 및 테스트 케이스 리펙토링
    
    * test: QuerydslAuctionRepository Fixture 생성 및 테스트 케이스 리펙토링
    
    * test: JpaAuctionRepositoryTest Fixture 테스트 케이스 리펙토링
    
    * test: JpaAuctionRepositoryTest Fixture 추가 및 테스트 케이스 리펙토링
    
    * test: 사용하지 않는 Fixture 삭제
    
    * test: Fixture 및 테스트 케이스 리펙토링
    
    * test: 의미 없는 mocking 삭제
    
    * test: 클래스 확장 시 개행 제거
    
    * test: Fixture를 사용하지 않은 부분을 사용하도록 수정
    
    * test: 도메인 엔티티 테스트 환경에서 JPA 제거
    
    * test: AuctionServiceFixture 변경 및 AuctionServiceTest 테스트 케이스 수정
    
    * test: AuctionControllerFixture 변경 및 AuctionControllerTest 테스트 케이스 수정
    
    * refactor: TODO 삭제
    
    * test: AuctionControllerTest Fixture 추가 및 검증 케이스 추가
    
    * test: auction persistence 영역 테스트 어노테이션 변경 및 메서드, Fixture 네이밍을 명확하게 변경

---
+++ backend/ddang/src/test/java/com/ddang/ddang/auction/application/fixture/AuctionServiceFixture.java
@@ -0,0 +1,233 @@
+package com.ddang.ddang.auction.application.fixture;
+
+import com.ddang.ddang.auction.application.AuctionService;
+import com.ddang.ddang.auction.application.dto.CreateAuctionDto;
+import com.ddang.ddang.auction.domain.Auction;
+import com.ddang.ddang.auction.infrastructure.persistence.JpaAuctionRepository;
+import com.ddang.ddang.bid.domain.Bid;
+import com.ddang.ddang.bid.domain.BidPrice;
+import com.ddang.ddang.bid.infrastructure.persistence.JpaBidRepository;
+import com.ddang.ddang.category.domain.Category;
+import com.ddang.ddang.category.infrastructure.persistence.JpaCategoryRepository;
+import com.ddang.ddang.chat.domain.ChatRoom;
+import com.ddang.ddang.chat.infrastructure.persistence.JpaChatRoomRepository;
+import com.ddang.ddang.image.domain.ProfileImage;
+import com.ddang.ddang.image.domain.dto.StoreImageDto;
+import com.ddang.ddang.region.domain.Region;
+import com.ddang.ddang.region.infrastructure.persistence.JpaRegionRepository;
+import com.ddang.ddang.user.domain.User;
+import com.ddang.ddang.user.infrastructure.persistence.JpaUserRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+
+@SuppressWarnings("NonAsciiCharacters")
+public class AuctionServiceFixture {
+
+    @Autowired
+    private AuctionService auctionService;
+
+    @Autowired
+    private JpaAuctionRepository auctionRepository;
+
+    @Autowired
+    private JpaRegionRepository regionRepository;
+
+    @Autowired
+    private JpaCategoryRepository categoryRepository;
+
+    @Autowired
+    private JpaUserRepository userRepository;
+
+    @Autowired
+    private JpaChatRoomRepository chatRoomRepository;
+
+    @Autowired
+    private JpaBidRepository bidRepository;
+
+    private Category 가구_카테고리 = new Category("가구");
+    private Category 가구_서브_의자_카테고리 = new Category("의자");
+
+    protected User 판매자 = User.builder()
+                             .name("판매자")
+                             .profileImage(new ProfileImage("upload.png", "store.png"))
+                             .reliability(4.7d)
+                             .oauthId("12345")
+                             .build();
+    protected User 구매자 = User.builder()
+                           .name("구매자")
+                           .profileImage(new ProfileImage("upload.png", "store.png"))
+                           .reliability(4.7d)
+                           .oauthId("54321")
+                           .build();
+
+    private MockMultipartFile 경매_이미지_파일 = new MockMultipartFile(
+            "image.png",
+            "image.png",
+            MediaType.IMAGE_PNG.toString(),
+            new byte[]{1}
+    );
+
+    private Long 존재하지_않는_지역_ID = -999L;
+    private Long 존재하지_않는_카테고리_ID = -999L;
+    private BidPrice 입찰이_존재하는_경매_입찰_가격 = new BidPrice(1_000);
+
+    protected Long 존재하지_않는_사용자_ID = -999L;
+    protected Long 존재하지_않는_경매_ID = -999L;
+    protected StoreImageDto 경매_이미지_엔티티 = new StoreImageDto("upload.png", "store.png");
+    protected CreateAuctionDto 유효한_경매_생성_dto;
+    protected CreateAuctionDto 존재하지_않는_판매자의_경매_생성_dto;
+    protected CreateAuctionDto 존재하지_않는_지역의_경매_생성_dto;
+    protected CreateAuctionDto 두_번째_지역의_경매_생성_dto;
+    protected CreateAuctionDto 존재하지_않는_카테고리의_경매_생성_dto;
+    protected CreateAuctionDto 메인_카테고리의_경매_생성_dto;
+    protected CreateAuctionDto 종료되는_날이_3일_뒤인_경매_생성_dto;
+    protected CreateAuctionDto 종료된_경매_생성_dto;
+    protected Auction 채팅방이_있는_경매;
+    protected Auction 종료되는_날이_3일_뒤인_경매;
+    protected Auction 입찰이_존재하는_경매;
+    protected Auction 종료된_경매;
+    protected BidPrice 채팅방이_있는_경매_입찰_가격 = new BidPrice(10_000);
+    protected Bid 채팅방이_있는_경매_입찰;
+    protected Bid 입찰이_존재하는_경매_입찰;
+
+    @BeforeEach
+    void setUp() {
+        final Region 서울특별시 = new Region("서울특별시");
+        final Region 강남구 = new Region("강남구");
+        final Region 역삼동 = new Region("역삼동");
+
+        서울특별시.addSecondRegion(강남구);
+        강남구.addThirdRegion(역삼동);
+
+        regionRepository.save(서울특별시);
+
+        가구_카테고리.addSubCategory(가구_서브_의자_카테고리);
+
+        categoryRepository.save(가구_카테고리);
+
+        userRepository.saveAll(List.of(판매자, 구매자));
+
+        유효한_경매_생성_dto = new CreateAuctionDto(
+                "제목",
+                "내용",
+                1_000,
+                1_000,
+                LocalDateTime.now(),
+                List.of(역삼동.getId()),
+                가구_서브_의자_카테고리.getId(),
+                List.of(경매_이미지_파일),
+                판매자.getId()
+        );
+        종료된_경매_생성_dto = new CreateAuctionDto(
+                "제목",
+                "내용",
+                1_000,
+                1_000,
+                LocalDateTime.now().minusDays(3),
+                List.of(역삼동.getId()),
+                가구_서브_의자_카테고리.getId(),
+                List.of(경매_이미지_파일),
+                판매자.getId()
+        );
+        유효한_경매_생성_dto = new CreateAuctionDto(
+                "제목",
+                "내용",
+                1_000,
+                1_000,
+                LocalDateTime.now(),
+                List.of(역삼동.getId()),
+                가구_서브_의자_카테고리.getId(),
+                List.of(경매_이미지_파일),
+                판매자.getId()
+        );
+        존재하지_않는_판매자의_경매_생성_dto = new CreateAuctionDto(
+                "제목",
+                "내용",
+                1_000,
+                1_000,
+                LocalDateTime.now(),
+                List.of(역삼동.getId()),
+                가구_서브_의자_카테고리.getId(),
+                List.of(경매_이미지_파일),
+                존재하지_않는_사용자_ID
+        );
+        존재하지_않는_지역의_경매_생성_dto = new CreateAuctionDto(
+                "제목",
+                "내용",
+                1_000,
+                1_000,
+                LocalDateTime.now(),
+                List.of(존재하지_않는_지역_ID),
+                가구_서브_의자_카테고리.getId(),
+                List.of(경매_이미지_파일),
+                판매자.getId()
+        );
+        두_번째_지역의_경매_생성_dto = new CreateAuctionDto(
+                "제목",
+                "내용",
+                1_000,
+                1_000,
+                LocalDateTime.now(),
+                List.of(강남구.getId()),
+                가구_서브_의자_카테고리.getId(),
+                List.of(경매_이미지_파일),
+                판매자.getId()
+        );
+        존재하지_않는_카테고리의_경매_생성_dto = new CreateAuctionDto(
+                "제목",
+                "내용",
+                1_000,
+                1_000,
+                LocalDateTime.now(),
+                List.of(역삼동.getId()),
+                존재하지_않는_카테고리_ID,
+                List.of(경매_이미지_파일),
+                판매자.getId()
+        );
+        메인_카테고리의_경매_생성_dto = new CreateAuctionDto(
+                "제목",
+                "내용",
+                1_000,
+                1_000,
+                LocalDateTime.now(),
+                List.of(역삼동.getId()),
+                가구_카테고리.getId(),
+                List.of(경매_이미지_파일),
+                판매자.getId()
+        );
+        종료되는_날이_3일_뒤인_경매_생성_dto = new CreateAuctionDto(
+                "제목",
+                "내용",
+                1_000,
+                1_000,
+                LocalDateTime.now().plusDays(3),
+                List.of(역삼동.getId()),
+                가구_서브_의자_카테고리.getId(),
+                List.of(경매_이미지_파일),
+                판매자.getId()
+        );
+
+        채팅방이_있는_경매 = 유효한_경매_생성_dto.toEntity(판매자, 가구_서브_의자_카테고리);
+        종료되는_날이_3일_뒤인_경매 = 종료되는_날이_3일_뒤인_경매_생성_dto.toEntity(판매자, 가구_서브_의자_카테고리);
+        입찰이_존재하는_경매 = 유효한_경매_생성_dto.toEntity(판매자, 가구_서브_의자_카테고리);
+        종료된_경매 = 유효한_경매_생성_dto.toEntity(판매자, 가구_서브_의자_카테고리);
+
+        채팅방이_있는_경매_입찰 = new Bid(채팅방이_있는_경매, 구매자, 채팅방이_있는_경매_입찰_가격);
+        입찰이_존재하는_경매_입찰 = new Bid(입찰이_존재하는_경매, 구매자, 입찰이_존재하는_경매_입찰_가격);
+
+        bidRepository.saveAll(List.of(채팅방이_있는_경매_입찰, 입찰이_존재하는_경매_입찰));
+
+        채팅방이_있는_경매.updateLastBid(채팅방이_있는_경매_입찰);
+        입찰이_존재하는_경매.updateLastBid(입찰이_존재하는_경매_입찰);
+
+        auctionRepository.saveAll(List.of(채팅방이_있는_경매, 종료되는_날이_3일_뒤인_경매, 입찰이_존재하는_경매, 종료된_경매));
+
+        final ChatRoom 채팅방 = new ChatRoom(채팅방이_있는_경매, 구매자);
+
+        chatRoomRepository.save(채팅방);
+    }
+}
--- backend/ddang/src/test/java/com/ddang/ddang/auction/domain/PriceTest.java
+++ backend/ddang/src/test/java/com/ddang/ddang/auction/domain/PriceTest.java
@@ -1,16 +1,17 @@
 package com.ddang.ddang.auction.domain;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 import com.ddang.ddang.auction.domain.exception.InvalidPriceValueException;
+import com.ddang.ddang.auction.domain.fixture.PriceFixture;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
-class PriceTest {
+class PriceTest extends PriceFixture {
 
     @Test
     void 가능한_범위_내의_가격을_받는_경우_정상적으로_생성된다() {
@@ -20,22 +21,16 @@ class PriceTest {
 
     @Test
     void 최소_가격_미만으로_받는_경우_예외가_발생한다() {
-        // given
-        final int invalidPrice = -1;
-
         // when & then
-        assertThatThrownBy(() -> new Price(invalidPrice))
+        assertThatThrownBy(() -> new Price(최소_금액보다_낮은_가격))
                 .isInstanceOf(InvalidPriceValueException.class)
                 .hasMessage("가격은 0원 이상이어야 합니다.");
     }
 
     @Test
     void 최대_가격_초과하여_받는_경우_예외가_발생한다() {
-        // given
-        final int invalidPrice = Integer.MAX_VALUE;
-
         // when & then
-        assertThatThrownBy(() -> new Price(invalidPrice))
+        assertThatThrownBy(() -> new Price(최대_금액보다_높은_가격))
                 .isInstanceOf(InvalidPriceValueException.class)
                 .hasMessage("가격은 2100000000원 이하여야 합니다.");
     }
