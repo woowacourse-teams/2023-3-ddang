303b0eb9cbe54d845cbeb03c65ae2c40751801d5
swonny <wonny921@gmail.com>
2023-10-04 01:32:06 +0900

    refactor: 리다이렉트와 이미지 주소 변환 로직 이벤트 리스너로 이동

--- backend/ddang/src/main/java/com/ddang/ddang/notification/application/NotificationEventListener.java
+++ backend/ddang/src/main/java/com/ddang/ddang/notification/application/NotificationEventListener.java
@@ -1,8 +1,14 @@
 package com.ddang.ddang.notification.application;
 
+import com.ddang.ddang.auction.domain.Auction;
+import com.ddang.ddang.bid.application.dto.BidNotificationDto;
+import com.ddang.ddang.bid.application.event.BidNotificationEvent;
 import com.ddang.ddang.chat.application.dto.MessageDto;
 import com.ddang.ddang.chat.application.event.MessageNotificationEvent;
+import com.ddang.ddang.image.domain.AuctionImage;
+import com.ddang.ddang.image.presentation.util.ImageUrlCalculator;
 import com.ddang.ddang.notification.application.dto.CreateNotificationDto;
+import com.ddang.ddang.notification.domain.NotificationType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -11,11 +17,30 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class NotificationEventListener {
 
+    private static final String URI_DELIMITER = "/";
+    private static final String MESSAGE_NOTIFICATION_REDIRECT_URI = "/chattings";
+    private static final String BID_NOTIFICATION_REDIRECT_URI = "/auctions";
+    private static final String BID_NOTIFICATION_MESSAGE_FORMAT = "상위 입찰자가 나타났습니다. 구매를 원하신다면 더 높은 가격을 제시해 주세요.";
+
     private final NotificationService notificationService;
 
     @EventListener
     public void sendMessageNotification(final MessageNotificationEvent messageNotificationEvent) {
         final MessageDto messageDto = messageNotificationEvent.messageDto();
-        notificationService.send(CreateNotificationDto.from(messageDto));
+        final CreateNotificationDto createNotificationDto = new CreateNotificationDto(
+                NotificationType.MESSAGE,
+                messageDto.receiver().getId(),
+                messageDto.writer().getName(),
+                messageDto.contents(),
+                calculateRedirectUrl(MESSAGE_NOTIFICATION_REDIRECT_URI, messageDto.chatRoom().getId()),
+                ImageUrlCalculator.calculateBy(messageDto.profileImageAbsoluteUrl(), messageDto.receiver()
+                                                                                               .getProfileImage()
+                                                                                               .getId())
+        );
+        notificationService.send(createNotificationDto);
+    }
+
+    private String calculateRedirectUrl(final String uri, final Long id) {
+        return uri + URI_DELIMITER + id;
     }
 }
