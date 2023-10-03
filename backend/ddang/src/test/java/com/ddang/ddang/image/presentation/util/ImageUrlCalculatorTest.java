10ca10b07a61e502745a3257f8ac1b11dce7114d
제이미 <63184334+JJ503@users.noreply.github.com>
2023-10-03 12:33:21 +0900

    refactor: #487 이미지 관련 테스트 코드 리팩토링 (#501)
    * test: AuctionProfileImageTest 픽스처 추가 및 리팩토링
    
    * test: LocalStoreProfileImageProcessorTest 픽스처 추가 및 리팩토링
    
    * test: JpaAuctionImageRepositoryTest 픽스처 추가 및 리팩토링
    
    * test: JpaProfileImageRepositoryTest 픽스처 추가 및 리팩토링
    
    * test: ImageIdProcessorTest 픽스처 추가 및 리팩토링
    
    * test: ProfileImageServiceTest 픽스처 추가 및 리팩토링
    
    * test: ImageUrlCalculatorTest 픽스처 추가 및 리팩토링
    
    * test: ProfileImageControllerTest 픽스처 추가 및 리팩토링
    
    * test: 반환값을 통해 비교하도록 수정
    
    * test: 변수명 오탈자 수정
    
    * test: import 와일드카드 제거
    
    * test: 클래스 네이밍 수정
    
    * test: 픽스처 필드 접근 제어자 수정

--- backend/ddang/src/test/java/com/ddang/ddang/image/presentation/util/ImageUrlCalculatorTest.java
+++ backend/ddang/src/test/java/com/ddang/ddang/image/presentation/util/ImageUrlCalculatorTest.java
@@ -1,5 +1,6 @@
 package com.ddang.ddang.image.presentation.util;
 
+import com.ddang.ddang.image.presentation.util.fixture.ImageUrlCalculatorFixture;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -8,31 +9,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
-class ImageUrlCalculatorTest {
+class ImageUrlCalculatorTest extends ImageUrlCalculatorFixture {
 
     @Test
     void 프로필_사진의_URL을_계산한다() {
-        // given
-        final Long profileImageId = 1L;
-        final String absoluteUrl = "http://3-ddang.store/users/images/";
-
         // when
-        final String actual = ImageUrlCalculator.calculateBy(absoluteUrl, profileImageId);
+        final String actual = ImageUrlCalculator.calculateBy(프로필_이미지_절대_URL, 프로필_이미지_아이디);
 
         // then
-        assertThat(actual).isEqualTo(absoluteUrl + profileImageId);
+        assertThat(actual).isEqualTo(프로필_이미지_전체_URL);
     }
 
     @Test
     void 경매_대표_이미지의_URL을_계산한다() {
-        // given
-        final Long auctionImageId = 1L;
-        final String absoluteUrl = "http://3-ddang.store/users/images/";
-
         // when
-        final String actual = ImageUrlCalculator.calculateBy(absoluteUrl, auctionImageId);
+        final String actual = ImageUrlCalculator.calculateBy(경매_이미지_절대_URL, 경매_이미지_아이디);
 
         // then
-        assertThat(actual).isEqualTo(absoluteUrl + auctionImageId);
+        assertThat(actual).isEqualTo(경매_이미지_전체_URL);
     }
 }
