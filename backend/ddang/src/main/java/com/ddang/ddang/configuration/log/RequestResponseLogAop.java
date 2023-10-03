0e14778b107da93a6b2aa30b19c6b9189c664c9d
apptie <57691173+apptie@users.noreply.github.com>
2023-10-03 20:14:38 +0900

    feature: #505 요청과 응답의 내용을 출력하는 로그 추가 (#513)
    * refactor: 요청의 모든 처리 과정에 대한 로그를 출력하는 AOP 이름 변경
    
    * refactor: 로그 출력 형식 변경
    
    * refactor: 요청을 추적할 때 RestController는 제외하도록 적용 범위 변경
    
    * feat: 요청과 응답의 정보를 출력하는 로그 기능 추가
    
    * feat: 요청과 응답의 정보를 출력하는 로그 AOP 추가
    
    * chore: logback 설정 변경
    
    * ci: 계정 아이디 대소문자 통일

--- backend/ddang/src/main/java/com/ddang/ddang/configuration/log/RequestResponseLogAop.java
+++ backend/ddang/src/main/java/com/ddang/ddang/configuration/log/RequestResponseLogAop.java
@@ -11,25 +11,15 @@ import org.springframework.web.context.request.RequestContextHolder;
 @Aspect
 @Component
 @RequiredArgsConstructor
-public class LogAop {
+public class RequestResponseLogAop {
 
-    private static final String PROXY_CLASS_PREFIX = "Proxy";
-
-    private final LogTracer logTrace;
+    private final RequestResponseLogProcessor logProcessor;
 
     @Pointcut("@within(org.springframework.web.bind.annotation.RestController)")
     private void restControllerAnnotatedClass() {
     }
 
-    @Pointcut("@within(org.springframework.stereotype.Service)")
-    private void serviceAnnotatedClass() {
-    }
-
-    @Pointcut("execution(* com.ddang.ddang..*Repository+.*(..))")
-    private void repositoryClass() {
-    }
-
-    @Around("restControllerAnnotatedClass() || serviceAnnotatedClass() || repositoryClass()")
+    @Around("restControllerAnnotatedClass()")
     public Object doLog(final ProceedingJoinPoint joinPoint) throws Throwable {
         if (isNotRequestScope()) {
             return joinPoint.proceed();
@@ -37,15 +27,16 @@ public class LogAop {
 
         final String className = findClassSimpleName(joinPoint);
         final String methodName = findMethodName(joinPoint);
-        final TraceStatus status = logTrace.begin(className, methodName);
+
+        final TraceStatus status = logProcessor.begin(className, methodName, joinPoint.getArgs());
 
         try {
             final Object result = joinPoint.proceed();
 
-            logTrace.end(status, className, methodName);
+            logProcessor.end(status, className, methodName, result);
             return result;
         } catch (final Throwable ex) {
-            logTrace.exception(status, className, methodName, ex);
+            logProcessor.exception(status, className, methodName, ex);
 
             throw ex;
         }
@@ -57,12 +48,8 @@ public class LogAop {
 
     private String findClassSimpleName(final ProceedingJoinPoint joinPoint) {
         final Class<?> clazz = joinPoint.getTarget().getClass();
-        final String className = clazz.getSimpleName();
 
-        if (className.contains(PROXY_CLASS_PREFIX)) {
-            return clazz.getInterfaces()[0].getSimpleName();
-        }
-        return className;
+        return clazz.getSimpleName();
     }
 
     private String findMethodName(final ProceedingJoinPoint joinPoint) {
