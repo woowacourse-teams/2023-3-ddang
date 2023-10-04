7f051f47ab32bc603330512f807bee2964c04f92
swonny <wonny921@gmail.com>
2023-10-05 01:09:34 +0900

    test: 알림 실패 시에도 메시지 커밋되는지 확인하는 테스트 추가

--- backend/ddang/src/test/java/com/ddang/ddang/notification/application/NotificationEventListenerTest.java
+++ backend/ddang/src/test/java/com/ddang/ddang/notification/application/NotificationEventListenerTest.java
@@ -4,22 +4,32 @@ import com.ddang.ddang.bid.application.BidService;
 import com.ddang.ddang.bid.application.event.BidNotificationEvent;
 import com.ddang.ddang.chat.application.MessageService;
 import com.ddang.ddang.chat.application.event.MessageNotificationEvent;
+import com.ddang.ddang.chat.infrastructure.persistence.JpaMessageRepository;
 import com.ddang.ddang.configuration.IsolateDatabase;
 import com.ddang.ddang.notification.application.fixture.NotificationEventListenerFixture;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.event.ApplicationEvents;
 import org.springframework.test.context.event.RecordApplicationEvents;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
 @IsolateDatabase
 @RecordApplicationEvents
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
 class NotificationEventListenerTest extends NotificationEventListenerFixture {
+
+    @MockBean
+    FirebaseMessaging firebaseMessaging;
 
     @Autowired
     NotificationEventListener notificationEventListener;
@@ -29,6 +39,9 @@ class NotificationEventListenerTest extends NotificationEventListenerFixture {
 
     @Autowired
     MessageService messageService;
+
+    @Autowired
+    JpaMessageRepository messageRepository;
 
     @Autowired
     BidService bidService;
@@ -41,6 +54,20 @@ class NotificationEventListenerTest extends NotificationEventListenerFixture {
         // then
         final long actual = events.stream(MessageNotificationEvent.class).count();
         assertThat(actual).isEqualTo(1);
+    }
+
+    @Test
+    void 메시지_알림이_실패해도_메시지는_저장된다() throws FirebaseMessagingException {
+        // when
+        given(firebaseMessaging.send(any())).willThrow(FirebaseMessagingException.class);
+        final Long actualSavedMessageId = messageService.create(메시지_생성_DTO, 이미지_절대_경로);
+        final long actual = events.stream(MessageNotificationEvent.class).count();
+
+        // then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(messageRepository.findById(actualSavedMessageId)).isPresent();
+            softAssertions.assertThat(actual).isEqualTo(1);
+        });
     }
 
     @Test
