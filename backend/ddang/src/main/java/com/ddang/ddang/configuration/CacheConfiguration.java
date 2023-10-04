6b09cded118df476014b4ec61bef3d21560032a6
apptie <appti@naver.com>
2023-10-05 01:42:29 +0900

    feat: 메모리 캐시 설정 추가

---
+++ backend/ddang/src/main/java/com/ddang/ddang/configuration/CacheConfiguration.java
@@ -0,0 +1,9 @@
+package com.ddang.ddang.configuration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+}
