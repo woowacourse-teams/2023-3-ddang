fd9da42728c08bfd968319e9be36827cf1234970
hyemdooly <hyemdooly@gmail.com>
2023-10-05 18:00:42 +0900

    feat: ViewModel Event 생성

--- android/app/src/main/java/com/ddangddangddang/android/feature/messageRoom/review/UserReviewViewModel.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/messageRoom/review/UserReviewViewModel.kt
@@ -1,20 +1,31 @@
 package com.ddangddangddang.android.feature.messageRoom.review
 
-import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ddangddangddang.android.feature.common.ErrorType
 import com.ddangddangddang.android.model.MessageRoomDetailModel
+import com.ddangddangddang.android.util.livedata.SingleLiveEvent
+import com.ddangddangddang.data.model.request.ReviewRequest
+import com.ddangddangddang.data.remote.ApiResponse
 import com.ddangddangddang.data.repository.ReviewRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class UserReviewViewModel @Inject constructor(private val reviewRepository: ReviewRepository) : ViewModel() {
+class UserReviewViewModel @Inject constructor(private val reviewRepository: ReviewRepository) :
+    ViewModel() {
     private var partnerId: Long? = null
     private var auctionId: Long? = null
 
-    val ratingGrade = MutableLiveData(0F)
+    val ratingGrade = MutableLiveData(0.0)
     val reviewDetailContent = MutableLiveData("")
+
+    private var _event: SingleLiveEvent<ReviewEvent> = SingleLiveEvent()
+    val event: LiveData<ReviewEvent>
+        get() = _event
 
     fun setPartnerInfo(detail: MessageRoomDetailModel) {
         partnerId = detail.messagePartnerId
@@ -22,7 +33,27 @@ class UserReviewViewModel @Inject constructor(private val reviewRepository: Revi
     }
 
     fun submitReview() {
-        // submit
-        Log.d("test", "submit - ${ratingGrade.value}")
+        val auctionId = auctionId ?: return
+        val partnerId = partnerId ?: return
+
+        val request = ReviewRequest(
+            auctionId,
+            partnerId,
+            ratingGrade.value ?: 0.0,
+            reviewDetailContent.value ?: "",
+        )
+        viewModelScope.launch {
+            when (val response = reviewRepository.reviewUser(request)) {
+                is ApiResponse.Success -> TODO()
+                is ApiResponse.Failure -> TODO()
+                is ApiResponse.NetworkError -> TODO()
+                is ApiResponse.Unexpected -> TODO()
+            }
+        }
+    }
+
+    sealed class ReviewEvent {
+        object ReviewSuccess : ReviewEvent()
+        class ReviewFailure(error: ErrorType) : ReviewEvent()
     }
 }
