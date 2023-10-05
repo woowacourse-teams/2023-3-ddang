1c5e860180f5b269b5ed997fecc9a69a7c90cc04
swonny <wonny921@gmail.com>
2023-10-05 17:05:18 +0900

    refactor: 알림 서비스에서 발생하는 예외 이벤트 리스너에서 처리하도록 수정

--- backend/ddang/src/main/java/com/ddang/ddang/notification/application/NotificationEventListener.java
+++ backend/ddang/src/main/java/com/ddang/ddang/notification/application/NotificationEventListener.java
@@ -10,12 +10,15 @@ import com.ddang.ddang.image.domain.ProfileImage;
 import com.ddang.ddang.image.presentation.util.ImageUrlCalculator;
 import com.ddang.ddang.notification.application.dto.CreateNotificationDto;
 import com.ddang.ddang.notification.domain.NotificationType;
+import com.google.firebase.messaging.FirebaseMessagingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class NotificationEventListener {
 
     private static final String URI_DELIMITER = "/";
@@ -27,33 +30,41 @@ public class NotificationEventListener {
 
     @TransactionalEventListener
     public void sendMessageNotification(final MessageNotificationEvent messageNotificationEvent) {
-        final MessageDto messageDto = messageNotificationEvent.messageDto();
-        final ProfileImage profileImage = messageDto.receiver().getProfileImage();
-        final CreateNotificationDto createNotificationDto = new CreateNotificationDto(
-                NotificationType.MESSAGE,
-                messageDto.receiver().getId(),
-                messageDto.writer().getName(),
-                messageDto.contents(),
-                calculateRedirectUrl(MESSAGE_NOTIFICATION_REDIRECT_URI, messageDto.chatRoom().getId()),
-                ImageUrlCalculator.calculateBy(messageDto.profileImageAbsoluteUrl(), profileImage.getId())
-        );
-        notificationService.send(createNotificationDto);
+        try {
+            final MessageDto messageDto = messageNotificationEvent.messageDto();
+            final ProfileImage profileImage = messageDto.receiver().getProfileImage();
+            final CreateNotificationDto createNotificationDto = new CreateNotificationDto(
+                    NotificationType.MESSAGE,
+                    messageDto.receiver().getId(),
+                    messageDto.writer().getName(),
+                    messageDto.contents(),
+                    calculateRedirectUrl(MESSAGE_NOTIFICATION_REDIRECT_URI, messageDto.chatRoom().getId()),
+                    ImageUrlCalculator.calculateBy(messageDto.profileImageAbsoluteUrl(), profileImage.getId())
+            );
+            notificationService.send(createNotificationDto);
+        } catch (final FirebaseMessagingException ex) {
+            log.error("exception type : {}, ", ex.getClass().getSimpleName(), ex);
+        }
     }
 
     @TransactionalEventListener
     public void sendBidNotification(final BidNotificationEvent bidNotificationEvent) {
-        final BidNotificationDto bidNotificationDto = bidNotificationEvent.bidNotificationDto();
-        final Auction auction = bidNotificationDto.auctionAndImageDto().auction();
-        final AuctionImage auctionImage = bidNotificationDto.auctionAndImageDto().auctionImage();
-        final CreateNotificationDto createNotificationDto = new CreateNotificationDto(
-                NotificationType.BID,
-                bidNotificationDto.previousBidderId(),
-                auction.getTitle(),
-                BID_NOTIFICATION_MESSAGE_FORMAT,
-                calculateRedirectUrl(BID_NOTIFICATION_REDIRECT_URI, auction.getId()),
-                ImageUrlCalculator.calculateBy(bidNotificationDto.auctionImageAbsoluteUrl(), auctionImage.getId())
-        );
-        notificationService.send(createNotificationDto);
+        try {
+            final BidNotificationDto bidNotificationDto = bidNotificationEvent.bidNotificationDto();
+            final Auction auction = bidNotificationDto.auctionAndImageDto().auction();
+            final AuctionImage auctionImage = bidNotificationDto.auctionAndImageDto().auctionImage();
+            final CreateNotificationDto createNotificationDto = new CreateNotificationDto(
+                    NotificationType.BID,
+                    bidNotificationDto.previousBidderId(),
+                    auction.getTitle(),
+                    BID_NOTIFICATION_MESSAGE_FORMAT,
+                    calculateRedirectUrl(BID_NOTIFICATION_REDIRECT_URI, auction.getId()),
+                    ImageUrlCalculator.calculateBy(bidNotificationDto.auctionImageAbsoluteUrl(), auctionImage.getId())
+            );
+            notificationService.send(createNotificationDto);
+        } catch (final FirebaseMessagingException ex) {
+            log.error("exception type : {}, ", ex.getClass().getSimpleName(), ex);
+        }
     }
 
     private String calculateRedirectUrl(final String uri, final Long id) {
