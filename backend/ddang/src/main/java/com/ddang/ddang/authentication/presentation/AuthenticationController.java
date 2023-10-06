7fe42976ee9478c3ed1f2d22dc09c1d00ad28f31
최승원 Merry <wonny921@gmail.com>
2023-10-06 20:35:20 +0900

    Revert "refactor: 탈퇴 시 요청 메서드 변경 (#409)"
    This reverts commit 93e31374b2c6751639b4872e781e728d4a776993.

--- backend/ddang/src/main/java/com/ddang/ddang/authentication/presentation/AuthenticationController.java
+++ backend/ddang/src/main/java/com/ddang/ddang/authentication/presentation/AuthenticationController.java
@@ -14,6 +14,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,7 +68,7 @@ public class AuthenticationController {
                              .build();
     }
 
-    @PostMapping("/withdrawal/{oauth2Type}")
+    @DeleteMapping("/withdrawal/{oauth2Type}")
     public ResponseEntity<Void> withdrawal(
             @PathVariable final Oauth2Type oauth2Type,
             @RequestHeader(HttpHeaders.AUTHORIZATION) final String accessToken,
