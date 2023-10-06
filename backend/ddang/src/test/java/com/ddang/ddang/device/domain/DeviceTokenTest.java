ffc2e66d8d23530a214315a897ee73b67c572a7b
최승원 Merry <wonny921@gmail.com>
2023-10-06 20:35:20 +0900

    Revert "refactor: #480 디바이스 토큰 관련 테스트 코드 리팩토링 (#491)"
    This reverts commit d64c2577d1bad30ab92f0450a5acec1fa8cccba2.

--- backend/ddang/src/test/java/com/ddang/ddang/device/domain/DeviceTokenTest.java
+++ backend/ddang/src/test/java/com/ddang/ddang/device/domain/DeviceTokenTest.java
@@ -1,21 +1,29 @@
 package com.ddang.ddang.device.domain;
 
-import com.ddang.ddang.device.domain.fixture.DeviceTokenFixture;
+import com.ddang.ddang.image.domain.ProfileImage;
+import com.ddang.ddang.user.domain.User;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SuppressWarnings("NonAsciiCharacters")
-class DeviceTokenTest extends DeviceTokenFixture {
-
+class DeviceTokenTest {
 
     @Test
     void 디바이스_토큰이_다르다면_참을_반환한다() {
         // given
-        final DeviceToken deviceToken = new DeviceToken(사용자, 디바이스_토큰);
+        final String deviceTokenValue = "deviceToken";
+        final User user = User.builder()
+                              .name("사용자")
+                              .profileImage(new ProfileImage("upload.png", "store.png"))
+                              .reliability(4.7d)
+                              .oauthId("12345")
+                              .build();
+        final DeviceToken deviceToken = new DeviceToken(user, deviceTokenValue);
+
+        final String targetDeviceTokenValue = "differentDeviceToken";
 
         // when
-        final boolean actual = deviceToken.isDifferentToken(새로운_디바이스_토큰);
+        final boolean actual = deviceToken.isDifferentToken(targetDeviceTokenValue);
 
         // then
         assertThat(actual).isTrue();
@@ -24,10 +32,19 @@ class DeviceTokenTest extends DeviceTokenFixture {
     @Test
     void 디바이스_토큰이_같다면_거짓을_반환한다() {
         // given
-        final DeviceToken deviceToken = new DeviceToken(사용자, 디바이스_토큰);
+        final String deviceTokenValue = "deviceToken";
+        final User user = User.builder()
+                              .name("사용자")
+                              .profileImage(new ProfileImage("upload.png", "store.png"))
+                              .reliability(4.7d)
+                              .oauthId("12345")
+                              .build();
+        final DeviceToken deviceToken = new DeviceToken(user, deviceTokenValue);
+
+        final String targetDeviceTokenValue = deviceTokenValue;
 
         // when
-        final boolean actual = deviceToken.isDifferentToken(디바이스_토큰);
+        final boolean actual = deviceToken.isDifferentToken(targetDeviceTokenValue);
 
         // then
         assertThat(actual).isFalse();
@@ -36,12 +53,21 @@ class DeviceTokenTest extends DeviceTokenFixture {
     @Test
     void 디바이스_토큰을_갱신한다() {
         // given
-        final DeviceToken deviceToken = new DeviceToken(사용자, 디바이스_토큰);
+        final String deviceTokenValue = "deviceToken";
+        final User user = User.builder()
+                              .name("사용자")
+                              .profileImage(new ProfileImage("upload.png", "store.png"))
+                              .reliability(4.7d)
+                              .oauthId("12345")
+                              .build();
+        final DeviceToken deviceToken = new DeviceToken(user, deviceTokenValue);
+
+        final String newDeviceTokenValue = "newDeviceToken";
 
         // when
-        deviceToken.updateDeviceToken(새로운_디바이스_토큰);
+        deviceToken.updateDeviceToken(newDeviceTokenValue);
 
         // then
-        assertThat(deviceToken.getDeviceToken()).isEqualTo(새로운_디바이스_토큰);
+        assertThat(deviceToken.getDeviceToken()).isEqualTo(newDeviceTokenValue);
     }
 }
--- backend/ddang/src/test/java/com/ddang/ddang/device/infrastructure/persistence/JpaDeviceTokenRepositoryTest.java
+++ backend/ddang/src/test/java/com/ddang/ddang/device/infrastructure/persistence/JpaDeviceTokenRepositoryTest.java
@@ -3,7 +3,11 @@ package com.ddang.ddang.device.infrastructure.persistence;
 import com.ddang.ddang.configuration.JpaConfiguration;
 import com.ddang.ddang.configuration.QuerydslConfiguration;
 import com.ddang.ddang.device.domain.DeviceToken;
-import com.ddang.ddang.device.infrastructure.persistence.fixture.JpaDeviceTokenRepositoryFixture;
+import com.ddang.ddang.image.domain.ProfileImage;
+import com.ddang.ddang.user.domain.User;
+import com.ddang.ddang.user.infrastructure.persistence.JpaUserRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -16,20 +20,43 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@Import({JpaConfiguration.class, QuerydslConfiguration.class})
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
-class JpaDeviceTokenRepositoryTest extends JpaDeviceTokenRepositoryFixture {
+@Import({JpaConfiguration.class, QuerydslConfiguration.class})
+class JpaDeviceTokenRepositoryTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired
+    JpaUserRepository userRepository;
 
     @Autowired
     JpaDeviceTokenRepository userDeviceTokenRepository;
 
     @Test
     void 주어진_사용자_아이디에_해당하는_기기토큰을_조회한다() {
+        // given
+        final String deviceToken = "token1234";
+
+        final User user = User.builder()
+                              .name("사용자")
+                              .profileImage(new ProfileImage("upload.png", "store.png"))
+                              .reliability(4.7d)
+                              .oauthId("12345")
+                              .build();
+        userRepository.save(user);
+
+        final DeviceToken expect = new DeviceToken(user, deviceToken);
+        userDeviceTokenRepository.save(expect);
+
+        em.flush();
+        em.clear();
+
         // when
-        final Optional<DeviceToken> actual = userDeviceTokenRepository.findByUserId(사용자.getId());
+        final Optional<DeviceToken> actual = userDeviceTokenRepository.findByUserId(user.getId());
 
         // then
-        assertThat(actual).contains(사용자의_디바이스_토큰);
+        assertThat(actual).contains(expect);
     }
 }
