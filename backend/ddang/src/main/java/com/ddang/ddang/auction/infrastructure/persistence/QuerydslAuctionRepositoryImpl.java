4276202f363a2f1c25a85674067f2d1e2c617246
최승원 Merry <wonny921@gmail.com>
2023-10-06 20:35:20 +0900

    Revert "fix: #336 경매 목록 조회 쿼리 변경 (#338)"
    This reverts commit 2ed80d2359010c23640e90be87d1c4d2fced704f.

--- backend/ddang/src/main/java/com/ddang/ddang/auction/infrastructure/persistence/QuerydslAuctionRepositoryImpl.java
+++ backend/ddang/src/main/java/com/ddang/ddang/auction/infrastructure/persistence/QuerydslAuctionRepositoryImpl.java
@@ -19,36 +19,25 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class QuerydslAuctionRepositoryImpl implements QuerydslAuctionRepository {
 
-    private static final long SLICE_OFFSET = 1L;
-
     private final JPAQueryFactory queryFactory;
 
     @Override
     public Slice<Auction> findAuctionsAllByLastAuctionId(final Long lastAuctionId, final int size) {
-        final List<Long> findAuctionIds = queryFactory.select(auction.id)
-                                                      .from(auction)
-                                                      .where(
-                                                              auction.deleted.isFalse(),
-                                                              lessThanLastAuctionId(lastAuctionId)
-                                                      )
-                                                      .orderBy(auction.id.desc())
-                                                      .limit(size + SLICE_OFFSET)
-                                                      .fetch();
+        final List<Auction> auctions = queryFactory
+                .selectFrom(auction)
+                .leftJoin(auction.auctionRegions, auctionRegion).fetchJoin()
+                .leftJoin(auctionRegion.thirdRegion, region).fetchJoin()
+                .leftJoin(region.firstRegion).fetchJoin()
+                .leftJoin(region.secondRegion).fetchJoin()
+                .leftJoin(auction.subCategory, category).fetchJoin()
+                .leftJoin(category.mainCategory).fetchJoin()
+                .leftJoin(auction.seller).fetchJoin()
+                .where(auction.deleted.isFalse(), lessThanLastAuctionId(lastAuctionId))
+                .orderBy(auction.id.desc())
+                .limit(size + 1L)
+                .fetch();
 
-        final List<Auction> findAuctions = queryFactory.selectFrom(auction)
-                                                       .leftJoin(auction.auctionRegions, auctionRegion).fetchJoin()
-                                                       .leftJoin(auctionRegion.thirdRegion, region).fetchJoin()
-                                                       .leftJoin(region.firstRegion).fetchJoin()
-                                                       .leftJoin(region.secondRegion).fetchJoin()
-                                                       .leftJoin(auction.subCategory, category).fetchJoin()
-                                                       .leftJoin(category.mainCategory).fetchJoin()
-                                                       .leftJoin(auction.seller).fetchJoin()
-                                                       .leftJoin(auction.lastBid).fetchJoin()
-                                                       .where(auction.id.in(findAuctionIds.toArray(Long[]::new)))
-                                                       .orderBy(auction.id.desc())
-                                                       .fetch();
-
-        return QuerydslSliceHelper.toSlice(findAuctions, size);
+        return QuerydslSliceHelper.toSlice(auctions, size);
     }
 
     private BooleanExpression lessThanLastAuctionId(final Long lastAuctionId) {
@@ -61,17 +50,17 @@ public class QuerydslAuctionRepositoryImpl implements QuerydslAuctionRepository 
 
     @Override
     public Optional<Auction> findAuctionById(final Long auctionId) {
-        final Auction findAuction = queryFactory.selectFrom(auction)
-                                                .leftJoin(auction.auctionRegions, auctionRegion).fetchJoin()
-                                                .leftJoin(auctionRegion.thirdRegion, region).fetchJoin()
-                                                .leftJoin(region.firstRegion).fetchJoin()
-                                                .leftJoin(region.secondRegion).fetchJoin()
-                                                .leftJoin(auction.subCategory, category).fetchJoin()
-                                                .leftJoin(category.mainCategory).fetchJoin()
-                                                .leftJoin(auction.seller).fetchJoin()
-                                                .leftJoin(auction.lastBid).fetchJoin()
-                                                .where(auction.deleted.isFalse(), auction.id.eq(auctionId))
-                                                .fetchOne();
+        final Auction findAuction = queryFactory
+                .selectFrom(auction)
+                .leftJoin(auction.auctionRegions, auctionRegion).fetchJoin()
+                .leftJoin(auctionRegion.thirdRegion, region).fetchJoin()
+                .leftJoin(region.firstRegion).fetchJoin()
+                .leftJoin(region.secondRegion).fetchJoin()
+                .leftJoin(auction.subCategory, category).fetchJoin()
+                .leftJoin(category.mainCategory).fetchJoin()
+                .leftJoin(auction.seller).fetchJoin()
+                .where(auction.deleted.isFalse(), auction.id.eq(auctionId))
+                .fetchOne();
 
         return Optional.ofNullable(findAuction);
     }
