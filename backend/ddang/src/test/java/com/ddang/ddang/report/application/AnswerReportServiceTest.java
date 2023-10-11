01704d910d2b444cc3c43d366eaa55f3440043d2
swonny <wonny921@gmail.com>
2023-10-11 10:32:35 +0900

    feat: 답변 서비스에서 알림 이벤트 발행

--- backend/ddang/src/test/java/com/ddang/ddang/report/application/AnswerReportServiceTest.java
+++ backend/ddang/src/test/java/com/ddang/ddang/report/application/AnswerReportServiceTest.java
@@ -1,85 +1,118 @@
-package com.ddang.ddang.report.application;
+package com.ddang.ddang.qna.application;
 
+import com.ddang.ddang.auction.application.exception.UserForbiddenException;
 import com.ddang.ddang.configuration.IsolateDatabase;
+import com.ddang.ddang.notification.application.dto.AnswerNotificationEvent;
+import com.ddang.ddang.notification.application.dto.QuestionNotificationEvent;
+import com.ddang.ddang.qna.application.exception.AlreadyAnsweredException;
 import com.ddang.ddang.qna.application.exception.AnswerNotFoundException;
-import com.ddang.ddang.report.application.dto.ReadAnswerReportDto;
-import com.ddang.ddang.report.application.exception.InvalidAnswererReportException;
-import com.ddang.ddang.report.application.fixture.AnswerReportServiceFixture;
+import com.ddang.ddang.qna.application.exception.InvalidAnswererException;
+import com.ddang.ddang.qna.application.exception.QuestionNotFoundException;
+import com.ddang.ddang.qna.application.fixture.AnswerServiceFixture;
 import com.ddang.ddang.user.application.exception.UserNotFoundException;
-import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
+import org.springframework.test.context.event.ApplicationEvents;
+import org.springframework.test.context.event.RecordApplicationEvents;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @IsolateDatabase
+@RecordApplicationEvents
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
-class AnswerReportServiceTest extends AnswerReportServiceFixture {
+class AnswerServiceTest extends AnswerServiceFixture {
 
     @Autowired
-    AnswerReportService answerReportService;
+    AnswerService answerService;
+
+    @Autowired
+    ApplicationEvents events;
 
     @Test
-    void 답변_신고를_등록한다() {
+    void 답변을_등록한다() {
         // when
-        final Long actual = answerReportService.create(답변_신고_요청_dto);
+        final Long actual = answerService.create(답변_등록_요청_dto);
 
         // then
         assertThat(actual).isPositive();
     }
 
     @Test
-    void 존재하지_않는_답변_신고시_예외가_발생한다() {
+    void 존재하지_않는_사용자가_질문에_답하는_경우_예외가_발생한다() {
         // when & then
-        assertThatThrownBy(() -> answerReportService.create(존재하지_않는_답변_신고_요청_dto))
-                .isInstanceOf(AnswerNotFoundException.class)
-                .hasMessage("해당 답변을 찾을 수 없습니다.");
-    }
-
-    @Test
-    void 존재하지_않는_사용자가_답변_신고시_예외가_발생한다() {
-        // when & then
-        assertThatThrownBy(() -> answerReportService.create(존재하지_않는_사용자가_답변_신고_요청_dto))
+        assertThatThrownBy(() -> answerService.create(존재하지_않는_사용자의_답변_등록_요청_dto))
                 .isInstanceOf(UserNotFoundException.class)
                 .hasMessage("해당 사용자를 찾을 수 없습니다.");
     }
 
     @Test
-    void 답변자가_본인_답변_신고시_예외가_발생한다() {
+    void 존재하지_않는_질문에_답하는_경우_예외가_발생한다() {
         // when & then
-        assertThatThrownBy(() -> answerReportService.create(답변자가_본인_답변_신고_요청_dto))
-                .isInstanceOf(InvalidAnswererReportException.class)
-                .hasMessage("본인 답변입니다.");
+        assertThatThrownBy(() -> answerService.create(존재하지_않는_질문에_답변_등록_요청_dto))
+                .isInstanceOf(QuestionNotFoundException.class)
+                .hasMessage("해당 질문을 찾을 수 없습니다.");
     }
 
     @Test
-    void 이미_신고한_답변_신고시_예외가_발생한다() {
+    void 판매자가_아닌_다른_사용자가_질문에_답하는_경우_예외가_발생한다() {
         // when & then
-        assertThatThrownBy(() -> answerReportService.create(이미_신고한_답변_신고_요청_dto))
-                .isInstanceOf(InvalidAnswererReportException.class)
-                .hasMessage("이미 신고한 답변입니다.");
+        assertThatThrownBy(() -> answerService.create(판매자가_아닌_사용자가_질문에_답변_등록_요청_dto))
+                .isInstanceOf(InvalidAnswererException.class)
+                .hasMessage("판매자만 답변할 수 있습니다.");
     }
 
     @Test
-    void 전체_신고_목록을_조회한다() {
+    void 이미_답변한_질문에_답하는_경우_예외가_발생한다() {
+        // when & then
+        assertThatThrownBy(() -> answerService.create(이미_답변한_질문에_답변_등록_요청_dto))
+                .isInstanceOf(AlreadyAnsweredException.class)
+                .hasMessage("이미 답변한 질문입니다.");
+    }
+
+    @Test
+    void 답변을_삭제한다() {
         // when
-        final List<ReadAnswerReportDto> actual = answerReportService.readAll();
+        answerService.deleteById(답변.getId(), 판매자.getId());
 
         // then
-        SoftAssertions.assertSoftly(softAssertions -> {
-            softAssertions.assertThat(actual).hasSize(3);
-            softAssertions.assertThat(actual.get(0).reporterDto().id()).isEqualTo(이미_신고한_신고자1.getId());
-            softAssertions.assertThat(actual.get(0).id()).isEqualTo(답변_신고1.getId());
-            softAssertions.assertThat(actual.get(1).reporterDto().id()).isEqualTo(이미_신고한_신고자2.getId());
-            softAssertions.assertThat(actual.get(1).id()).isEqualTo(답변_신고2.getId());
-            softAssertions.assertThat(actual.get(2).reporterDto().id()).isEqualTo(이미_신고한_신고자3.getId());
-            softAssertions.assertThat(actual.get(2).id()).isEqualTo(답변_신고3.getId());
-        });
+        assertThat(답변.isDeleted()).isTrue();
+    }
+
+    @Test
+    void 존재하지_않는_답변_삭제시_예외가_발생한다() {
+        // when & then
+        assertThatThrownBy(() -> answerService.deleteById(존재하지_않는_답변_아이디, 판매자.getId()))
+                .isInstanceOf(AnswerNotFoundException.class)
+                .hasMessage("해당 답변을 찾을 수 없습니다.");
+    }
+
+    @Test
+    void 존재하지_않는_사용자가_답변_삭제시_예외가_발생한다() {
+        // when & then
+        assertThatThrownBy(() -> answerService.deleteById(답변.getId(), 존재하지_않는_사용자_아이디))
+                .isInstanceOf(UserNotFoundException.class)
+                .hasMessage("해당 사용자를 찾을 수 없습니다.");
+    }
+
+    @Test
+    void 답변_작성자가_아닌_사용자가_답변_삭제시_예외가_발생한다() {
+        // when & then
+        assertThatThrownBy(() -> answerService.deleteById(답변.getId(), 판매자가_아닌_사용자.getId()))
+                .isInstanceOf(UserForbiddenException.class)
+                .hasMessage("삭제할 권한이 없습니다.");
+    }
+
+    @Test
+    void 질문에_대한_답변이_생성되면_질문자에게_알림을_보낸다() {
+        // when
+        answerService.create(답변_등록_요청_dto);
+        final long actual = events.stream(AnswerNotificationEvent.class).count();
+
+        // then
+        assertThat(actual).isEqualTo(1);
     }
 }
