6c3e2d8c7e4028a2a667f81e69c43ec9bbacf956
hyemdooly <hyemdooly@gmail.com>
2023-10-12 17:56:38 +0900

    feat: QnA Status Model 생성

---
+++ android/app/src/main/java/com/ddangddangddang/android/model/QnAStatusModel.kt
@@ -0,0 +1,23 @@
+package com.ddangddangddang.android.model
+
+import androidx.annotation.ColorRes
+import androidx.annotation.StringRes
+import com.ddangddangddang.android.R
+
+enum class QnAStatusModel(
+    @StringRes val questionStatus: Int,
+    @ColorRes val colorId: Int,
+) {
+    WAITING(R.string.detail_auction_qna_waiting, R.color.red_300),
+    COMPLETE(R.string.detail_auction_qna_complete, R.color.green),
+    ;
+
+    companion object {
+        private const val ERROR_NOT_FOUND = "[ERROR] 매칭되는 Status가 존재하지 않습니다"
+        fun find(status: String): QnAStatusModel {
+            return values().find { it.name == status } ?: throw IllegalArgumentException(
+                ERROR_NOT_FOUND,
+            )
+        }
+    }
+}
