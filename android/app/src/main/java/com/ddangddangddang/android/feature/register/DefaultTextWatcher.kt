11b9ee11400bec29659cd8fdf5b37090ba29dc25
ippnsj <ippnsj@gmail.com>
2023-10-12 21:54:04 +0900

    feat: 경매 등록 화면에서 금액 수정 시 커서가 수정 위치에 유지되는 기능 추가

--- android/app/src/main/java/com/ddangddangddang/android/feature/register/DefaultTextWatcher.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/register/DefaultTextWatcher.kt
@@ -3,12 +3,18 @@ package com.ddangddangddang.android.feature.register
 import android.text.Editable
 import android.text.TextWatcher
 
-class DefaultTextWatcher(private val onAfterChanged: (String) -> Unit) : TextWatcher {
+class DefaultTextWatcher(private val onAfterChanged: (Int, String) -> Unit) : TextWatcher {
+    private var cursorPositionFromEnd: Int = 0
+
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
-    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        s?.let {
+            cursorPositionFromEnd = it.length - (start + count)
+        }
+    }
 
     override fun afterTextChanged(s: Editable?) {
-        s?.let { onAfterChanged(s.toString()) }
+        s?.let { onAfterChanged(cursorPositionFromEnd, s.toString()) }
     }
 }
