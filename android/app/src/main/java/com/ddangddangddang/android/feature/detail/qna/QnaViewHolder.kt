cab23d9a5db5d6da94c68380ef72d9ef422b1a12
hyemdooly <hyemdooly@gmail.com>
2023-10-12 22:49:53 +0900

    feat: model class 변경 및 ViewHolder 작성

---
+++ android/app/src/main/java/com/ddangddangddang/android/feature/detail/qna/QnaViewHolder.kt
@@ -0,0 +1,28 @@
+package com.ddangddangddang.android.feature.detail.qna
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ddangddangddang.android.databinding.ItemQnaBinding
+import com.ddangddangddang.android.model.QnAModel
+
+class QnaViewHolder private constructor(
+    private val binding: ItemQnaBinding,
+    onItemClick: (Long) -> Unit,
+) : RecyclerView.ViewHolder(binding.root) {
+    init {
+        binding.onItemClick = onItemClick
+    }
+
+    fun bind(qna: QnAModel.QuestionAndAnswer) {
+        binding.model = qna
+    }
+
+    companion object {
+        fun create(parent: ViewGroup, onItemClick: (Long) -> Unit): QnaViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = ItemQnaBinding.inflate(layoutInflater, parent, false)
+            return QnaViewHolder(binding, onItemClick)
+        }
+    }
+}
--- android/app/src/main/java/com/ddangddangddang/android/model/QnAModel.kt
+++ android/app/src/main/java/com/ddangddangddang/android/model/QnAModel.kt
@@ -1,10 +1,14 @@
 package com.ddangddangddang.android.model
 
-data class QnAModel(
-    val questionTitle: String,
-    val contents: String,
-    val answer: String?,
-    val status: QnAStatusModel,
-    val isOwner: Boolean,
-    val isQuestioner: Boolean,
-)
+data class QnAModel(val questionAndAnswers: List<QuestionAndAnswer>) {
+    data class QuestionAndAnswer(
+        val id: Long,
+        val questionTitle: String,
+        val contents: String,
+        val answer: String?,
+        val status: QnAStatusModel,
+        val isOwner: Boolean,
+        val isQuestioner: Boolean,
+        val isPicked: Boolean,
+    )
+}
