689322f155570735fd4cb0718c2efc442277003e
hyemdooly <hyemdooly@gmail.com>
2023-10-12 16:43:42 +0900

    refactor: 작업 전인 fragment 삭제

--- android/app/src/main/java/com/ddangddangddang/android/feature/detail/DetailFragmentType.kt
+++ android/app/src/main/java/com/ddangddangddang/android/feature/detail/DetailFragmentType.kt
@@ -3,7 +3,6 @@ package com.ddangddangddang.android.feature.detail
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import com.ddangddangddang.android.R
-import com.ddangddangddang.android.feature.detail.bidhistory.BidHistoryFragment
 import com.ddangddangddang.android.feature.detail.info.AuctionInfoFragment
 import com.ddangddangddang.android.feature.detail.qna.QnaFragment
 
@@ -13,16 +12,17 @@ enum class DetailFragmentType(val tag: String, @StringRes val nameId: Int) {
             return AuctionInfoFragment()
         }
     },
+
     QNA("qna_tag", R.string.detail_auction_qna_title) {
         override fun create(): Fragment {
             return QnaFragment()
         }
     },
-    BID_HISTORY("bid_history_tag", R.string.detail_auction_bid_history) {
-        override fun create(): Fragment {
-            return BidHistoryFragment()
-        }
-    },
+//    BID_HISTORY("bid_history_tag", R.string.detail_auction_bid_history) {
+//        override fun create(): Fragment {
+//            return BidHistoryFragment()
+//        }
+//    },
     ;
 
     abstract fun create(): Fragment
