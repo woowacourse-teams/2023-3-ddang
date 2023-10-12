fdcaeb2e6470b62f7763d7cc9d283645f08153e0
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2023-10-12 16:08:53 +0900

    feat: #603 경매 상세 조회 시 경매글을 찾을 수 없을 때만 404를 보내도록 수정 (#604)
    * feat: 경매 상세 조회 시 경매글을 찾을 수 없을 때만 404를 보내도록 수정
    
    * refactor: 빈 유저와 빈 경매 상수 추가 및 적용
    
    * refactor: 지정한 경매 아이디와 관련된 채팅방을 조회할 때 경매를 찾을 수 없을 때 예외를 던지도록 다시 변경
    
    * test: 테스트 메서드 이름 명확하게 변경

--- backend/ddang/src/main/java/com/ddang/ddang/auction/application/dto/ReadChatRoomDto.java
+++ backend/ddang/src/main/java/com/ddang/ddang/auction/application/dto/ReadChatRoomDto.java
@@ -1,4 +1,6 @@
 package com.ddang.ddang.auction.application.dto;
 
 public record ReadChatRoomDto(Long id, boolean isChatParticipant) {
+
+    public static ReadChatRoomDto CANNOT_CHAT_DTO = new ReadChatRoomDto(null, false);
 }
