a1b9603a3fa081669338da9ecc759347d7a15165
apptie <appti@naver.com>
2023-10-13 23:44:32 +0900

    feat: 도메인 영역의 Repository 구현체 추가

---
+++ backend/ddang/src/test/java/com/ddang/ddang/auction/infrastructure/persistence/AuctionRepositoryImplTest.java
@@ -0,0 +1,142 @@
+package com.ddang.ddang.auction.infrastructure.persistence;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.ddang.ddang.auction.domain.Auction;
+import com.ddang.ddang.auction.domain.repository.AuctionRepository;
+import com.ddang.ddang.auction.infrastructure.persistence.fixture.AuctionRepositoryFixture;
+import com.ddang.ddang.configuration.JpaConfiguration;
+import com.ddang.ddang.configuration.QuerydslConfiguration;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+
+@DataJpaTest
+@Import({JpaConfiguration.class, QuerydslConfiguration.class})
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class AuctionRepositoryImplTest extends AuctionRepositoryFixture {
+
+    AuctionRepository auctionRepository;
+
+    @BeforeEach
+    void setUp(@Autowired JpaAuctionRepository jpaAuctionRepository, @Autowired JPAQueryFactory queryFactory) {
+        auctionRepository = new AuctionRepositoryImpl(jpaAuctionRepository, new QuerydslAuctionRepository(queryFactory));
+    }
+
+    @Test
+    void 경매를_저장한다() {
+        // when
+        final Auction actual = auctionRepository.save(저장하기_전_경매_엔티티);
+
+        assertThat(actual.getId()).isPositive();
+    }
+
+    @Test
+    void 지정한_아이디에_대한_경매와_관련된_데이터를_모두_조회한다() {
+        // when
+        final Optional<Auction> actual = auctionRepository.findTotalAuctionById(저장된_경매_엔티티.getId());
+
+        // then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(actual).isPresent();
+            softAssertions.assertThat(actual.get().getId()).isEqualTo(저장된_경매_엔티티.getId());
+            softAssertions.assertThat(actual.get().getTitle()).isEqualTo(저장된_경매_엔티티.getTitle());
+            softAssertions.assertThat(actual.get().getDescription()).isEqualTo(저장된_경매_엔티티.getDescription());
+            softAssertions.assertThat(actual.get().getBidUnit()).isEqualTo(저장된_경매_엔티티.getBidUnit());
+            softAssertions.assertThat(actual.get().getStartPrice()).isEqualTo(저장된_경매_엔티티.getStartPrice());
+            softAssertions.assertThat(actual.get().getClosingTime()).isEqualTo(저장된_경매_엔티티.getClosingTime());
+            softAssertions.assertThat(actual.get().getAuctionRegions()).isNotNull();
+            softAssertions.assertThat(actual.get().getAuctionRegions().get(0)).isNotNull();
+            softAssertions.assertThat(actual.get().getAuctionRegions().get(0).getThirdRegion()).isNotNull();
+            softAssertions.assertThat(actual.get().getAuctionRegions().get(0).getThirdRegion().getFirstRegion()).isNotNull();
+            softAssertions.assertThat(actual.get().getAuctionRegions().get(0).getThirdRegion().getSecondRegion()).isNotNull();
+            softAssertions.assertThat(actual.get().getSubCategory()).isNotNull();
+            softAssertions.assertThat(actual.get().getSubCategory().getMainCategory()).isNotNull();
+            softAssertions.assertThat(actual.get().getSeller()).isNotNull();
+        });
+    }
+
+    @Test
+    void 지정한_아이디에_대한_경매를_조회한다() {
+        // when
+        final Optional<Auction> actual = auctionRepository.findPureAuctionById(저장된_경매_엔티티.getId());
+
+        // then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(actual).isPresent();
+            softAssertions.assertThat(actual.get().getId()).isEqualTo(저장된_경매_엔티티.getId());
+            softAssertions.assertThat(actual.get().getTitle()).isEqualTo(저장된_경매_엔티티.getTitle());
+            softAssertions.assertThat(actual.get().getDescription()).isEqualTo(저장된_경매_엔티티.getDescription());
+            softAssertions.assertThat(actual.get().getBidUnit()).isEqualTo(저장된_경매_엔티티.getBidUnit());
+            softAssertions.assertThat(actual.get().getStartPrice()).isEqualTo(저장된_경매_엔티티.getStartPrice());
+            softAssertions.assertThat(actual.get().getClosingTime()).isEqualTo(저장된_경매_엔티티.getClosingTime());
+        });
+    }
+
+    @Test
+    void 삭제된_아이디에_대한_경매_조회시_빈_optional을_반환한다() {
+        // when
+        final Optional<Auction> actual = auctionRepository.findTotalAuctionById(삭제된_경매_엔티티.getId());
+
+        // then
+        assertThat(actual).isEmpty();
+    }
+
+    @Test
+    void 경매_목록을_조회한다() {
+        // when
+        final Slice<Auction> actual = auctionRepository.findAuctionsAllByCondition(
+                검색어_없음,
+                PageRequest.of(페이지_1, 페이지_크기)
+        );
+
+        // then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            assertThat(actual).hasSize(1);
+            assertThat(actual.getContent().get(0)).isEqualTo(저장된_경매_엔티티);
+            assertThat(actual.hasNext()).isFalse();
+        });
+    }
+
+    @Test
+    void 사용자가_등록한_경매_목록을_조회한다() {
+        // when
+        final Slice<Auction> actual = auctionRepository.findAuctionsAllByUserId(
+                판매자.getId(),
+                PageRequest.of(페이지_1, 페이지_크기)
+        );
+
+        // then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            assertThat(actual).hasSize(1);
+            assertThat(actual.getContent().get(0)).isEqualTo(저장된_경매_엔티티);
+            assertThat(actual.hasNext()).isFalse();
+        });
+    }
+
+    @Test
+    void 사용자가_입찰한_경매_목록을_조회한다() {
+        // when
+        final Slice<Auction> actual = auctionRepository.findAuctionsAllByBidderId(
+                구매자.getId(),
+                PageRequest.of(페이지_1, 페이지_크기)
+        );
+
+        // then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            assertThat(actual).hasSize(1);
+            assertThat(actual.getContent().get(0)).isEqualTo(저장된_경매_엔티티);
+            assertThat(actual.hasNext()).isFalse();
+        });
+    }
+}
---
+++ backend/ddang/src/test/java/com/ddang/ddang/auction/infrastructure/persistence/fixture/AuctionRepositoryFixture.java
@@ -0,0 +1,151 @@
+package com.ddang.ddang.auction.infrastructure.persistence.fixture;
+
+import com.ddang.ddang.auction.domain.Auction;
+import com.ddang.ddang.auction.domain.BidUnit;
+import com.ddang.ddang.auction.domain.Price;
+import com.ddang.ddang.auction.infrastructure.persistence.JpaAuctionRepository;
+import com.ddang.ddang.auction.presentation.dto.request.ReadAuctionSearchCondition;
+import com.ddang.ddang.bid.domain.Bid;
+import com.ddang.ddang.bid.domain.BidPrice;
+import com.ddang.ddang.bid.infrastructure.persistence.JpaBidRepository;
+import com.ddang.ddang.category.domain.Category;
+import com.ddang.ddang.category.infrastructure.persistence.JpaCategoryRepository;
+import com.ddang.ddang.image.domain.ProfileImage;
+import com.ddang.ddang.region.domain.AuctionRegion;
+import com.ddang.ddang.region.domain.Region;
+import com.ddang.ddang.region.infrastructure.persistence.JpaRegionRepository;
+import com.ddang.ddang.user.domain.Reliability;
+import com.ddang.ddang.user.domain.User;
+import com.ddang.ddang.user.infrastructure.persistence.JpaUserRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SuppressWarnings("NonAsciiCharacters")
+public class AuctionRepositoryFixture {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Autowired
+    private JpaUserRepository userRepository;
+
+    @Autowired
+    private JpaAuctionRepository auctionRepository;
+
+    @Autowired
+    private JpaRegionRepository regionRepository;
+
+    @Autowired
+    private JpaCategoryRepository categoryRepository;
+
+    @Autowired
+    private JpaBidRepository bidRepository;
+
+    private Instant 시간 = Instant.parse("2023-07-08T22:21:20Z");
+    private ZoneId 위치 = ZoneId.of("UTC");
+
+    protected ReadAuctionSearchCondition 검색어_없음 = new ReadAuctionSearchCondition(null);
+    protected int 페이지_1 = 0;
+    protected int 페이지_크기 = 3;
+    protected Auction 저장하기_전_경매_엔티티 = Auction.builder()
+                                             .title("제목")
+                                             .description("내용")
+                                             .bidUnit(new BidUnit(1_000))
+                                             .startPrice(new Price(1_000))
+                                             .closingTime(LocalDateTime.now())
+                                             .build();
+    protected Auction 저장된_경매_엔티티;
+    protected Auction 삭제된_경매_엔티티;
+    protected User 판매자;
+    protected User 구매자;
+
+    @BeforeEach
+    void setUp() {
+        final Region 서울특별시 = new Region("서울특별시");
+        final Region 강남구 = new Region("강남구");
+        final Region 역삼동 = new Region("역삼동");
+
+        서울특별시.addSecondRegion(강남구);
+        강남구.addThirdRegion(역삼동);
+
+        regionRepository.save(서울특별시);
+
+        final Category 가구_카테고리 = new Category("가구");
+        final Category 가구_서브_의자_카테고리 = new Category("의자");
+
+        가구_카테고리.addSubCategory(가구_서브_의자_카테고리);
+
+        categoryRepository.save(가구_카테고리);
+
+        판매자 = User.builder()
+                  .name("판매자")
+                  .profileImage(new ProfileImage("upload.png", "store.png"))
+                  .reliability(new Reliability(4.7d))
+                  .oauthId("12345")
+                  .build();
+
+        구매자 = User.builder()
+                  .name("구매자")
+                  .profileImage(new ProfileImage("upload.png", "store.png"))
+                  .reliability(new Reliability(2.7d))
+                  .oauthId("54321")
+                  .build();
+
+        userRepository.save(판매자);
+        userRepository.save(구매자);
+
+        저장된_경매_엔티티 = Auction.builder()
+                            .title("경매 상품 1")
+                            .description("이것은 경매 상품 1 입니다.")
+                            .bidUnit(new BidUnit(1_000))
+                            .startPrice(new Price(1_000))
+                            .closingTime(시간.atZone(위치).toLocalDateTime())
+                            .subCategory(가구_서브_의자_카테고리)
+                            .seller(판매자)
+                            .build();
+        삭제된_경매_엔티티 = Auction.builder()
+                            .title("경매 상품 1")
+                            .description("이것은 경매 상품 1 입니다.")
+                            .bidUnit(new BidUnit(1_000))
+                            .startPrice(new Price(1_000))
+                            .closingTime(시간.atZone(위치).toLocalDateTime())
+                            .subCategory(가구_서브_의자_카테고리)
+                            .seller(판매자)
+                            .build();
+
+        삭제된_경매_엔티티.addAuctionRegions(List.of(new AuctionRegion(역삼동)));
+        저장된_경매_엔티티.addAuctionRegions(List.of(new AuctionRegion(역삼동)));
+        삭제된_경매_엔티티.delete();
+        bidding(저장된_경매_엔티티, 구매자);
+        addAuctioneerCount(저장된_경매_엔티티, 1);
+
+        auctionRepository.saveAll(List.of(저장된_경매_엔티티, 삭제된_경매_엔티티));
+
+        em.flush();
+        em.clear();
+    }
+
+    private void bidding(final Auction targetAuction, final User bidder) {
+        final Bid lastBid = new Bid(targetAuction, bidder, new BidPrice(1));
+
+        bidRepository.save(lastBid);
+
+        targetAuction.updateLastBid(lastBid);
+    }
+
+    private void addAuctioneerCount(final Auction targetAuction, final int count) {
+        final Bid lastBid = new Bid(targetAuction, targetAuction.getSeller(), new BidPrice(1));
+
+        bidRepository.save(lastBid);
+
+        for (int i = 0; i < count; i++) {
+            targetAuction.updateLastBid(lastBid);
+        }
+    }
+}
