69af9748bb55081810c8ae45746d680254051ff2
apptie <appti@naver.com>
2023-10-14 14:10:03 +0900

    test: BlackListToken의 도메인 인터페이스 Fixture에서 구체적인 인프라 구현 방식을 알지 못하도록 변경

--- backend/ddang/src/test/java/com/ddang/ddang/authentication/infrastructure/persistence/fixture/BlackListTokenRepositoryFixture.java
+++ backend/ddang/src/test/java/com/ddang/ddang/authentication/infrastructure/persistence/fixture/BlackListTokenRepositoryFixture.java
@@ -2,20 +2,17 @@ package com.ddang.ddang.authentication.infrastructure.persistence.fixture;
 
 import com.ddang.ddang.authentication.domain.BlackListToken;
 import com.ddang.ddang.authentication.domain.TokenType;
+import com.ddang.ddang.authentication.domain.repository.BlackListTokenRepository;
+import com.ddang.ddang.authentication.infrastructure.persistence.BlackListTokenRepositoryImpl;
 import com.ddang.ddang.authentication.infrastructure.persistence.JpaBlackListTokenRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class BlackListTokenRepositoryFixture {
 
-    @PersistenceContext
-    private EntityManager em;
-
     @Autowired
-    private JpaBlackListTokenRepository blackListTokenRepository;
+    private JpaBlackListTokenRepository jpaBlackListTokenRepository;
 
     protected String 만료_토큰_내용 = "expired token";
     protected String 만료되지_않은_토큰_내용 = "not expired token";
@@ -25,9 +22,9 @@ public class BlackListTokenRepositoryFixture {
 
     @BeforeEach
     void setUp() {
-        blackListTokenRepository.save(만료된_토큰);
+        final BlackListTokenRepository blackListTokenRepository =
+                new BlackListTokenRepositoryImpl(jpaBlackListTokenRepository);
 
-        em.flush();
-        em.clear();
+        blackListTokenRepository.save(만료된_토큰);
     }
 }
