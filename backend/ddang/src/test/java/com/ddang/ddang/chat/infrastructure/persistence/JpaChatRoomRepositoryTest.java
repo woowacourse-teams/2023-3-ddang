7a52cedf3eecc6e750b7470425092fd7b26acbc6
kwonyj1022 <kwonyj1022@naver.com>
2023-10-14 18:21:47 +0900

    refactor: 불필요하게 Querydsl을 사용한 코드를 JPQL을 사용하도록 변경

--- backend/ddang/src/test/java/com/ddang/ddang/chat/infrastructure/persistence/JpaChatRoomRepositoryTest.java
+++ backend/ddang/src/test/java/com/ddang/ddang/chat/infrastructure/persistence/JpaChatRoomRepositoryTest.java
@@ -54,6 +54,15 @@ class JpaChatRoomRepositoryTest extends JpaChatRoomRepositoryFixture {
     }
 
     @Test
+    void 지정한_경매_아이디가_포함된_채팅방의_아이디를_조회한다() {
+        // when
+        final Optional<Long> actual = chatRoomRepository.findChatRoomIdByAuctionId(경매.getId());
+
+        // then
+        assertThat(actual).contains(채팅방.getId());
+    }
+
+    @Test
     void 지정한_경매_아이디가_포함된_채팅방이_존재한다면_참을_반환한다() {
         // when
         final boolean actual = chatRoomRepository.existsByAuctionId(경매.getId());
