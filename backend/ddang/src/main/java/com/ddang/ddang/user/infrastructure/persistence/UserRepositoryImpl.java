fb14b1f7d3f8376e5920243082826471a3fbcdd2
kwonyj1022 <kwonyj1022@naver.com>
2023-10-14 15:40:55 +0900

    feat: 도메인 영역의 사용자 Repository 구현체 추가

---
+++ backend/ddang/src/main/java/com/ddang/ddang/user/infrastructure/persistence/UserRepositoryImpl.java
@@ -0,0 +1,50 @@
+package com.ddang.ddang.user.infrastructure.persistence;
+
+import com.ddang.ddang.user.domain.User;
+import com.ddang.ddang.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepositoryImpl implements UserRepository {
+
+    private final JpaUserRepository jpaUserRepository;
+
+    @Override
+    public User save(final User user) {
+        return jpaUserRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findById(final Long id) {
+        return jpaUserRepository.findById(id);
+    }
+
+    @Override
+    public boolean existsById(final Long id) {
+        return jpaUserRepository.existsById(id);
+    }
+
+    @Override
+    public Optional<User> findByOauthId(final String oauthId) {
+        return jpaUserRepository.findByOauthId(oauthId);
+    }
+
+    @Override
+    public Optional<User> findByIdAndDeletedIsFalse(final Long id) {
+        return jpaUserRepository.findByIdAndDeletedIsFalse(id);
+    }
+
+    @Override
+    public boolean existsByIdAndDeletedIsTrue(final Long id) {
+        return jpaUserRepository.existsByIdAndDeletedIsTrue(id);
+    }
+
+    @Override
+    public boolean existsByNameEndingWith(final String name) {
+        return existsByNameEndingWith(name);
+    }
+}
---
+++ backend/ddang/src/test/java/com/ddang/ddang/user/infrastructure/persistence/fixture/UserRepositoryImplFixture.java
@@ -0,0 +1,50 @@
+package com.ddang.ddang.user.infrastructure.persistence.fixture;
+
+import com.ddang.ddang.image.domain.ProfileImage;
+import com.ddang.ddang.user.domain.Reliability;
+import com.ddang.ddang.user.domain.User;
+import com.ddang.ddang.user.domain.repository.UserRepository;
+import com.ddang.ddang.user.infrastructure.persistence.JpaUserRepository;
+import com.ddang.ddang.user.infrastructure.persistence.UserRepositoryImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SuppressWarnings("NonAsciiCharacters")
+public class UserRepositoryImplFixture {
+
+    private UserRepository userRepository;
+
+    protected ProfileImage 프로필_이미지 = new ProfileImage("upload.png", "store.png");
+    protected String 존재하지_않는_oauth_아이디 = "invalidOauthId";
+    protected Long 존재하지_않는_사용자_아이디 = -999L;
+    protected String 존재하지_않는_사용자_이름 = "새로운 이름";
+    protected String 존재하는_사용자_이름;
+    protected User 사용자;
+    protected User 탈퇴한_사용자;
+
+    @BeforeEach
+    void setUp(@Autowired JpaUserRepository jpaUserRepository) {
+        userRepository = new UserRepositoryImpl(jpaUserRepository);
+
+        final ProfileImage 사용자_프로필_이미지 = new ProfileImage("upload.png", "store.png");
+        사용자 = User.builder()
+                  .name("사용자")
+                  .profileImage(사용자_프로필_이미지)
+                  .reliability(new Reliability(4.7d))
+                  .oauthId("12345")
+                  .build();
+
+        탈퇴한_사용자 = User.builder()
+                      .name("탈퇴한 사용자")
+                      .profileImage(사용자_프로필_이미지)
+                      .reliability(new Reliability(4.7d))
+                      .oauthId("12345")
+                      .build();
+        탈퇴한_사용자.withdrawal();
+
+        userRepository.save(사용자);
+        userRepository.save(탈퇴한_사용자);
+
+        존재하는_사용자_이름 = 사용자.getName();
+    }
+}
