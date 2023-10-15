25f55e730ffbe1ba447cdc70611cf0a6848616de
swonny <wonny921@gmail.com>
2023-10-15 22:01:04 +0900

    refactor: 테스트 픽스처에서 도메인 영역의 사용자 레포지토리 사용하도록 변경

--- backend/ddang/src/test/java/com/ddang/ddang/device/infrastructure/persistence/fixture/JpaDeviceTokenRepositoryFixture.java
+++ backend/ddang/src/test/java/com/ddang/ddang/device/infrastructure/persistence/fixture/JpaDeviceTokenRepositoryFixture.java
@@ -5,7 +5,9 @@ import com.ddang.ddang.device.infrastructure.persistence.JpaDeviceTokenRepositor
 import com.ddang.ddang.image.domain.ProfileImage;
 import com.ddang.ddang.user.domain.Reliability;
 import com.ddang.ddang.user.domain.User;
+import com.ddang.ddang.user.domain.repository.UserRepository;
 import com.ddang.ddang.user.infrastructure.persistence.JpaUserRepository;
+import com.ddang.ddang.user.infrastructure.persistence.UserRepositoryImpl;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,8 +19,7 @@ public class JpaDeviceTokenRepositoryFixture {
     @PersistenceContext
     private EntityManager em;
 
-    @Autowired
-    private JpaUserRepository userRepository;
+    private UserRepository userRepository;
 
     @Autowired
     private JpaDeviceTokenRepository jpaDeviceTokenRepository;
@@ -27,13 +28,15 @@ public class JpaDeviceTokenRepositoryFixture {
     protected DeviceToken 사용자의_디바이스_토큰;
 
     @BeforeEach
-    void setUp() {
+    void setUp(@Autowired final JpaUserRepository jpaUserRepository) {
+        userRepository = new UserRepositoryImpl(jpaUserRepository);
+
         사용자 = User.builder()
-                    .name("사용자")
-                    .profileImage(new ProfileImage("upload.png", "store.png"))
-                    .reliability(new Reliability(4.7d))
-                    .oauthId("12345")
-                    .build();
+                  .name("사용자")
+                  .profileImage(new ProfileImage("upload.png", "store.png"))
+                  .reliability(new Reliability(4.7d))
+                  .oauthId("12345")
+                  .build();
 
         사용자의_디바이스_토큰 = new DeviceToken(사용자, "deviceToken");
 
