7471d91a1822c246d68c7de7a0766694a4de82d5
swonny <wonny921@gmail.com>
2023-10-15 18:31:02 +0900

    feat: 도메인 영역에 메시지 레포지토리 추가

---
+++ backend/ddang/src/main/java/com/ddang/ddang/chat/domain/repository/MessageRepository.java
@@ -0,0 +1,22 @@
+package com.ddang.ddang.chat.domain.repository;
+
+import com.ddang.ddang.chat.domain.Message;
+import com.querydsl.core.types.dsl.BooleanExpression;
+
+import java.util.List;
+
+public interface MessageRepository {
+
+    Message save(final Message message);
+
+    // TODO: 2023/10/15 [고민] null 체크 안 하는 로직에서는 원시타입으로 바꿔야할지?
+    boolean existsById(final Long lastMessageId);
+
+    List<Message> findMessagesAllByLastMessageId(
+            final Long messageReaderId,
+            final Long chatRoomId,
+            final Long lastMessageId
+    );
+
+    BooleanExpression isGreaterThanLastId(final Long lastMessageId);
+}
