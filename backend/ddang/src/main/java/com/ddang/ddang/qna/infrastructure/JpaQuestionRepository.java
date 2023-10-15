19f99f5f0ff12d516db8cab13f843d20d47b27c8
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-10-15 13:26:28 +0900

    refactor: EntityGraph를 jpql로 변경

--- backend/ddang/src/main/java/com/ddang/ddang/qna/infrastructure/JpaQuestionRepository.java
+++ backend/ddang/src/main/java/com/ddang/ddang/qna/infrastructure/JpaQuestionRepository.java
@@ -1,8 +1,8 @@
 package com.ddang.ddang.qna.infrastructure;
 
 import com.ddang.ddang.qna.domain.Question;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +11,14 @@ public interface JpaQuestionRepository extends JpaRepository<Question, Long> {
 
     Optional<Question> findByIdAndDeletedIsFalse(final Long id);
 
-    @EntityGraph(attributePaths = {"writer", "answer", "auction", "auction.seller"})
+    @Query("""
+        select q
+        from Question q
+        join fetch q.writer
+        join fetch q.answer
+        join fetch q.auction a
+        join fetch a.seller
+        where a.id = :id
+    """)
     List<Question> findAllByAuctionId(final Long auctionId);
 }
