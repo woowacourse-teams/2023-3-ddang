72167b327519a8e41fb22829909ef2bb63f01948
swonny <wonny921@gmail.com>
2023-10-15 21:13:53 +0900

    feat: 도메인 기기토큰 레포지토리의 구현체 추가

---
+++ backend/ddang/src/main/java/com/ddang/ddang/device/infrastructure/persistence/DeviceTokenRepositoryImpl.java
@@ -0,0 +1,25 @@
+package com.ddang.ddang.device.infrastructure.persistence;
+
+import com.ddang.ddang.device.domain.DeviceToken;
+import com.ddang.ddang.device.domain.repository.DeviceTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class DeviceTokenRepositoryImpl implements DeviceTokenRepository {
+
+    private final JpaDeviceTokenRepository jpaDeviceTokenRepository;
+
+    @Override
+    public Optional<DeviceToken> findByUserId(final Long userId) {
+        return jpaDeviceTokenRepository.findByUserId(userId);
+    }
+
+    @Override
+    public void deleteByUserId(final Long id) {
+        jpaDeviceTokenRepository.deleteByUserId(id);
+    }
+}
