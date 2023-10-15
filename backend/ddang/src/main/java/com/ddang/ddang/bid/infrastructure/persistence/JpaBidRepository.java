50d9b923a730922a6017372e836ce12e33083a92
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-10-15 15:03:05 +0900

    refactor: 쿼리문 명령어를 대문자로 수정

--- backend/ddang/src/main/java/com/ddang/ddang/bid/infrastructure/persistence/JpaBidRepository.java
+++ backend/ddang/src/main/java/com/ddang/ddang/bid/infrastructure/persistence/JpaBidRepository.java
@@ -11,6 +11,6 @@ public interface JpaBidRepository extends JpaRepository<Bid, Long> {
 
     List<Bid> findAllByAuctionIdOrderByIdAsc(final Long auctionId);
 
-    @Query("select b from Bid b where b.auction.id = :auctionId order by b.id desc limit 1")
+    @Query("SELECT b FROM Bid b WHERE b.auction.id = :auctionId ORDER BY b.id DESC LIMIT 1")
     Bid findLastBidByAuctionId(@Param("auctionId") final Long auctionId);
 }
