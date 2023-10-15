b3cf42448572bf5fd51f6f5f44f54989ce7629ce
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-10-15 13:22:06 +0900

    feat: 도메인 영역에 질문 repository 추가

---
+++ backend/ddang/src/main/java/com/ddang/ddang/qna/domain/repository/QuestionRepository.java
@@ -0,0 +1,17 @@
+package com.ddang.ddang.qna.domain.repository;
+
+import com.ddang.ddang.qna.domain.Question;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface QuestionRepository {
+
+    Question save(final Question question);
+
+    Optional<Question> findById(final Long id);
+
+    Optional<Question> findByIdIncludeDeleted(final Long id);
+
+    List<Question> findAllByAuctionId(final Long auctionId);
+}
