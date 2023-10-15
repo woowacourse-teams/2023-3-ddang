40136f7dd40e4c89276d96f6b73a25ec60172159
swonny <wonny921@gmail.com>
2023-10-15 21:35:22 +0900

    refactor: 불필요한 주석 삭제

--- backend/ddang/src/main/java/com/ddang/ddang/chat/domain/repository/MessageRepository.java
+++ backend/ddang/src/main/java/com/ddang/ddang/chat/domain/repository/MessageRepository.java
@@ -9,7 +9,6 @@ public interface MessageRepository {
 
     Message save(final Message message);
 
-    // TODO: 2023/10/15 [고민] null 체크 안 하는 로직에서는 원시타입으로 바꿔야할지?
     boolean existsById(final Long lastMessageId);
 
     List<Message> findAllByLastMessageId(
