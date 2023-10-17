ac9ccc992ee414b932af2375ed384f1fa5a8056b
kwonyj1022 <kwonyj1022@naver.com>
2023-10-17 17:07:42 +0900

    feat: S3 이미지 업로드 기능 추가

--- backend/ddang/src/main/java/com/ddang/ddang/image/infrastructure/s3/S3StoreImageProcessor.java
+++ backend/ddang/src/main/java/com/ddang/ddang/image/infrastructure/s3/S3StoreImageProcessor.java
@@ -1,33 +1,43 @@
-package com.ddang.ddang.image.infrastructure.local;
+package com.ddang.ddang.image.infrastructure.s3;
 
 import com.ddang.ddang.image.domain.StoreImageProcessor;
 import com.ddang.ddang.image.domain.dto.StoreImageDto;
 import com.ddang.ddang.image.infrastructure.local.exception.EmptyImageException;
 import com.ddang.ddang.image.infrastructure.local.exception.StoreImageFailureException;
 import com.ddang.ddang.image.infrastructure.local.exception.UnsupportedImageFileExtensionException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.exception.SdkException;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Component
-@Profile("local || test")
-public class LocalStoreImageProcessor implements StoreImageProcessor {
+@Profile("!local && !test")
+@RequiredArgsConstructor
+public class S3StoreImageProcessor implements StoreImageProcessor {
 
-    @Value("${image.store.dir}")
-    private String imageStoreDir;
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+    @Value("${aws.s3.image-path}")
+    private String path;
+
+    private final S3Client s3Client;
 
     @Override
     public List<StoreImageDto> storeImageFiles(final List<MultipartFile> imageFiles) {
         final List<StoreImageDto> storeImageDtos = new ArrayList<>();
 
-        for (MultipartFile imageFile : imageFiles) {
+        for (final MultipartFile imageFile : imageFiles) {
             if (imageFile.isEmpty()) {
                 throw new EmptyImageException("이미지 파일의 데이터가 비어 있습니다.");
             }
@@ -44,17 +54,27 @@ public class LocalStoreImageProcessor implements StoreImageProcessor {
             final String originalImageFileName = imageFile.getOriginalFilename();
             final String storeImageFileName = createStoreImageFileName(originalImageFileName);
             final String fullPath = findFullPath(storeImageFileName);
+            final PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                                                                      .key(fullPath)
+                                                                      .bucket(bucketName)
+                                                                      .contentType(imageFile.getContentType())
+                                                                      .build();
 
-            imageFile.transferTo(new File(fullPath));
+            s3Client.putObject(
+                    putObjectRequest,
+                    RequestBody.fromInputStream(imageFile.getInputStream(), imageFile.getSize())
+            );
 
             return new StoreImageDto(originalImageFileName, storeImageFileName);
         } catch (final IOException ex) {
             throw new StoreImageFailureException("이미지 저장에 실패했습니다.", ex);
+        } catch (final SdkException ex) {
+            throw new StoreImageFailureException("AWS 이미지 저장에 실패했습니다.", ex);
         }
     }
 
     private String findFullPath(final String storeImageFileName) {
-        return imageStoreDir + storeImageFileName;
+        return path + storeImageFileName;
     }
 
     private String createStoreImageFileName(final String originalFilename) {
