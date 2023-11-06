1358994c1c46b931e7ac8f35eef0e30d2c0b3c1f
ippnsj <ippnsj@gmail.com>
2023-11-06 16:41:54 +0900

    feat: 안읽은 메시지 개수 서버 연동

--- android/data/src/main/java/com/ddangddangddang/data/repository/ChatRepository.kt
+++ android/data/src/main/java/com/ddangddangddang/data/repository/ChatRepository.kt
@@ -6,12 +6,13 @@ import com.ddangddangddang.data.model.response.ChatMessageIdResponse
 import com.ddangddangddang.data.model.response.ChatMessageResponse
 import com.ddangddangddang.data.model.response.ChatRoomIdResponse
 import com.ddangddangddang.data.model.response.ChatRoomPreviewResponse
+import com.ddangddangddang.data.model.response.ChatRoomResponse
 import com.ddangddangddang.data.remote.ApiResponse
 
 interface ChatRepository {
     suspend fun getChatRoomId(getChatRoomIdRequest: GetChatRoomIdRequest): ApiResponse<ChatRoomIdResponse>
     suspend fun getChatRoomPreviews(): ApiResponse<List<ChatRoomPreviewResponse>>
-    suspend fun getChatRoomPreview(chatRoomId: Long): ApiResponse<ChatRoomPreviewResponse>
+    suspend fun getChatRoom(chatRoomId: Long): ApiResponse<ChatRoomResponse>
     suspend fun getMessages(
         chatRoomId: Long,
         lastMessageId: Long?,
