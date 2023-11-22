81c16e3aab94bc98e585a5d4a70ea5e2617a34a3
apptie <appti@naver.com>
2023-11-22 10:22:26 +0900

    refactor: 리뷰 조회 시 점수를 변환하는 로직을 DTO 내부에서 수행하도록 변경

--- backend/ddang/src/main/java/com/ddang/ddang/auction/presentation/dto/response/ReadReviewDetailResponse.java
+++ backend/ddang/src/main/java/com/ddang/ddang/auction/presentation/dto/response/ReadReviewDetailResponse.java
@@ -3,15 +3,9 @@ package com.ddang.ddang.auction.presentation.dto.response;
 import com.ddang.ddang.review.application.dto.ReadReviewDetailDto;
 import jakarta.annotation.Nullable;
 
-public record ReadReviewDetailResponse(@Nullable Float score, @Nullable String content) {
+public record ReadReviewDetailResponse(float score, @Nullable String content) {
 
-    public static ReadReviewDetailResponse from(final ReadReviewDetailDto readReviewDetailDto) {
-        final Double nullableScore = readReviewDetailDto.score();
-
-        if (nullableScore == null) {
-            return new ReadReviewDetailResponse(null, readReviewDetailDto.content());
-        }
-
-        return new ReadReviewDetailResponse(nullableScore.floatValue(), readReviewDetailDto.content());
+    public static ReadReviewDetailResponse from(final ReadReviewDetailDto dto) {
+        return new ReadReviewDetailResponse(dto.reviewScore(), dto.content());
     }
 }
--- backend/ddang/src/main/java/com/ddang/ddang/review/application/dto/ReadReviewDetailDto.java
+++ backend/ddang/src/main/java/com/ddang/ddang/review/application/dto/ReadReviewDetailDto.java
@@ -8,9 +8,18 @@ public record ReadReviewDetailDto(@Nullable Double score, @Nullable String conte
 
     private static final Double EMPTY_SCORE = null;
     private static final String EMPTY_CONTENT = null;
+
     public static final ReadReviewDetailDto EMPTY = new ReadReviewDetailDto(EMPTY_SCORE, EMPTY_CONTENT);
 
     public static ReadReviewDetailDto from(final Review review) {
         return new ReadReviewDetailDto(review.getScore().getValue(), review.getContent());
+    }
+
+    public float reviewScore() {
+        if (score == null) {
+            return 0.0f;
+        }
+
+        return score.floatValue();
     }
 }
