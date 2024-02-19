78a8f54c1e9f5282d446ddd0e6211fe4b65664ec
swonny <wonny921@gmail.com>
2024-02-19 17:29:46 +0900

    feat: 웹소켓 세션 등록 일급컬렉션 추가

---
+++ backend/ddang/src/main/java/com/ddang/ddang/chat/handler/WebSocketChatHandler.java
@@ -0,0 +1,34 @@
+package com.ddang.ddang.chat.handler;
+
+import com.ddang.ddang.chat.domain.WebSocketChatSessions;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class WebSocketChatHandler extends TextWebSocketHandler {
+
+    private final WebSocketChatSessions sessions;
+
+    public WebSocketChatHandler() {
+        this.sessions = new WebSocketChatSessions();
+    }
+
+    @Override
+    public void afterConnectionEstablished(final WebSocketSession session) throws Exception {
+        sessions.add(session);
+        super.afterConnectionEstablished(session);
+    }
+
+    @Override
+    protected void handleTextMessage(final WebSocketSession session, final TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+    }
+
+    @Override
+    public void afterConnectionClosed(final WebSocketSession session, final CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+    }
+}
