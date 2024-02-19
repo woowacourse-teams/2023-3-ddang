0f7a219f77933b4bf400baab3bc44c89f71604a6
swonny <wonny921@gmail.com>
2024-02-19 17:29:46 +0900

    feat: 웹소켓 세션 등록 일급컬렉션 추가

---
+++ backend/ddang/src/main/java/com/ddang/ddang/chat/domain/WebSocketSessions.java
@@ -0,0 +1,20 @@
+package com.ddang.ddang.chat.domain;
+
+import org.springframework.web.socket.WebSocketSession;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class WebSocketSessions {
+
+    private final Set<WebSocketSession> sessions;
+
+    public WebSocketSessions() {
+        this.sessions = Collections.newSetFromMap(new ConcurrentHashMap<>());
+    }
+
+    public void add(final WebSocketSession session) {
+        sessions.add(session);
+    }
+}
