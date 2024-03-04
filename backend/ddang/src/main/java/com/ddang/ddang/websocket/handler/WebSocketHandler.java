5059465e4800893845d7a313b7536d5faa06da0f
swonny <wonny921@gmail.com>
2024-03-04 16:45:07 +0900

    feat: 웹소켓 핸들러 생성

--- backend/ddang/src/main/java/com/ddang/ddang/websocket/handler/WebSocketHandler.java
+++ backend/ddang/src/main/java/com/ddang/ddang/websocket/handler/WebSocketHandler.java
@@ -1,6 +1,8 @@
-package com.ddang.ddang.chat.handler;
+package com.ddang.ddang.websocket.handler;
 
 import com.ddang.ddang.chat.domain.WebSocketChatSessions;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -8,28 +10,24 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Component
-public class WebSocketChatHandler extends TextWebSocketHandler {
+@RequiredArgsConstructor
+public class WebSocketHandler extends TextWebSocketHandler {
 
     private final WebSocketChatSessions sessions;
-
-    public WebSocketChatHandler() {
-        this.sessions = new WebSocketChatSessions();
-    }
+    private final ObjectMapper objectMapper;
 
     @Override
     public void afterConnectionEstablished(final WebSocketSession session) throws Exception {
         sessions.add(session);
-        super.afterConnectionEstablished(session);
     }
 
     @Override
-    protected void handleTextMessage(final WebSocketSession session, final TextMessage message) throws Exception {
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         super.handleTextMessage(session, message);
     }
 
     @Override
     public void afterConnectionClosed(final WebSocketSession session, final CloseStatus status) throws Exception {
         sessions.remove(session);
-        super.afterConnectionClosed(session, status);
     }
 }
