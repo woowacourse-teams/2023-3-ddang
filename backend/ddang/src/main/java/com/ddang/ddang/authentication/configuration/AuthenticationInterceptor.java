86fe042d5442307555ce9b670ebbf20bf8203436
swonny <wonny921@gmail.com>
2024-03-04 16:10:30 +0900

    refactor: 인터셉터 서비스 구현 및 인가 인터셉터 분리

--- backend/ddang/src/main/java/com/ddang/ddang/authentication/configuration/AuthenticationInterceptor.java
+++ backend/ddang/src/main/java/com/ddang/ddang/authentication/configuration/AuthenticationInterceptor.java
@@ -1,13 +1,5 @@
 package com.ddang.ddang.authentication.configuration;
 
-import com.ddang.ddang.authentication.application.AuthenticationUserService;
-import com.ddang.ddang.authentication.application.BlackListTokenService;
-import com.ddang.ddang.authentication.infrastructure.jwt.PrivateClaims;
-import com.ddang.ddang.authentication.domain.TokenDecoder;
-import com.ddang.ddang.authentication.domain.TokenType;
-import com.ddang.ddang.authentication.domain.dto.AuthenticationStore;
-import com.ddang.ddang.authentication.domain.dto.AuthenticationUserInfo;
-import com.ddang.ddang.authentication.domain.exception.InvalidTokenException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -19,10 +11,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @RequiredArgsConstructor
 public class AuthenticationInterceptor implements HandlerInterceptor {
 
-    private final BlackListTokenService blackListTokenService;
-    private final AuthenticationUserService authenticationUserService;
-    private final TokenDecoder tokenDecoder;
-    private final AuthenticationStore store;
+    private final AuthenticationInterceptorService authenticationInterceptorService;
 
     @Override
     public boolean preHandle(
@@ -31,35 +20,9 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
             final Object handler
     ) {
         final String accessToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+        authenticationInterceptorService.handleAccessToken(accessToken);
 
-        if (isNotRequiredAuthenticate(accessToken)) {
-            store.set(new AuthenticationUserInfo(null));
-            return true;
-        }
-
-        validateLogoutToken(accessToken);
-
-        final PrivateClaims privateClaims = tokenDecoder.decode(TokenType.ACCESS, accessToken)
-                                                        .orElseThrow(() ->
-                                                                new InvalidTokenException("유효한 토큰이 아닙니다.")
-                                                        );
-
-        if (authenticationUserService.isWithdrawal(privateClaims.userId())) {
-            throw new InvalidTokenException("유효한 토큰이 아닙니다.");
-        }
-
-        store.set(new AuthenticationUserInfo(privateClaims.userId()));
         return true;
-    }
-
-    private boolean isNotRequiredAuthenticate(final String token) {
-        return token == null || token.length() == 0;
-    }
-
-    private void validateLogoutToken(final String accessToken) {
-        if (blackListTokenService.existsBlackListToken(TokenType.ACCESS, accessToken)) {
-            throw new InvalidTokenException("유효한 토큰이 아닙니다.");
-        }
     }
 
     @Override
@@ -69,6 +32,6 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
             final Object handler,
             final Exception ex
     ) {
-        store.remove();
+        authenticationInterceptorService.removeStore();
     }
 }
