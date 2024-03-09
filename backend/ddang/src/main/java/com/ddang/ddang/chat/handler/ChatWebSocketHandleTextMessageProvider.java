9a5ca1ef917e15310dd8b025a6fce7215fb01412
JJ503 <63184334+JJ503@users.noreply.github.com>
2024-03-10 05:04:38 +0900

    refactor: map 타입에 대한 dto 생성

--- backend/ddang/src/main/java/com/ddang/ddang/chat/handler/ChatWebSocketHandleTextMessageProvider.java
+++ backend/ddang/src/main/java/com/ddang/ddang/chat/handler/ChatWebSocketHandleTextMessageProvider.java
@@ -4,10 +4,12 @@ import com.ddang.ddang.chat.application.MessageService;
 import com.ddang.ddang.chat.application.dto.CreateMessageDto;
 import com.ddang.ddang.chat.domain.WebSocketChatSessions;
 import com.ddang.ddang.chat.domain.WebSocketSessions;
+import com.ddang.ddang.chat.handler.dto.ChatMessageDataDto;
 import com.ddang.ddang.chat.presentation.dto.request.CreateMessageRequest;
 import com.ddang.ddang.chat.presentation.dto.response.ReadMessageResponse;
 import com.ddang.ddang.websocket.handler.WebSocketHandleTextMessageProvider;
 import com.ddang.ddang.websocket.handler.dto.SendMessagesDto;
+import com.ddang.ddang.websocket.handler.dto.SessionAttributeDto;
 import com.ddang.ddang.websocket.handler.dto.TextMessageType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -36,36 +38,45 @@ public class ChatWebSocketHandleTextMessageProvider implements WebSocketHandleTe
 
     @Override
     public List<SendMessagesDto> handle(final WebSocketSession session, final Map<String, String> data) throws Exception {
-        final CreateMessageRequest request = new CreateMessageRequest(
-                Long.parseLong(data.get("recevierId")),
-                data.get("contents")
-        );
+        final SessionAttributeDto sessionAttribute = getSessionAttributes(session);
+        final ChatMessageDataDto messageData = objectMapper.convertValue(data, ChatMessageDataDto.class);
+        sessions.add(session, messageData.chatRoomId());
 
-        final long chatRoomId = Long.parseLong(data.get("chatRoomId"));
-        sessions.add(session, chatRoomId);
+        final Long senderId = sessionAttribute.userId();
+        final CreateMessageDto createMessageDto = createMessageDto(messageData, senderId);
 
-        final Map<String, Object> attributes = session.getAttributes();
-        final Long userId = Long.parseLong(String.valueOf(attributes.get("userId")));
-        final CreateMessageDto createMessageDto = CreateMessageDto.of(userId, chatRoomId, request);
-
-        final String baseUrl = String.valueOf(attributes.get("baseUrl"));
-        final Long messageId = messageService.create(createMessageDto, baseUrl);
+        final Long messageId = messageService.create(createMessageDto, sessionAttribute.baseUrl());
         final Map<Long, WebSocketSessions> chatRoomSessions = sessions.getChatRoomSessions();
-        final WebSocketSessions webSocketSessions = chatRoomSessions.get(chatRoomId);
+        final WebSocketSessions webSocketSessions = chatRoomSessions.get(messageData.chatRoomId());
         final Set<WebSocketSession> groupSessions = webSocketSessions.getSessions();
         final List<SendMessagesDto> sendMessagesDtos = new ArrayList<>();
         for (WebSocketSession currentSession : groupSessions) {
             ReadMessageResponse response;
-            if (currentSession.getAttributes().get("userId") == userId) {
-                response = new ReadMessageResponse(messageId, LocalDateTime.now(), true, request.contents());
+            if (currentSession.getAttributes().get("userId") == senderId) {
+                response = new ReadMessageResponse(messageId, LocalDateTime.now(), true, messageData.contents());
             } else {
-                response = new ReadMessageResponse(messageId, LocalDateTime.now(), false, request.contents());
+                response = new ReadMessageResponse(messageId, LocalDateTime.now(), false, messageData.contents());
             }
             final TextMessage textMessage = new TextMessage(objectMapper.writeValueAsString(response));
             sendMessagesDtos.add(new SendMessagesDto(session, textMessage));
         }
 
         return sendMessagesDtos;
+    }
+
+    private SessionAttributeDto getSessionAttributes(final WebSocketSession session) {
+        final Map<String, Object> attributes = session.getAttributes();
+
+        return objectMapper.convertValue(attributes, SessionAttributeDto.class);
+    }
+
+    private CreateMessageDto createMessageDto(final ChatMessageDataDto messageData, final Long userId) {
+        final CreateMessageRequest request = new CreateMessageRequest(
+                messageData.receiverId(),
+                messageData.contents()
+        );
+
+        return CreateMessageDto.of(userId, messageData.chatRoomId(), request);
     }
 
     @Override
